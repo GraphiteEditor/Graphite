0578e8f7c765c37d322c99e0a7210b0c4c6c0e8f
Keavon Chambers <keavon@keavon.com>
2021-02-14 18:46:08 -0800

    Add WASM bindings and web project structure

--- packages/wasm-bindings/src/lib.rs
+++ packages/wasm-bindings/src/lib.rs
@@ -1,7 +1,18 @@
-#[cfg(test)]
-mod tests {
-	#[test]
-	fn it_works() {
-		assert_eq!(2 + 2, 4);
-	}
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+	fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+	alert("Hello, Graphite!");
 }
