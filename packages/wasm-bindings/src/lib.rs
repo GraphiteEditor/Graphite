a3777cd08dff93c231c3516e6989623eee553ee3
protheory8 <editaw5@gmail.com>
2021-03-19 17:52:49 +0000

    Remove wee_alloc

--- packages/wasm-bindings/src/lib.rs
+++ packages/wasm-bindings/src/lib.rs
@@ -2,11 +2,6 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
 	fn alert(s: &str);
