018708791d442573738effa406fc2f410e9a1a26
ProTheory8 <59506423+protheory8@users.noreply.github.com>
2021-03-19 22:57:18 +0500

    Add init function to wasm-bindings crate and update dependencies (#33)

--- packages/wasm-bindings/src/lib.rs
+++ packages/wasm-bindings/src/lib.rs
@@ -2,17 +2,13 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
 	fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
+#[wasm_bindgen(start)]
+pub fn init() {
+	utils::set_panic_hook();
 	alert("Hello, Graphite!");
 }
