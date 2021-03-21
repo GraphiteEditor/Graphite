GraphiteEditor/Graphite

commit 29f5672bcb4b19d2130e8b680f03f33fd3f9e875

Add graphite-editor library skeleton (#34)

packages/graphite-editor/src/color.rs
@@ -1,0 +1,45 @@
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+pub struct Color {
+	red: f32,
+	green: f32,
+	blue: f32,
+	alpha: f32,
+}
+
+impl Color {
+	pub fn from_rgbaf32(red: f32, green: f32, blue: f32, alpha: f32) -> Result<Color, EditorError> {
+		let color = Color { red, green, blue, alpha };
+		if [red, green, blue, alpha].iter().any(|c| c.is_sign_negative() || !c.is_finite()) {
+			return EditorError::Color(color);
+		}
+		Ok(color)
+	}
+	pub fn from_rgb8(red: u8, green: u8, blue: u8) -> Color {
+		from_rgba8(red, green, blue, 255)
+	}
+	pub fn from_rgba8(red: u8, green: u8, blue: u8, alpha: u8) -> Color {
+		let map = |int_color| int_color as f32 / 255.0;
+		Color {
+			red: map(red),
+			green: map(green),
+			blue: map(blue),
+			alpha: map(alpha),
+		}
+	}
+	pub fn r(&self) -> f32 {
+		self.red
+	}
+	pub fn g(&self) -> f32 {
+		self.green
+	}
+	pub fn b(&self) -> f32 {
+		self.blue
+	}
+	pub fn a(&self) -> f32 {
+		self.alpha
+	}
+	pub fn components(&self) -> (f32, f32, f32, f32) {
+		(red, green, blue, alpha)
+	}
+}

packages/graphite-editor/src/error.rs
@@ -1,0 +1,37 @@
+use crate::Color;
+use std::error::Error;
+use std::fmt::{self, Display};
+
+/// The error type used by the graphite editor.
+#[derive(Debug)]
+pub enum EditorError {
+	InvalidOperation(String),
+	Misc(String),
+	Color(Color),
+}
+
+impl Display for EngineError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match self {
+			EngineError::InvalidOperation(e) => write!(f, "Failed to execute operation: {}", e),
+			EngineError::Misc(e) => write!(f, "{}", e),
+			EngineError::Color(c) => write!(f, "Tried to construct an invalid color {:?}", c),
+		}
+	}
+}
+
+impl Error for EditorError {}
+
+macro_rules! derive_from {
+	($type:ty, $kind:ident) => {
+		impl From<$type> for EngineError {
+			fn from(error: $type) -> Self {
+				EngineError::$kind(format!("{}", error))
+			}
+		}
+	};
+}
+
+derive_from!(&str, Misc);
+derive_from!(String, Misc);
+derive_from!(Color, Color);

packages/graphite-editor/src/tools/mod.rs
@@ -1,0 +1,42 @@
+use crate::Color;
+
+const TOOL_COUNT: usize = 10;
+
+struct ToolState {
+	primary_color: Color,
+	secondary_color: Color,
+	active_tool: ToolType,
+	tool_settings: [ToolSettings; TOOL_COUNT],
+}
+
+impl ToolState {
+	pub fn select_tool(&mut self, tool: ToolType) {
+		self.active_tool = ToolType
+	}
+}
+
+#[repr(usize)]
+enum ToolType {
+	Select = 0,
+	Crop = 1,
+	Navigate = 2,
+	Sample = 3,
+	Path = 4,
+	Pen = 5,
+	Line = 6,
+	Rectangle = 7,
+	Ellipse = 8,
+	Shape = 9,
+	// all discriminats must be strictly smaller than TOOL_COUNT!
+}
+
+enum ToolSettings {
+	Select { append_mode: SelectAppendMode },
+}
+
+enum SelectAppendMode {
+	New,
+	Add,
+	Substract,
+	Intersect,
+}

packages/graphite-editor/src/layout/mod.rs
@@ -1,0 +1,34 @@
+use crate::EditorError;
+type PanelId = usize;
+
+struct LayoutRoot {
+	hovered_panel: PanelId,
+	root: PanelGroup,
+}
+
+impl LayoutRoot {
+	// add panel / panel group
+	// delete panel / panel group
+	// move panel / panel group
+	// get_serialized_layout()
+}
+
+struct PanelGroup {
+	contents: Vec<Contents>,
+	layout_direction: LayoutDirection,
+}
+
+enum Contents {
+	PanelArea(PanelArea),
+	Group(PanelGroup),
+}
+
+struct PanelArea {
+	panels: Vec<PanelId>,
+	active: PanelId,
+}
+
+enum LayoutDirection {
+	Horizontal,
+	Vertical,
+}

packages/graphite-editor/Cargo.toml
@@ -1,0 +1,9 @@
+[package]
+name = "graphite-editor"
+version = "0.1.0"
+authors = ["Keavon Chambers <graphite@keavon.com>"]
+edition = "2018"
+
+
+[dependencies]
+log = "0.4"

packages/graphite-editor/src/lib.rs
@@ -1,0 +1,18 @@
+mod color;
+mod error;
+pub mod layout;
+mod scheduler;
+pub mod tools;
+
+#[doc(inline)]
+pub use error::EditorError;
+
+#[doc(inline)]
+pub use color::Color;
+
+use tools::ToolState;
+
+// TODO: serialize with serde to save the current editor state
+struct Editor {
+	tools: ToolState,
+}

commit b1ddbcc516ac7d23aab77e81190861376e3958b2

Fix errors introduced in 29f5672

packages/graphite-editor/src/color.rs
@@ -1,3 +1,5 @@
+use crate::EditorError;
+
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct Color {
@@ -11,12 +13,12 @@ impl Color {
 	pub fn from_rgbaf32(red: f32, green: f32, blue: f32, alpha: f32) -> Result<Color, EditorError> {
 		let color = Color { red, green, blue, alpha };
 		if [red, green, blue, alpha].iter().any(|c| c.is_sign_negative() || !c.is_finite()) {
-			return EditorError::Color(color);
+			Err(color)?
 		}
 		Ok(color)
 	}
 	pub fn from_rgb8(red: u8, green: u8, blue: u8) -> Color {
-		from_rgba8(red, green, blue, 255)
+		Color::from_rgba8(red, green, blue, 255)
 	}
 	pub fn from_rgba8(red: u8, green: u8, blue: u8, alpha: u8) -> Color {
 		let map = |int_color| int_color as f32 / 255.0;
@@ -40,6 +42,6 @@ impl Color {
 		self.alpha
 	}
 	pub fn components(&self) -> (f32, f32, f32, f32) {
-		(red, green, blue, alpha)
+		(self.red, self.green, self.blue, self.alpha)
 	}
 }

packages/graphite-editor/src/error.rs
@@ -7,15 +7,15 @@ use std::fmt::{self, Display};
 pub enum EditorError {
 	InvalidOperation(String),
 	Misc(String),
-	Color(Color),
+	Color(String),
 }
 
-impl Display for EngineError {
+impl Display for EditorError {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
-			EngineError::InvalidOperation(e) => write!(f, "Failed to execute operation: {}", e),
-			EngineError::Misc(e) => write!(f, "{}", e),
-			EngineError::Color(c) => write!(f, "Tried to construct an invalid color {:?}", c),
+			EditorError::InvalidOperation(e) => write!(f, "Failed to execute operation: {}", e),
+			EditorError::Misc(e) => write!(f, "{}", e),
+			EditorError::Color(c) => write!(f, "Tried to construct an invalid color {:?}", c),
 		}
 	}
 }
@@ -24,9 +24,9 @@ impl Error for EditorError {}
 
 macro_rules! derive_from {
 	($type:ty, $kind:ident) => {
-		impl From<$type> for EngineError {
+		impl From<$type> for EditorError {
 			fn from(error: $type) -> Self {
-				EngineError::$kind(format!("{}", error))
+				EditorError::$kind(format!("{:?}", error))
 			}
 		}
 	};

packages/graphite-editor/src/tools/mod.rs
@@ -2,7 +2,7 @@ use crate::Color;
 
 const TOOL_COUNT: usize = 10;
 
-struct ToolState {
+pub struct ToolState {
 	primary_color: Color,
 	secondary_color: Color,
 	active_tool: ToolType,
@@ -11,12 +11,12 @@ struct ToolState {
 
 impl ToolState {
 	pub fn select_tool(&mut self, tool: ToolType) {
-		self.active_tool = ToolType
+		self.active_tool = tool
 	}
 }
 
 #[repr(usize)]
-enum ToolType {
+pub enum ToolType {
 	Select = 0,
 	Crop = 1,
 	Navigate = 2,
@@ -30,11 +30,11 @@ enum ToolType {
 	// all discriminats must be strictly smaller than TOOL_COUNT!
 }
 
-enum ToolSettings {
+pub enum ToolSettings {
 	Select { append_mode: SelectAppendMode },
 }
 
-enum SelectAppendMode {
+pub enum SelectAppendMode {
 	New,
 	Add,
 	Substract,