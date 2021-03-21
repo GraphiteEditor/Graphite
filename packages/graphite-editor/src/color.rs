6b93e2c008b979a341f353ed16bbfb3cfe61a32e
TrueDoctor <dennis@kobert.dev>
2021-03-21 21:03:29 +0100

    Fix errors introduced in 29f5672 (#35)

--- packages/graphite-editor/src/color.rs
+++ packages/graphite-editor/src/color.rs
@@ -1,3 +1,5 @@
+use crate::EditorError;
+
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct Color {
@@ -11,12 +13,12 @@ impl Color {
 	pub fn from_rgbaf32(red: f32, green: f32, blue: f32, alpha: f32) -> Result<Color, EditorError> {
 		let color = Color { red, green, blue, alpha };
 		if [red, green, blue, alpha].iter().any(|c| c.is_sign_negative() || !c.is_finite()) {
-			return EditorError::Color(color);
+			Err(color)?
 		}
 		Ok(color)
 	}
 	pub fn from_rgb8(red: u8, green: u8, blue: u8) -> Color {
-		from_rgba8(red, green, blue, 255)
+		Color::from_rgba8(red, green, blue, 255)
 	}
 	pub fn from_rgba8(red: u8, green: u8, blue: u8, alpha: u8) -> Color {
 		let map = |int_color| int_color as f32 / 255.0;
@@ -40,6 +42,6 @@ impl Color {
 		self.alpha
 	}
 	pub fn components(&self) -> (f32, f32, f32, f32) {
-		(red, green, blue, alpha)
+		(self.red, self.green, self.blue, self.alpha)
 	}
 }
--- packages/graphite-editor/src/error.rs
+++ packages/graphite-editor/src/error.rs
@@ -7,15 +7,15 @@ use std::fmt::{self, Display};
 pub enum EditorError {
 	InvalidOperation(String),
 	Misc(String),
-	Color(Color),
+	Color(String),
 }
 
-impl Display for EngineError {
+impl Display for EditorError {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
 		match self {
-			EngineError::InvalidOperation(e) => write!(f, "Failed to execute operation: {}", e),
-			EngineError::Misc(e) => write!(f, "{}", e),
-			EngineError::Color(c) => write!(f, "Tried to construct an invalid color {:?}", c),
+			EditorError::InvalidOperation(e) => write!(f, "Failed to execute operation: {}", e),
+			EditorError::Misc(e) => write!(f, "{}", e),
+			EditorError::Color(c) => write!(f, "Tried to construct an invalid color {:?}", c),
 		}
 	}
 }
@@ -24,9 +24,9 @@ impl Error for EditorError {}
 
 macro_rules! derive_from {
 	($type:ty, $kind:ident) => {
-		impl From<$type> for EngineError {
+		impl From<$type> for EditorError {
 			fn from(error: $type) -> Self {
-				EngineError::$kind(format!("{}", error))
+				EditorError::$kind(format!("{:?}", error))
 			}
 		}
 	};
