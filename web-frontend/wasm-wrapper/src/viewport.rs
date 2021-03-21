d254916430a3fdb5cd7acc4f7e0b73f42ed33ce8
TrueDoctor <dennis@kobert.dev>
2021-03-21 19:32:56 +0100

    Define the js wasm-editor interface (#31)

---
+++ web-frontend/wasm-wrapper/src/viewport.rs
@@ -0,0 +1,18 @@
+/// Modify the currently selected tool in the document state store
+#[wasm_bindgen]
+pub fn select_tool(tool: String) {
+	todo!()
+}
+
+/// Mouse movement with the bounds of the canvas
+#[wasm_bindgen]
+pub fn on_mouse_move(x: u32, y: u32) {
+	todo!()
+}
+
+use graphite_editor::Color;
+/// Update working colors
+#[wasm_bindgen]
+pub fn update_colors(primary_color: Color, secondary_color: Color) {
+	todo!()
+}
