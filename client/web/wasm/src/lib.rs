6719f6e8f7ea967d41dafc1c920b6f855ff286be
Keavon Chambers <keavon@keavon.com>
2021-03-27 12:56:53 -0700

    Add asset manager sources to docs outline

--- client/web/wasm/src/lib.rs
+++ client/web/wasm/src/lib.rs
@@ -8,6 +8,7 @@ use graphite_editor_core::Editor;
 use std::cell::RefCell;
 use wasm_bindgen::prelude::*;
 
+// the thread_local macro provides a way to initialize static variables with non-constant functions
 thread_local! {pub static EDITOR_STATE: RefCell<Editor> = RefCell::new(Editor::new())}
 
 #[wasm_bindgen(start)]
--- client/web/wasm/src/viewport.rs
+++ client/web/wasm/src/viewport.rs
@@ -1,17 +1,17 @@
 use crate::shims::Error;
 use crate::wrappers::{translate_tool, Color};
+use crate::EDITOR_STATE;
 use wasm_bindgen::prelude::*;
 
 /// Modify the currently selected tool in the document state store
 #[wasm_bindgen]
 pub fn select_tool(tool: String) -> Result<(), JsValue> {
-	crate::EDITOR_STATE.with(|editor| {
-		if let Some(tool) = translate_tool(&tool) {
+	EDITOR_STATE.with(|editor| match translate_tool(&tool) {
+		Some(tool) => {
 			editor.borrow_mut().tools.active_tool = tool;
 			Ok(())
-		} else {
-			Err(Error::new(&format!("Couldn't select {} because it was not recognized as a valid tool", tool)).into())
 		}
+		None => Err(Error::new(&format!("Couldn't select {} because it was not recognized as a valid tool", tool)).into()),
 	})
 }
 
@@ -24,7 +24,7 @@ pub fn on_mouse_move(x: u32, y: u32) {
 /// Update working colors
 #[wasm_bindgen]
 pub fn update_colors(primary_color: Color, secondary_color: Color) {
-	crate::EDITOR_STATE.with(|editor| {
+	EDITOR_STATE.with(|editor| {
 		let mut editor = editor.borrow_mut();
 		editor.tools.primary_color = primary_color.inner();
 		editor.tools.secondary_color = secondary_color.inner();
