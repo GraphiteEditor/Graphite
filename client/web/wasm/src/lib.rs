593572a4e826464ca441c11ee25cfcf5b939cdee
TrueDoctor <dennis@kobert.dev>
2021-03-30 18:48:32 +0200

    Add loging implementation for wasm (#56)

--- client/web/wasm/src/lib.rs
+++ client/web/wasm/src/lib.rs
@@ -4,16 +4,20 @@ pub mod utils;
 pub mod window;
 pub mod wrappers;
 
-use editor_core::{events::Response, Callback, Editor};
+use editor_core::{events::Response, Editor};
 use std::cell::RefCell;
+use utils::WasmLog;
 use wasm_bindgen::prelude::*;
 
 // the thread_local macro provides a way to initialize static variables with non-constant functions
 thread_local! { pub static EDITOR_STATE: RefCell<Editor> = RefCell::new(Editor::new(Box::new(handle_response))) }
+static LOGGER: WasmLog = WasmLog;
 
 #[wasm_bindgen(start)]
 pub fn init() {
 	utils::set_panic_hook();
+	log::set_logger(&LOGGER).expect("Failed to set logger");
+	log::set_max_level(log::LevelFilter::Debug);
 }
 
 fn handle_response(response: Response) {
--- client/web/wasm/src/utils.rs
+++ client/web/wasm/src/utils.rs
@@ -1,3 +1,4 @@
+use wasm_bindgen::prelude::*;
 pub fn set_panic_hook() {
 	// When the `console_error_panic_hook` feature is enabled, we can call the
 	// `set_panic_hook` function at least once during initialization, and then
@@ -7,4 +8,38 @@ pub fn set_panic_hook() {
 	// https://github.com/rustwasm/console_error_panic_hook#readme
 	#[cfg(feature = "console_error_panic_hook")]
 	console_error_panic_hook::set_once();
+}
+
+#[wasm_bindgen]
+extern "C" {
+	#[wasm_bindgen(js_namespace = console)]
+	fn debug(msg: &str, format: &str);
+	#[wasm_bindgen(js_namespace = console)]
+	fn info(msg: &str, format: &str);
+	#[wasm_bindgen(js_namespace = console)]
+	fn warn(msg: &str, format: &str);
+	#[wasm_bindgen(js_namespace = console)]
+	fn error(msg: &str, format: &str);
+}
+
+#[derive(Default)]
+pub struct WasmLog;
+
+impl log::Log for WasmLog {
+	fn enabled(&self, metadata: &log::Metadata) -> bool {
+		metadata.level() <= log::Level::Info
+	}
+
+	fn log(&self, record: &log::Record) {
+		let (log, name, color): (fn(&str, &str), &str, &str) = match record.level() {
+			log::Level::Trace => (debug, "trace", "color:plum"),
+			log::Level::Debug => (debug, "debug", "color:plum"),
+			log::Level::Warn => (warn, "warn", "color:#1b8"),
+			log::Level::Info => (info, "info", "color:#fa2"),
+			log::Level::Error => (error, "error", "color:red"),
+		};
+		let msg = &format!("{}", format_args!("%c{}%c\t{}", name, record.args()));
+		log(msg, color)
+	}
+	fn flush(&self) {}
 }
