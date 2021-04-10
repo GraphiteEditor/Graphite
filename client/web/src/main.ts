958d3515054f089a35349b840e8aeeca2a321aee
Keavon Chambers <keavon@keavon.com>
2021-04-10 02:48:33 -0700

    Plumb Responses from WASM to JS in the page

--- client/web/src/main.ts
+++ client/web/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { attachResponseHandlerToPage } from "./response-handler";
+
+attachResponseHandlerToPage();
 
 createApp(App).mount("#app");
---
+++ client/web/src/response-handler.ts
@@ -0,0 +1,30 @@
+type ResponseCallback = (responseData: string) => void;
+type ResponseMap = {
+	[response: string]: ResponseCallback | undefined;
+};
+declare global {
+	interface Window { responseMap: ResponseMap }
+}
+
+export enum ResponseType {
+	UpdateCanvas = "UpdateCanvas",
+}
+
+export function attachResponseHandlerToPage() {
+	window.responseMap = {};
+}
+
+export function registerResponseHandler(responseType: ResponseType, callback: ResponseCallback) {
+	window.responseMap[responseType] = callback;
+}
+
+export function handleResponse(responseType: ResponseType, responseData: string) {
+	const callback = window.responseMap[responseType];
+
+	if (callback) {
+		callback(responseData);
+	}
+	else {
+		console.error(`Received a Response of type "${responseType}" but no handler was registered for it from the client.`);
+	}
+}
