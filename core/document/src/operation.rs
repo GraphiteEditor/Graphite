6511f5a628861a5fe40efae059f04d559c7b62f5
TrueDoctor <dennis@kobert.dev>
2021-04-12 00:57:39 +0200

    Implement svg layer structure (#59)
    * Add folder struct
    
    * Add file system like structure to interface with svgs
    
    * Add primitive undo functionality
    
    * Restructure layer system

--- core/document/src/operation.rs
+++ core/document/src/operation.rs
@@ -1,4 +1,25 @@
+use crate::LayerId;
+
 pub enum Operation {
-	AddCircle { cx: f64, cy: f64, r: f64 },
-	AddRect { x0: f64, y0: f64, x1: f64, y1: f64 },
+	AddCircle {
+		path: Vec<LayerId>,
+		insert_index: isize,
+		cx: f64,
+		cy: f64,
+		r: f64,
+	},
+	AddRect {
+		path: Vec<LayerId>,
+		insert_index: isize,
+		x0: f64,
+		y0: f64,
+		x1: f64,
+		y1: f64,
+	},
+	DeleteLayer {
+		path: Vec<LayerId>,
+	},
+	AddFolder {
+		path: Vec<LayerId>,
+	},
 }
