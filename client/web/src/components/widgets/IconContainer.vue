0283852d1db96357dc6a35b3a85aa77a60a6960c
Keavon Chambers <keavon@keavon.com>
2021-05-02 13:55:22 -0700

    Add basic HTML for layers in the layer panel (#97)

---
+++ client/web/src/components/widgets/IconContainer.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="icon-container" :class="[`size-${String(size)}`, gapAfter && 'gap-after']">
+		<slot></slot>
+	</div>
+</template>
+
+<style lang="scss">
+.icon-container {
+	display: inline-block;
+	flex: 0 0 auto;
+	outline: none;
+	border: none;
+	padding: 0;
+	width: 16px;
+	height: 16px;
+	background: none;
+	font-weight: bold;
+	font-size: 10px;
+	border-radius: 2px;
+	color: #ddd;
+	fill: #ddd;
+
+	&:not(.gap-after) + .icon-container {
+		margin-left: 0;
+	}
+
+	&.size-10 {
+		width: 10px;
+		height: 10px;
+	}
+
+	&.size-12 {
+		width: 12px;
+		height: 12px;
+	}
+
+	&.size-16 {
+		width: 16px;
+		height: 16px;
+	}
+
+	&.size-24 {
+		width: 24px;
+		height: 24px;
+	}
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	props: {
+		size: { type: Number, required: true },
+		gapAfter: { type: Boolean, default: false },
+	},
+});
+</script>
