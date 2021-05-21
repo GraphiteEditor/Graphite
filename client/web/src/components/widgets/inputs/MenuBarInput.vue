897d4c281bad8ec0a1d0633fb50a562d1b528f2c
Keavon Chambers <keavon@keavon.com>
2021-05-21 01:55:06 -0700

    Rename and organize widget components to conform to design terminology

---
+++ client/web/src/components/widgets/inputs/MenuBarInput.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="menu-bar-input">
+		<div class="entry"><GraphiteLogo width="16" height="16" /></div>
+		<div class="entry"><span>File</span></div>
+		<div class="entry"><span>Edit</span></div>
+		<div class="entry"><span>Document</span></div>
+		<div class="entry"><span>View</span></div>
+		<div class="entry"><span>Help</span></div>
+	</div>
+</template>
+
+<style lang="scss">
+.menu-bar-input {
+	display: flex;
+
+	.entry {
+		display: flex;
+		align-items: center;
+		white-space: nowrap;
+		padding: 0 8px;
+
+		svg {
+			fill: #ddd;
+		}
+
+		&:hover {
+			background: #666;
+
+			svg {
+				fill: #fff;
+			}
+
+			span {
+				color: #fff;
+			}
+		}
+	}
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ApplicationPlatform } from "../../window/MainWindow.vue";
+import GraphiteLogo from "../../../../assets/svg/16x16-bounds-16x16-icon/graphite-logo.svg";
+
+export default defineComponent({
+	components: { GraphiteLogo },
+	data() {
+		return {
+			ApplicationPlatform,
+		};
+	},
+});
+</script>
---
+++ client/web/src/components/widgets/inputs/SwatchPairInput.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="swatch-pair">
+		<div class="secondary swatch">
+			<button @click="clickSecondarySwatch" style="background: white"></button>
+			<Popover :direction="PopoverDirection.Right" horizontal ref="secondarySwatchPopover">
+				<ColorPicker />
+			</Popover>
+		</div>
+		<div class="primary swatch">
+			<button @click="clickPrimarySwatch" style="background: black"></button>
+			<Popover :direction="PopoverDirection.Right" horizontal ref="primarySwatchPopover">
+				<ColorPicker />
+			</Popover>
+		</div>
+	</div>
+</template>
+
+<style lang="scss">
+.swatch-pair {
+	display: flex;
+	// Reversed order of elements paired with `column-reverse` allows primary to overlap secondary without relying on `z-index`
+	flex-direction: column-reverse;
+
+	.swatch {
+		width: 28px;
+		height: 28px;
+		margin: 2px;
+		position: relative;
+
+		button {
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+			border: 2px #888 solid;
+			box-shadow: 0 0 0 2px #333;
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+			outline: none;
+		}
+
+		.popover {
+			top: 50%;
+			right: -2px;
+		}
+
+		&.primary {
+			margin-bottom: -8px;
+		}
+	}
+}
+</style>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ColorPicker from "../../popovers/ColorPicker.vue";
+import Popover, { PopoverDirection } from "../overlays/Popover.vue";
+
+export default defineComponent({
+	components: {
+		Popover,
+		ColorPicker,
+	},
+	props: {},
+	methods: {
+		clickPrimarySwatch() {
+			(this.$refs.primarySwatchPopover as typeof Popover).setOpen();
+			(this.$refs.secondarySwatchPopover as typeof Popover).setClosed();
+		},
+		clickSecondarySwatch() {
+			(this.$refs.secondarySwatchPopover as typeof Popover).setOpen();
+			(this.$refs.primarySwatchPopover as typeof Popover).setClosed();
+		},
+	},
+	data() {
+		return {
+			PopoverDirection,
+		};
+	},
+});
+</script>
