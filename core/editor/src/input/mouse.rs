0b3db1797c0a6dfe64ba416591bceb773f1c9d45
0hypercube <0hypercube@gmail.com>
2021-05-23 10:54:28 +0100

    Add canvas position and canvas transform

--- core/editor/src/input/mouse.rs
+++ core/editor/src/input/mouse.rs
@@ -1,5 +1,47 @@
 use bitflags::bitflags;
 
+// A position on the infinite canvas
+#[derive(Debug, Copy, Clone, Default, PartialEq)]
+pub struct CanvasPosition {
+	pub x: f64,
+	pub y: f64,
+}
+
+impl CanvasPosition {
+	pub fn distance(&self, other: &Self) -> f64 {
+		let x_diff = other.x - self.x;
+		let y_diff = other.y - self.y;
+		f64::sqrt(x_diff * x_diff + y_diff * y_diff)
+	}
+	pub fn rotate(&mut self, theta: f64) -> &mut Self {
+		let cosine = theta.cos();
+		let sine = theta.sin();
+		self.x = self.x * cosine - self.y * sine;
+		self.y = self.x * sine + self.y * cosine;
+		self
+	}
+}
+
+// The location of the viewport (or anything else) in the canvas
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct CanvasTransform {
+	pub location: CanvasPosition,
+	pub rotation: f64,
+	pub scale: f64,
+	pub center: ViewportPosition,
+}
+
+impl Default for CanvasTransform {
+	fn default() -> Self {
+		Self {
+			location: CanvasPosition{x:100., y:100.},
+			scale: 1.,
+			rotation: 0.,
+			center: ViewportPosition::default(),
+		}
+	}
+}
+
 // origin is top left
 #[derive(Debug, Copy, Clone, Default, Eq, PartialEq)]
 pub struct ViewportPosition {
@@ -12,6 +54,13 @@ impl ViewportPosition {
 		let x_diff = other.x as i64 - self.x as i64;
 		let y_diff = other.y as i64 - self.y as i64;
 		f64::sqrt((x_diff * x_diff + y_diff * y_diff) as f64)
+	}
+	pub fn to_canvas_position(&self, canvas_transform: &CanvasTransform) -> CanvasPosition {
+		*CanvasPosition {
+			x: (self.x - canvas_transform.center.x) as f64 * canvas_transform.scale + canvas_transform.location.x,
+			y: (self.x - canvas_transform.center.x) as f64 * canvas_transform.scale + canvas_transform.location.y,
+		}
+		.rotate(canvas_transform.rotation)
 	}
 }
 
