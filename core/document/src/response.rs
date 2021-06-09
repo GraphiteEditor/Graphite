0e8cbad003c5f4156dfffeba1aec3c430dbf24a3
TrueDoctor <dennis@kobert.dev>
2021-06-09 12:20:50 +0200

    Implement backend part of the layer selection (#172)
    * Implement backend part of the layer selection

--- core/document/src/response.rs
+++ core/document/src/response.rs
@@ -1,91 +1,19 @@
-use crate::{
-	layers::{Layer, LayerDataTypes},
-	LayerId,
-};
+use crate::LayerId;
 use serde::{Deserialize, Serialize};
 use std::fmt;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
-pub struct LayerPanelEntry {
-	pub name: String,
-	pub visible: bool,
-	pub layer_type: LayerType,
-	pub collapsed: bool,
-	pub path: Vec<LayerId>,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
-pub enum LayerType {
-	Folder,
-	Shape,
-	Circle,
-	Rect,
-	Line,
-	PolyLine,
-	Ellipse,
-}
-
-impl fmt::Display for LayerType {
-	fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-		let name = match self {
-			LayerType::Folder => "Folder",
-			LayerType::Shape => "Shape",
-			LayerType::Rect => "Rectangle",
-			LayerType::Line => "Line",
-			LayerType::Circle => "Circle",
-			LayerType::PolyLine => "Polyline",
-			LayerType::Ellipse => "Ellipse",
-		};
-
-		formatter.write_str(name)
-	}
-}
-
-impl From<&LayerDataTypes> for LayerType {
-	fn from(data: &LayerDataTypes) -> Self {
-		use LayerDataTypes::*;
-		match data {
-			Folder(_) => LayerType::Folder,
-			Shape(_) => LayerType::Shape,
-			Circle(_) => LayerType::Circle,
-			Rect(_) => LayerType::Rect,
-			Line(_) => LayerType::Line,
-			PolyLine(_) => LayerType::PolyLine,
-			Ellipse(_) => LayerType::Ellipse,
-		}
-	}
-}
-
-impl LayerPanelEntry {
-	pub fn from_layer(layer: &Layer, path: Vec<LayerId>) -> Self {
-		let layer_type: LayerType = (&layer.data).into();
-		let name = layer.name.clone().unwrap_or_else(|| format!("Unnamed {}", layer_type));
-		let collapsed = if let LayerDataTypes::Folder(f) = &layer.data { f.collapsed } else { true };
-		Self {
-			name,
-			visible: layer.visible,
-			layer_type,
-			collapsed,
-			path,
-		}
-	}
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 #[repr(C)]
-// TODO - Make Copy when possible
 pub enum DocumentResponse {
 	DocumentChanged,
-	CollapseFolder { path: Vec<LayerId> },
-	ExpandFolder { path: Vec<LayerId>, children: Vec<LayerPanelEntry> },
+	FolderChanged { path: Vec<LayerId> },
 }
 
 impl fmt::Display for DocumentResponse {
 	fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
 		let name = match self {
 			DocumentResponse::DocumentChanged { .. } => "DocumentChanged",
-			DocumentResponse::CollapseFolder { .. } => "CollapseFolder",
-			DocumentResponse::ExpandFolder { .. } => "ExpandFolder",
+			DocumentResponse::FolderChanged { .. } => "FolderChanged",
 		};
 
 		formatter.write_str(name)
