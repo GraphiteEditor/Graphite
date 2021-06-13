ce66e35d1a29f503b03f0d008e505781ac89f1d1
0hypercube <0hypercube@gmail.com>
2021-06-13 15:29:57 +0100

    Format

--- core/document/src/document.rs
+++ core/document/src/document.rs
@@ -190,8 +190,16 @@ impl Document {
 
 				Some(vec![DocumentResponse::DocumentChanged, DocumentResponse::SelectLayer { path }])
 			}
-			Operation::AddLine { path, insert_index, x0,y0,x1,y1, style } => {
-				let id = self.add_layer(&path, Layer::new(LayerDataTypes::Line(Line::new(*x0,*y0,*x1,*y1, *style))), *insert_index)?;
+			Operation::AddLine {
+				path,
+				insert_index,
+				x0,
+				y0,
+				x1,
+				y1,
+				style,
+			} => {
+				let id = self.add_layer(&path, Layer::new(LayerDataTypes::Line(Line::new(*x0, *y0, *x1, *y1, *style))), *insert_index)?;
 				let path = [path.clone(), vec![id]].concat();
 
 				Some(vec![DocumentResponse::DocumentChanged, DocumentResponse::SelectLayer { path }])
--- core/document/src/layers/line.rs
+++ core/document/src/layers/line.rs
@@ -10,12 +10,9 @@ pub struct Line {
 }
 
 impl Line {
-	pub fn new(x0:f64, y0:f64, x1:f64, y1:f64, style: style::PathStyle) -> Line {
+	pub fn new(x0: f64, y0: f64, x1: f64, y1: f64, style: style::PathStyle) -> Line {
 		Line {
-			shape: kurbo::Line::new(
-				(x0,y0),
-				(x1,y1),
-			),
+			shape: kurbo::Line::new((x0, y0), (x1, y1)),
 			style,
 		}
 	}
