b1eb29b4e3a40d9246c39bab723159dd848885f1
geom3trik <geomyles@yahoo.co.uk>
2021-06-13 23:31:57 +0100

    Remove keybind for previous document. Change keybind for next document.

--- core/editor/src/input/input_mapper.rs
+++ core/editor/src/input/input_mapper.rs
@@ -162,8 +162,7 @@ impl Default for Mapping {
 			entry! {action=DocumentMessage::ExportDocument, key_down=KeyS, modifiers=[KeyControl, KeyShift]},
 			entry! {action=DocumentMessage::ExportDocument, key_down=KeyE, modifiers=[KeyControl]},
 			entry! {action=DocumentMessage::NewDocument, key_down=KeyN, modifiers=[KeyShift]},
-			entry! {action=DocumentMessage::PrevDocument, key_down=KeyTab, modifiers=[KeyShift]},
-			entry! {action=DocumentMessage::NextDocument, key_down=KeyTab},
+			entry! {action=DocumentMessage::NextDocument, key_down=KeyTab, modifiers=[KeyShift]},
 			// Global Actions
 			entry! {action=GlobalMessage::LogInfo, key_down=Key1},
 			entry! {action=GlobalMessage::LogDebug, key_down=Key2},
