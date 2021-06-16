87e5bf352a06093f41f86cd093ab27922d3d7454
akshay1992kalbhor <akshay1992kalbhor@gmail.com>
2021-06-16 15:47:00 +0530

    Remove comments

--- client/web/src/components/workspace/Panel.vue
+++ client/web/src/components/workspace/Panel.vue
@@ -157,7 +157,6 @@ export default defineComponent({
 	},
 	methods: {
 		async handleTabClick(tabIndex: number) {
-			console.log("HAHAHAHAHAHAHA");
 			const { select_document } = await wasm;
 			select_document(tabIndex);
 		},
