5d3a4964568a0d57acf46ddd4b401dc434968316
0HyperCube <78500760+0HyperCube@users.noreply.github.com>
2021-07-11 10:24:46 +0100

    Allow transforming layers (#244)
    * Allow transforming layers
    
    * Add document_layer function to return non mut

--- core/document/src/document.rs
+++ core/document/src/document.rs
@@ -140,6 +140,24 @@ impl Document {
 		Ok(root)
 	}
 
+	/// Returns a reference to the layer or folder at the path. Does not return an error for root
+	pub fn document_layer(&mut self, path: &[LayerId]) -> Result<&Layer, DocumentError> {
+		if path.is_empty() {
+			return Ok(&self.root);
+		}
+		let (path, id) = split_path(path)?;
+		self.document_folder(path)?.as_folder()?.layer(id).ok_or(DocumentError::LayerNotFound)
+	}
+
+	/// Returns a mutable reference to the layer or folder at the path. Does not return an error for root
+	pub fn document_layer_mut(&mut self, path: &[LayerId]) -> Result<&mut Layer, DocumentError> {
+		if path.is_empty() {
+			return Ok(&mut self.root);
+		}
+		let (path, id) = split_path(path)?;
+		self.document_folder_mut(path)?.as_folder_mut()?.layer_mut(id).ok_or(DocumentError::LayerNotFound)
+	}
+
 	/// Returns a reference to the layer struct at the specified `path`.
 	pub fn layer(&self, path: &[LayerId]) -> Result<&Layer, DocumentError> {
 		let (path, id) = split_path(path)?;
@@ -291,10 +309,11 @@ impl Document {
 				None
 			}
 			Operation::TransformLayer { path, transform } => {
-				let transform = self.root.transform * DAffine2::from_cols_array(&transform);
-				let layer = self.document_folder_mut(path).unwrap();
+				let layer = self.document_layer_mut(path).unwrap();
+				let transform = DAffine2::from_cols_array(&transform) * layer.transform;
 				layer.transform = transform;
 				layer.cache_dirty = true;
+				self.root.cache_dirty = true;
 				Some(vec![DocumentResponse::DocumentChanged])
 			}
 			Operation::DiscardWorkingFolder => {
