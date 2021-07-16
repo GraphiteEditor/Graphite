7a087ba71a63a0de2eed05b53dba12ad1830d471
Henry Sloan <henryksloan@gmail.com>
2021-07-16 02:38:46 -0400

    Pass layer metadata to tools (#277)

--- core/editor/src/communication/dispatcher.rs
+++ core/editor/src/communication/dispatcher.rs
@@ -40,7 +40,7 @@ impl Dispatcher {
 			Global(message) => self.global_message_handler.process_action(message, (), &mut self.messages),
 			Tool(message) => self
 				.tool_message_handler
-				.process_action(message, (&self.document_message_handler.active_document().document, &self.input_preprocessor), &mut self.messages),
+				.process_action(message, (&self.document_message_handler.active_document(), &self.input_preprocessor), &mut self.messages),
 			Frontend(message) => self.frontend_message_handler.process_action(message, (), &mut self.messages),
 			InputPreprocessor(message) => self.input_preprocessor.process_action(message, (), &mut self.messages),
 			InputMapper(message) => {
