cd6921540488bae84efc527c3ac8b6076bd5fd78
AJ Weeks <ajweeks@shaw.ca>
2021-07-16 17:18:36 +0100

    Fix merge mistake

--- core/document/src/document.rs
+++ core/document/src/document.rs
@@ -227,6 +227,14 @@ impl Document {
 		Ok(())
 	}
 
+	pub fn reorder_layer(&mut self, source_path: &[LayerId], target_path: &[LayerId]) -> Result<(), DocumentError> {
+		// TODO: Detect when moving between folders and handle properly
+
+		self.root.as_folder_mut()?.reorder_layer(*source_path.last().unwrap(), *target_path.last().unwrap())?;
+
+		Ok(())
+	}
+
 	pub fn layer_axis_aligned_bounding_box(&self, path: &[LayerId]) -> Result<Option<[DVec2; 2]>, DocumentError> {
 		// TODO: Replace with functions of the transform api
 		if let &[] = path {
@@ -236,19 +244,6 @@ impl Document {
 			let layer = self.document_layer(path)?;
 			Ok(layer.bounding_box(self.root.transform * layer.transform, layer.style))
 		}
-	}
-	
-	pub fn reorder_layer(&mut self, source_path: &[LayerId], target_path: &[LayerId]) -> Result<(), DocumentError> {
-		// TODO: Detect when moving between folders and handle properly
-
-		self.root.as_folder_mut()?.reorder_layer(*source_path.last().unwrap(), *target_path.last().unwrap())?;
-
-		Ok(())
-	}
-
-	pub fn layer_axis_aligned_bounding_box(&self, path: &[LayerId]) -> Result<[DVec2; 2], DocumentError> {
-		let layer = self.layer(path)?;
-		Ok(layer.bounding_box(self.root.transform * layer.transform, layer.style))
 	}
 
 	pub fn layer_local_bounding_box(&self, path: &[LayerId]) -> Result<Option<[DVec2; 2]>, DocumentError> {
