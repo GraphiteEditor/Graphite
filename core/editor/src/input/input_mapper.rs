dd7a5f5e57161dd9f81662430aaab5c5c906aad8
AJ Weeks <ajweeks@shaw.ca>
2021-07-16 13:01:35 +0100

    Fix "Move layer to top/bottom" keybinds

--- core/editor/src/input/input_mapper.rs
+++ core/editor/src/input/input_mapper.rs
@@ -237,8 +237,8 @@ impl Default for Mapping {
 			entry! {action=DocumentMessage::NudgeSelectedLayers(NUDGE_AMOUNT, 0.), key_down=KeyArrowRight},
 			entry! {action=DocumentMessage::MoveSelectedLayer(-1), key_down=KeyLeftBracket, modifiers=[KeyControl]},
 			entry! {action=DocumentMessage::MoveSelectedLayer(1), key_down=KeyRightBracket, modifiers=[KeyControl]},
-			entry! {action=DocumentMessage::MoveSelectedLayer(-999999), key_down=KeyLeftBracket, modifiers=[KeyControl, KeyShift]},
-			entry! {action=DocumentMessage::MoveSelectedLayer(999999), key_down=KeyRightBracket, modifiers=[KeyControl, KeyShift]},
+			entry! {action=DocumentMessage::MoveSelectedLayer(-999999), key_down=KeyLeftCurlyBracket, modifiers=[KeyControl]},
+			entry! {action=DocumentMessage::MoveSelectedLayer(999999), key_down=KeyRightCurlyBracket, modifiers=[KeyControl]},
 			// Global Actions
 			entry! {action=GlobalMessage::LogInfo, key_down=Key1},
 			entry! {action=GlobalMessage::LogDebug, key_down=Key2},
