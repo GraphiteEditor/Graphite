f9186191889eccafc7d4994f44ced34ea14a72e2
Simon Desloges <desloges.simon@gmail.com>
2021-07-21 16:30:53 -0400

    Fix a clippy lint

--- core/document/src/bounding_box.rs
+++ core/document/src/bounding_box.rs
@@ -5,5 +5,5 @@ pub fn merge_bounding_boxes([a_min, a_max]: [DVec2; 2], [b_min, b_max]: [DVec2; 
 	let min_y = a_min.y.min(b_min.y);
 	let max_x = a_max.x.max(b_max.x);
 	let max_y = a_max.y.max(b_max.y);
-	return [DVec2::new(min_x, min_y), DVec2::new(max_x, max_y)];
+	[DVec2::new(min_x, min_y), DVec2::new(max_x, max_y)]
 }
