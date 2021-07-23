dac60e94da163b30368cbcd720e1812466ad5c9b
RustyNixieTube <49496554+RustyNixieTube@users.noreply.github.com>
2021-07-23 16:16:26 +0200

    Add documentation (#287)
    * Add crate documentation
    
    * Use full path intead of relative path
    
    * Add documentation to core/document/src/color.rs
    
    * Add modification by
    
    * Add @Keavon's modification
    
    * @Keavon's changes
    
    * remove useless let
    
    Co-authored-by: TrueDoctor <dennis@kobert.dev>

--- core/renderer/src/lib.rs
+++ core/renderer/src/lib.rs
@@ -1,3 +1,9 @@
+//! Graphite Renderer Core Library: `/core/renderer/`  
+//! A stateless library (with the help of in-memory and/or on-disk caches for performance) for rendering Graphite's render graph (GRD) files.
+//! The official Graphite CLI and Document Core Library are the primary users,
+//! but this library is intended to be useful to any application that wants to link the library for the purpose of rendering Graphite's render graphs.
+//! For example, games can link the library and render procedural textures with customizable parametric input values.
+
 #[cfg(test)]
 mod tests {
 	#[test]
