3acec76e4cf0e8cadaf85dac98ffac1204353c97
Keavon Chambers <keavon@keavon.com>
2021-07-31 04:48:32 -0700

    Add TextButton widget

--- client/web/src/components/window/title-bar/WindowButtonsWeb.vue
+++ client/web/src/components/window/title-bar/WindowButtonsWeb.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="window-buttons-web" @click="handleClick" :title="fullscreen.windowFullscreen ? 'Exit Fullscreen (F11)' : 'Enter Fullscreen (F11)'">
-		<TextLabel v-if="requestFullscreenHotkeys">Click to access all hotkeys</TextLabel>
+		<TextLabel v-if="requestFullscreenHotkeys" :italic="true">Click to access all hotkeys</TextLabel>
 		<IconLabel :icon="fullscreen.windowFullscreen ? 'FullscreenExit' : 'FullscreenEnter'" />
 	</div>
 </template>
@@ -13,8 +13,6 @@
 	fill: var(--color-e-nearwhite);
 
 	.text-label {
-		font-weight: normal;
-		font-style: italic;
 		margin-right: 8px;
 	}
 
