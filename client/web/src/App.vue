63a4ed64c869d4f7465af96e63ceb0055151231c
Keavon Chambers <keavon@keavon.com>
2021-08-04 12:39:18 -0700

    Refactor document tab management from Panel.vue into utilities/documents.ts

--- client/web/src/App.vue
+++ client/web/src/App.vue
@@ -217,7 +217,7 @@ img {
 <script lang="ts">
 import { defineComponent } from "vue";
 import dialog from "@/utilities/dialog";
-import document from "@/utilities/document";
+import documents from "@/utilities/documents";
 import fullscreen from "@/utilities/fullscreen";
 import MainWindow from "@/components/window/MainWindow.vue";
 import LayoutRow from "@/components/layout/LayoutRow.vue";
@@ -225,7 +225,7 @@ import LayoutRow from "@/components/layout/LayoutRow.vue";
 export default defineComponent({
 	provide: {
 		dialog,
-		document,
+		documents,
 		fullscreen,
 	},
 	data() {
