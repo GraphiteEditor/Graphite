3349ecd33645fedf8a899db0c180b324de0fc0e2
Keavon Chambers <keavon@keavon.com>
2021-08-13 20:44:53 -0700

    Improve Frontend -> Backend user input system
    Includes refactor that sends coordinates of the document viewports to the backend so input is sent relative to the application window
    Closes #124

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,26 @@
 import { createApp } from "vue";
 
 import { fullscreenModeChanged } from "@/utilities/fullscreen";
-import { handleKeyUp, handleKeyDown, handleMouseDown } from "@/utilities/input";
+import { onKeyUp, onKeyDown, onMouseMove, onMouseDown, onMouseUp, onMouseScroll, onWindowResize } from "@/utilities/input";
 import "@/utilities/errors";
 
 import App from "@/App.vue";
 
 // Bind global browser events
+window.addEventListener("resize", onWindowResize);
+window.addEventListener("DOMContentLoaded", onWindowResize);
+
 document.addEventListener("contextmenu", (e) => e.preventDefault());
 document.addEventListener("fullscreenchange", () => fullscreenModeChanged());
-window.addEventListener("keyup", (e: KeyboardEvent) => handleKeyUp(e));
-window.addEventListener("keydown", (e: KeyboardEvent) => handleKeyDown(e));
-window.addEventListener("mousedown", (e: MouseEvent) => handleMouseDown(e));
+
+window.addEventListener("keyup", onKeyUp);
+window.addEventListener("keydown", onKeyDown);
+
+window.addEventListener("mousemove", onMouseMove);
+window.addEventListener("mousedown", onMouseDown);
+window.addEventListener("mouseup", onMouseUp);
+
+window.addEventListener("wheel", onMouseScroll, { passive: false });
 
 // Initialize the Vue application
 createApp(App).mount("#app");
--- frontend/src/utilities/input.ts
+++ frontend/src/utilities/input.ts
@@ -3,7 +3,11 @@ import { dialogIsVisible, dismissDialog, submitDialog } from "@/utilities/dialog
 
 const wasm = import("@/../wasm/pkg");
 
-export function shouldRedirectKeyboardEventToBackend(e: KeyboardEvent): boolean {
+let viewportMouseInteractionOngoing = false;
+
+// Keyboard events
+
+function shouldRedirectKeyboardEventToBackend(e: KeyboardEvent): boolean {
 	// Don't redirect user input from text entry into HTML elements
 	const target = e.target as HTMLElement;
 	if (target.nodeName === "INPUT" || target.nodeName === "TEXTAREA" || target.isContentEditable) return false;
@@ -31,7 +35,7 @@ export function shouldRedirectKeyboardEventToBackend(e: KeyboardEvent): boolean 
 	return true;
 }
 
-export async function handleKeyDown(e: KeyboardEvent) {
+export async function onKeyDown(e: KeyboardEvent) {
 	if (shouldRedirectKeyboardEventToBackend(e)) {
 		e.preventDefault();
 		const modifiers = makeModifiersBitfield(e.ctrlKey, e.shiftKey, e.altKey);
@@ -48,7 +52,7 @@ export async function handleKeyDown(e: KeyboardEvent) {
 	}
 }
 
-export async function handleKeyUp(e: KeyboardEvent) {
+export async function onKeyUp(e: KeyboardEvent) {
 	if (shouldRedirectKeyboardEventToBackend(e)) {
 		e.preventDefault();
 		const modifiers = makeModifiersBitfield(e.ctrlKey, e.shiftKey, e.altKey);
@@ -56,16 +60,68 @@ export async function handleKeyUp(e: KeyboardEvent) {
 	}
 }
 
-export async function handleMouseDown(e: MouseEvent) {
+// Mouse events
+
+export async function onMouseMove(e: MouseEvent) {
+	if (!e.buttons) viewportMouseInteractionOngoing = false;
+
+	const modifiers = makeModifiersBitfield(e.ctrlKey, e.shiftKey, e.altKey);
+	(await wasm).on_mouse_move(e.clientX, e.clientY, e.buttons, modifiers);
+}
+
+export async function onMouseDown(e: MouseEvent) {
 	const target = e.target && (e.target as HTMLElement);
-	const clickedInsideDialog = target && target.closest(".dialog-modal .floating-menu-content");
+	const inCanvas = target && target.closest(".canvas");
+	const inDialog = target && target.closest(".dialog-modal .floating-menu-content");
 
-	if (dialogIsVisible() && !clickedInsideDialog) {
+	// Block middle mouse button auto-scroll mode
+	if (e.button === 1) e.preventDefault();
+
+	if (dialogIsVisible() && !inDialog) {
 		dismissDialog();
-
 		e.preventDefault();
 		e.stopPropagation();
 	}
+
+	if (inCanvas) viewportMouseInteractionOngoing = true;
+
+	if (viewportMouseInteractionOngoing) {
+		const modifiers = makeModifiersBitfield(e.ctrlKey, e.shiftKey, e.altKey);
+		(await wasm).on_mouse_down(e.clientX, e.clientY, e.buttons, modifiers);
+	}
+}
+
+export async function onMouseUp(e: MouseEvent) {
+	if (!e.buttons) viewportMouseInteractionOngoing = false;
+
+	const modifiers = makeModifiersBitfield(e.ctrlKey, e.shiftKey, e.altKey);
+	(await wasm).on_mouse_up(e.clientX, e.clientY, e.buttons, modifiers);
+}
+
+export async function onMouseScroll(e: WheelEvent) {
+	const target = e.target && (e.target as HTMLElement);
+	const inCanvas = target && target.closest(".canvas");
+
+	if (inCanvas) {
+		e.preventDefault();
+		const modifiers = makeModifiersBitfield(e.ctrlKey, e.shiftKey, e.altKey);
+		(await wasm).on_mouse_scroll(e.clientX, e.clientY, e.buttons, e.deltaX, e.deltaY, e.deltaZ, modifiers);
+	}
+}
+
+export async function onWindowResize() {
+	const viewports = Array.from(document.querySelectorAll(".canvas"));
+	const boundsOfViewports = viewports.map((canvas) => {
+		const bounds = canvas.getBoundingClientRect();
+		return [bounds.left, bounds.top, bounds.right, bounds.bottom];
+	});
+
+	const flattened = boundsOfViewports.flat();
+	const data = Float64Array.from(flattened);
+
+	if (!boundsOfViewports.length) return;
+
+	(await wasm).bounds_of_viewports(data);
 }
 
 export function makeModifiersBitfield(control: boolean, shift: boolean, alt: boolean): number {
