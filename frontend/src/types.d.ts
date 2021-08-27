d2395b4dcf68af1e9657a041ddb161dcb5633611
Keavon Chambers <keavon@keavon.com>
2021-08-26 19:20:38 -0700

    Improve web infrastructure
    Upgrade npm packages that were locked to old versions due to now-resolved problems
    Remove unused/unnecessary dependencies and their configurations
    Fix VS Code format on save
    Fix VS Code removing EOF newline in JSON files in conflict with npm
    Remove JSON files from ESLint because it doesn't properly support JSON
    Add detailed comments to web-related configuration files

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,8 +1,10 @@
+// Allow JS import statements to work with .vue files
 declare module "*.vue" {
 	const component: DefineComponent;
 	export default component;
 }
 
+// Allow JS import statements to work with .svg files
 declare module "*.svg" {
 	const component: DefineComponent;
 	export default component;
