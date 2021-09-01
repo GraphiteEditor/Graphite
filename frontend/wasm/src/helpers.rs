5675126a894366a22772c030053e60f345a16401
Keavon Chambers <keavon@keavon.com>
2021-09-01 06:10:05 -0700

    Reorganize and clean up the WASM wrapper

---
+++ frontend/wasm/src/helpers.rs
@@ -0,0 +1,24 @@
+use wasm_bindgen::prelude::*;
+
+// The JavaScript `Error` type
+#[wasm_bindgen]
+extern "C" {
+	#[derive(Clone, Debug)]
+	pub type Error;
+
+	#[wasm_bindgen(constructor)]
+	pub fn new(msg: &str) -> Error;
+}
+
+/// Takes a string and matches it to its equivalently-named enum variant (useful for simple type translations)
+macro_rules! match_string_to_enum {
+	(match ($e:expr) {$($var:ident),* $(,)?}) => {
+		match $e {
+			$(
+			stringify!($var) => Some($var),
+			)*
+			_ => None
+		}
+	};
+}
+pub(crate) use match_string_to_enum;
