0759c5edab9340ea7757eb9cbf8106150aa2ec1a
Keavon Chambers <keavon@keavon.com>
2021-11-29 01:46:16 -0800

    Set CI and CD to use Node.js version 16

--- .deployment/cd.sh
+++ .deployment/cd.sh
@@ -3,8 +3,12 @@
 echo 🔧 Install Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH=$PATH:/opt/buildhome/.cargo/bin
+rust --version
 
 echo 🚧 Install Node dependencies
+export NODE_VERSION=16
+node --version
+npm --version
 cd frontend
 npm install
 
