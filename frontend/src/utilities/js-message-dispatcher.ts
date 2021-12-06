a3bb9160a22611ade6ab75e05f481126817ab9a9
mfish33 <32677537+mfish33@users.noreply.github.com>
2021-12-05 19:14:16 -0800

    Change responses to use classes instead of interfaces (#394)
    * ability to mark an open document as unsaved
    
    * unsaved detection now being triggered based on layer tree height
    
    * Changed responses to use classes instead of interfaces
    
    * - rust implementation of unsaved markers
    - upgraded eslint
    
    * updated eslint in package.json
    
    * - Renamed GetOpenDocumentsList -> UpdateOpenDocumentsList
    - is not -> was not
    
    * changed hash to current identifier to better reflect its meaning
    
    * resolve some merge conflicts
    
    * removed console.log statement leftover from debuging
    
    * - changed Response to jsMessage
    - split files
    - Array<> -> []
    
    * -remove path from UpdateLayer
    
    * - remove unused if statements
    
    * - comment for reflect-metadata
    - registerJsMessageHandler -> subscribeJsMessage
    - readonly message properties
    - fixed binding filename and comment
    - toRgb -> toRgba
    
    * - newOpacity -> transformer
    - added comments
    
    * MessageMaker -> messageMaker

---
+++ frontend/src/utilities/js-message-dispatcher.ts
@@ -0,0 +1,95 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { reactive } from "vue";
+import { plainToInstance } from "class-transformer";
+
+import {
+	DisplayConfirmationToCloseAllDocuments,
+	DisplayConfirmationToCloseDocument,
+	DisplayError,
+	DisplayPanic,
+	ExportDocument,
+	newDisplayFolderTreeStructure,
+	OpenDocumentBrowse,
+	SaveDocument,
+	SetActiveDocument,
+	SetActiveTool,
+	SetCanvasRotation,
+	SetCanvasZoom,
+	UpdateCanvas,
+	UpdateOpenDocumentsList,
+	UpdateRulers,
+	UpdateScrollbars,
+	UpdateWorkingColors,
+	UpdateLayer,
+	JsMessage,
+} from "./js-messages";
+
+type JsMessageCallback<T extends JsMessage> = (responseData: T) => void;
+type JsMessageCallbackMap = {
+	[response: string]: JsMessageCallback<any> | undefined;
+};
+
+const state = reactive({
+	responseMap: {} as JsMessageCallbackMap,
+});
+
+type Constructs<T> = new (...args: any[]) => T;
+type ConstructsJsMessage = Constructs<JsMessage> & typeof JsMessage;
+
+const responseMap = {
+	UpdateCanvas,
+	UpdateScrollbars,
+	UpdateRulers,
+	ExportDocument,
+	SaveDocument,
+	OpenDocumentBrowse,
+	DisplayFolderTreeStructure: newDisplayFolderTreeStructure,
+	UpdateLayer,
+	SetActiveTool,
+	SetActiveDocument,
+	UpdateOpenDocumentsList,
+	UpdateWorkingColors,
+	SetCanvasZoom,
+	SetCanvasRotation,
+	DisplayError,
+	DisplayPanic,
+	DisplayConfirmationToCloseDocument,
+	DisplayConfirmationToCloseAllDocuments,
+} as const;
+
+export type JsMessageType = keyof typeof responseMap;
+
+function isJsMessageConstructor(fn: ConstructsJsMessage | ((data: any) => JsMessage)): fn is ConstructsJsMessage {
+	return (fn as ConstructsJsMessage).jsMessageMarker !== undefined;
+}
+
+export function handleJsMessage(responseType: JsMessageType, responseData: any) {
+	const messageMaker = responseMap[responseType];
+	let message: JsMessage;
+
+	if (!messageMaker) {
+		// eslint-disable-next-line no-console
+		console.error(`Received a Response of type "${responseType}" but but was not able to parse the data.`);
+	}
+
+	if (isJsMessageConstructor(messageMaker)) {
+		message = plainToInstance(messageMaker, responseData[responseType]);
+	} else {
+		message = messageMaker(responseData[responseType]);
+	}
+
+	// It is ok to use constructor.name even with minification since it is used consistently with registerHandler
+	const callback = state.responseMap[message.constructor.name];
+
+	if (callback && message) {
+		callback(message);
+	} else if (message) {
+		// eslint-disable-next-line no-console
+		console.error(`Received a Response of type "${responseType}" but no handler was registered for it from the client.`);
+	}
+}
+
+export function subscribeJsMessage<T extends JsMessage>(responseType: Constructs<T>, callback: JsMessageCallback<T>) {
+	state.responseMap[responseType.name] = callback;
+}
