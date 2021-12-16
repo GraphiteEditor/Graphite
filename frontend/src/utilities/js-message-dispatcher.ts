d07eab029a7f7fbd0377ff35fe8c7ca51f3f9d19
Keavon Chambers <keavon@keavon.com>
2021-12-16 02:18:45 -0800

    Clean up JS message dispatcher and fix a bug thrown on empty-data messages
    Fixes bug in #394

--- frontend/src/utilities/js-message-dispatcher.ts
+++ frontend/src/utilities/js-message-dispatcher.ts
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { reactive } from "vue";
 import { plainToInstance } from "class-transformer";
-
 import {
+	JsMessage,
 	DisplayConfirmationToCloseAllDocuments,
 	DisplayConfirmationToCloseDocument,
 	DisplayError,
 	DisplayPanic,
 	ExportDocument,
-	newDisplayFolderTreeStructure,
+	newDisplayFolderTreeStructure as DisplayFolderTreeStructure,
 	OpenDocumentBrowse,
 	SaveDocument,
 	SetActiveDocument,
@@ -22,29 +21,16 @@ import {
 	UpdateScrollbars,
 	UpdateWorkingColors,
 	UpdateLayer,
-	JsMessage,
-} from "./js-messages";
+} from "@/utilities/js-messages";
 
-type JsMessageCallback<T extends JsMessage> = (responseData: T) => void;
-type JsMessageCallbackMap = {
-	[response: string]: JsMessageCallback<any> | undefined;
-};
-
-const state = reactive({
-	responseMap: {} as JsMessageCallbackMap,
-});
-
-type Constructs<T> = new (...args: any[]) => T;
-type ConstructsJsMessage = Constructs<JsMessage> & typeof JsMessage;
-
-const responseMap = {
+const messageConstructors = {
 	UpdateCanvas,
 	UpdateScrollbars,
 	UpdateRulers,
 	ExportDocument,
 	SaveDocument,
 	OpenDocumentBrowse,
-	DisplayFolderTreeStructure: newDisplayFolderTreeStructure,
+	DisplayFolderTreeStructure,
 	UpdateLayer,
 	SetActiveTool,
 	SetActiveDocument,
@@ -57,39 +43,51 @@ const responseMap = {
 	DisplayConfirmationToCloseDocument,
 	DisplayConfirmationToCloseAllDocuments,
 } as const;
+type JsMessageType = keyof typeof messageConstructors;
 
-export type JsMessageType = keyof typeof responseMap;
+type JsMessageCallback<T extends JsMessage> = (messageData: T) => void;
+type JsMessageCallbackMap = {
+	[message: string]: JsMessageCallback<any> | undefined;
+};
 
-function isJsMessageConstructor(fn: ConstructsJsMessage | ((data: any) => JsMessage)): fn is ConstructsJsMessage {
-	return (fn as ConstructsJsMessage).jsMessageMarker !== undefined;
+type Constructs<T> = new (...args: any[]) => T;
+type ConstructsJsMessage = Constructs<JsMessage> & typeof JsMessage;
+
+const subscriptions = {} as JsMessageCallbackMap;
+
+export function subscribeJsMessage<T extends JsMessage>(messageType: Constructs<T>, callback: JsMessageCallback<T>) {
+	subscriptions[messageType.name] = callback;
 }
 
-export function handleJsMessage(responseType: JsMessageType, responseData: any) {
-	const messageMaker = responseMap[responseType];
-	let message: JsMessage;
-
-	if (!messageMaker) {
+export function handleJsMessage(messageType: JsMessageType, messageData: any) {
+	const messageConstructor = messageConstructors[messageType];
+	if (!messageConstructor) {
 		// eslint-disable-next-line no-console
-		console.error(`Received a Response of type "${responseType}" but but was not able to parse the data.`);
+		console.error(`Received a frontend message of type "${messageType}" but but was not able to parse the data.`);
+		return;
 	}
 
-	if (isJsMessageConstructor(messageMaker)) {
-		message = plainToInstance(messageMaker, responseData[responseType]);
+	// Messages with non-empty data are provided by wasm-bindgen as an object with one key as the message name, like: { NameOfThisMessage: { ... } }
+	// Messages with empty data are provided by wasm-bindgen as a string with the message name, like: "NameOfThisMessage"
+	const unwrappedMessageData = messageData[messageType] || {};
+
+	const isJsMessageConstructor = (fn: ConstructsJsMessage | ((data: any) => JsMessage)): fn is ConstructsJsMessage => {
+		return (fn as ConstructsJsMessage).jsMessageMarker !== undefined;
+	};
+	let message: JsMessage;
+	if (isJsMessageConstructor(messageConstructor)) {
+		message = plainToInstance(messageConstructor, unwrappedMessageData);
 	} else {
-		message = messageMaker(responseData[responseType]);
+		message = messageConstructor(unwrappedMessageData);
 	}
 
 	// It is ok to use constructor.name even with minification since it is used consistently with registerHandler
-	const callback = state.responseMap[message.constructor.name];
+	const callback = subscriptions[message.constructor.name];
 
 	if (callback && message) {
 		callback(message);
 	} else if (message) {
 		// eslint-disable-next-line no-console
-		console.error(`Received a Response of type "${responseType}" but no handler was registered for it from the client.`);
+		console.error(`Received a frontend message of type "${messageType}" but no handler was registered for it from the client.`);
 	}
-}
-
-export function subscribeJsMessage<T extends JsMessage>(responseType: Constructs<T>, callback: JsMessageCallback<T>) {
-	state.responseMap[responseType.name] = callback;
 }
