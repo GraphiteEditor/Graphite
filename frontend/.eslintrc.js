653fe0baca88a9d53b49936856278f467cc79efa
Keavon Chambers <keavon@keavon.com>
2021-12-19 00:37:55 -0800

    Fix eslint rule to whitelist @/../

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -65,7 +65,7 @@ module.exports = {
 		"no-bitwise": "off",
 		"no-shadow": "off",
 		"no-use-before-define": "off",
-		"no-restricted-imports": ["error", { patterns: [".*"] }],
+		"no-restricted-imports": ["error", { patterns: [".*", "!@/*"] }],
 
 		// TypeScript plugin config
 		"@typescript-eslint/indent": ["error", "tab", { SwitchCase: 1 }],
