75a0233b28e87d2b361b7e0a4088f6f31d70d946
0hypercube <0hypercube@gmail.com>
2021-12-27 11:13:48 +0000

    Cleanup maths

--- editor/src/document/movement_handler.rs
+++ editor/src/document/movement_handler.rs
@@ -158,10 +158,10 @@ impl MessageHandler<MovementMessage, (&mut LayerData, &Document, &InputPreproces
 				if ipp.mouse.scroll_delta.y > 0 {
 					zoom_factor = 1. / zoom_factor
 				};
-				let new_viewport_bounds = viewport_bounds * (1. / zoom_factor);
+				let new_viewport_bounds = viewport_bounds / zoom_factor;
 				let delta_size = viewport_bounds - new_viewport_bounds;
-				let mouse_percent = mouse / viewport_bounds;
-				let delta = (-delta_size) * (mouse_percent - DVec2::splat(0.5));
+				let mouse_fraction = mouse / viewport_bounds;
+				let delta = delta_size * (DVec2::splat(0.5) - mouse_fraction);
 
 				let transformed_delta = document.root.transform.inverse().transform_vector2(delta);
 				let new = (layerdata.scale * zoom_factor).clamp(VIEWPORT_ZOOM_SCALE_MIN, VIEWPORT_ZOOM_SCALE_MAX);
