7b5515a66d1f8d29eaef6d82c45d7f8405e9ce2b
0HyperCube <78500760+0HyperCube@users.noreply.github.com>
2021-12-29 04:32:44 +0000

    Populate layer entry cache (#437)
    * Populate layer entry cache
    
    * Serialize the DocumentMessageHandler
    
    * Fix restoring of collapsed/expanded state, add iter impl for Layer, and clean up layer_data() functions
    
    * Fixed bug with CreateEmptyLayer revealed by test
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>
    Co-authored-by: otdavies <oliver@psyfer.io>

---
+++ editor/src/document/vectorize_layerdata.rs
@@ -0,0 +1,25 @@
+/// Necessary because serde can't serialize hashmaps when the keys don't implement display.
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::iter::FromIterator;
+
+pub fn serialize<'a, T, K, V, S>(target: T, ser: S) -> Result<S::Ok, S::Error>
+where
+	S: Serializer,
+	T: IntoIterator<Item = (&'a K, &'a V)>,
+	K: Serialize + 'a,
+	V: Serialize + 'a,
+{
+	let container: Vec<_> = target.into_iter().collect();
+	serde::Serialize::serialize(&container, ser)
+}
+
+pub fn deserialize<'de, T, K, V, D>(des: D) -> Result<T, D::Error>
+where
+	D: Deserializer<'de>,
+	T: FromIterator<(K, V)>,
+	K: Deserialize<'de>,
+	V: Deserialize<'de>,
+{
+	let container: Vec<_> = serde::Deserialize::deserialize(des)?;
+	Ok(T::from_iter(container.into_iter()))
+}
