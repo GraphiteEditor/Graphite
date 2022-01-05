753485238a1515ddacc0ecab61b0e5fd56c200c1
otdavies <oliver@psyfer.io>
2022-01-04 18:11:38 -0800

    Revert "Make uuid generator thread local for tests"
    This reverts commit d68e3b9c4ebf639c4b2ecae43f5ad3d060dcb12b.

--- editor/src/communication/mod.rs
+++ editor/src/communication/mod.rs
@@ -6,14 +6,13 @@ use rand_chacha::{
 	rand_core::{RngCore, SeedableRng},
 	ChaCha20Rng,
 };
-use spin::{Mutex, MutexGuard};
+use spin::Mutex;
 
 pub use crate::input::InputPreprocessor;
 use std::{cell::Cell, collections::VecDeque};
 
 pub type ActionList = Vec<Vec<MessageDiscriminant>>;
 
-#[cfg(not(test))]
 static RNG: Mutex<Option<ChaCha20Rng>> = Mutex::new(None);
 
 // TODO: Add Send + Sync requirement
@@ -32,29 +31,19 @@ where
 
 thread_local! {
 	pub static UUID_SEED: Cell<Option<u64>> = Cell::new(None);
-	#[cfg(test)]
-	static LOCAL_RNG: Mutex<Option<ChaCha20Rng>> = Mutex::new(None);
 }
 
 pub fn set_uuid_seed(random_seed: u64) {
-	UUID_SEED.with(|seed| seed.set(Some(random_seed)));
+	UUID_SEED.with(|seed| seed.set(Some(random_seed)))
 }
 
 pub fn generate_uuid() -> u64 {
-	let init = |mut lock: MutexGuard<Option<ChaCha20Rng>>| {
-		if lock.is_none() {
-			UUID_SEED.with(|seed| {
-				let random_seed = seed.get().expect("random seed not set before editor was initialized");
-				*lock = Some(ChaCha20Rng::seed_from_u64(random_seed));
-			})
-		}
-		lock.as_mut().map(ChaCha20Rng::next_u64).unwrap()
-	};
-	(
-		#[cfg(test)]
-		LOCAL_RNG.with(|rng| init(rng.lock())),
-		#[cfg(not(test))]
-		init(RNG.lock()),
-	)
-		.0
+	let mut lock = RNG.lock();
+	if lock.is_none() {
+		UUID_SEED.with(|seed| {
+			let random_seed = seed.get().expect("random seed not set before editor was initialized");
+			*lock = Some(ChaCha20Rng::seed_from_u64(random_seed));
+		})
+	}
+	lock.as_mut().map(ChaCha20Rng::next_u64).unwrap()
 }
