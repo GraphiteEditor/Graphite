439418896a89ce74a023e93a274b49b81f931f91
mfish33 <32677537+mfish33@users.noreply.github.com>
2022-01-08 07:50:08 -0800

    Implement artboards and document version enforcement (#466)
    * - graphite document artboard implementation
    - autosave document load hitch fix
    - Autosave will delete saved files when graphite document version changes
    
    * formating
    
    * - top left 0,0
    - fixed hitch on first document
    - vue calls first render
    
    * Revert
    
    * Merge branch 'master' into artboards
    
    * Small bug fixes and code review tweaks
    
    Co-authored-by: Oliver Davies <oliver@psyfer.io>
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

---
+++ editor/src/document/artboard_message_handler.rs
@@ -0,0 +1,83 @@
+pub use crate::document::layer_panel::*;
+use crate::document::{DocumentMessage, LayerMetadata};
+use crate::input::InputPreprocessor;
+use crate::message_prelude::*;
+use glam::{DAffine2, DVec2};
+use graphene::color::Color;
+use graphene::document::Document as GrapheneDocument;
+use graphene::layers::style::{self, Fill, ViewMode};
+use graphene::Operation as DocumentOperation;
+use serde::{Deserialize, Serialize};
+use std::collections::VecDeque;
+
+#[impl_message(Message, DocumentMessage, Artboard)]
+#[derive(PartialEq, Clone, Debug, Serialize, Deserialize)]
+pub enum ArtboardMessage {
+	DispatchOperation(Box<DocumentOperation>),
+	AddArtboard { top: f64, left: f64, height: f64, width: f64 },
+	RenderArtboards,
+}
+
+impl From<DocumentOperation> for ArtboardMessage {
+	fn from(operation: DocumentOperation) -> Self {
+		Self::DispatchOperation(Box::new(operation))
+	}
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct ArtboardMessageHandler {
+	pub artboards_graphene_document: GrapheneDocument,
+	pub artboard_ids: Vec<LayerId>,
+}
+
+impl MessageHandler<ArtboardMessage, (&mut LayerMetadata, &GrapheneDocument, &InputPreprocessor)> for ArtboardMessageHandler {
+	fn process_action(&mut self, message: ArtboardMessage, _data: (&mut LayerMetadata, &GrapheneDocument, &InputPreprocessor), responses: &mut VecDeque<Message>) {
+		// let (layer_metadata, document, ipp) = data;
+		use ArtboardMessage::*;
+		match message {
+			DispatchOperation(operation) => match self.artboards_graphene_document.handle_operation(&operation) {
+				Ok(_) => (),
+				Err(e) => log::error!("Artboard Error: {:?}", e),
+			},
+			AddArtboard { top, left, height, width } => {
+				let artboard_id = generate_uuid();
+				self.artboard_ids.push(artboard_id);
+
+				responses.push_back(
+					ArtboardMessage::DispatchOperation(
+						DocumentOperation::AddRect {
+							path: vec![artboard_id],
+							insert_index: -1,
+							transform: DAffine2::from_scale_angle_translation(DVec2::new(height, width), 0., DVec2::new(top, left)).to_cols_array(),
+							style: style::PathStyle::new(None, Some(Fill::new(Color::WHITE))),
+						}
+						.into(),
+					)
+					.into(),
+				);
+			}
+			RenderArtboards => {}
+		}
+
+		// Render an infinite canvas if there are no artboards
+		if self.artboard_ids.is_empty() {
+			responses.push_back(
+				FrontendMessage::UpdateArtboards {
+					svg: r##"<rect width="100%" height="100%" fill="#ffffff" />"##.to_string(),
+				}
+				.into(),
+			)
+		} else {
+			responses.push_back(
+				FrontendMessage::UpdateArtboards {
+					svg: self.artboards_graphene_document.render_root(ViewMode::Normal),
+				}
+				.into(),
+			);
+		}
+	}
+
+	fn actions(&self) -> ActionList {
+		actions!(ArtBoardMessageDiscriminant;)
+	}
+}
