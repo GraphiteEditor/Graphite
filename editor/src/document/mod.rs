0991312ba009f6a1452a9892e7d5a248a22939ed
Keavon Chambers <keavon@keavon.com>
2022-01-12 11:44:49 -0800

    Rename document_message_handler.rs to portfolio_message_handler.rs

--- editor/src/document/mod.rs
+++ editor/src/document/mod.rs
@@ -1,9 +1,9 @@
 mod artboard_message_handler;
 mod document_file;
-mod document_message_handler;
 pub mod layer_panel;
 mod movement_handler;
 mod overlay_message_handler;
+mod portfolio_message_handler;
 mod transform_layer_handler;
 mod vectorize_layer_metadata;
 
@@ -13,11 +13,11 @@ pub use document_file::LayerMetadata;
 #[doc(inline)]
 pub use document_file::{AlignAggregate, AlignAxis, DocumentMessage, DocumentMessageDiscriminant, DocumentMessageHandler, FlipAxis, VectorManipulatorSegment, VectorManipulatorShape};
 #[doc(inline)]
-pub use document_message_handler::{Clipboard, DocumentsMessage, DocumentsMessageDiscriminant, DocumentsMessageHandler};
-#[doc(inline)]
 pub use movement_handler::{MovementMessage, MovementMessageDiscriminant};
 #[doc(inline)]
 pub use overlay_message_handler::{OverlayMessage, OverlayMessageDiscriminant};
+#[doc(inline)]
+pub use portfolio_message_handler::{Clipboard, DocumentsMessage, DocumentsMessageDiscriminant, DocumentsMessageHandler};
 
 #[doc(inline)]
 pub use artboard_message_handler::{ArtboardMessage, ArtboardMessageDiscriminant};
