1a4ab0733b0775dca412cb639064eac779453cd0
0HyperCube <78500760+0HyperCube@users.noreply.github.com>
2022-01-16 19:46:41 +0000

    Add cursors and hints to global viewport navigation (#487)
    * Add global nav cursors
    
    * Update hints on global navigation
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

--- editor/src/viewport_tools/tool_message_handler.rs
+++ editor/src/viewport_tools/tool_message_handler.rs
@@ -102,7 +102,10 @@ impl MessageHandler<ToolMessage, (&DocumentMessageHandler, &InputPreprocessorMes
 				update_working_colors(document_data, responses);
 			}
 			tool_message => {
-				let tool_type = message_to_tool_type(&tool_message);
+				let tool_type = match &tool_message {
+					UpdateCursor | UpdateHints => self.tool_state.tool_data.active_tool_type,
+					tool_message => message_to_tool_type(tool_message),
+				};
 				let document_data = &self.tool_state.document_tool_data;
 				let tool_data = &mut self.tool_state.tool_data;
 
