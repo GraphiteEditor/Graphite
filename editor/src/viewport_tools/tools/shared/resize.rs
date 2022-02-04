599a9d076bf2fa8bcc79e409b7d60d227fa96360
0HyperCube <78500760+0HyperCube@users.noreply.github.com>
2022-02-04 16:46:44 +0000

    Transformation cage (#502)
    * Render corners and edges of selection box
    
    * Refactor
    
    * Add drag detection
    
    * Implement the transform handles
    
    * Implement rotation by dragging <40px from bounds
    
    * Refine clustered handle behaviour
    
    * Add cursors
    
    * Add snap angle
    
    * Fix MMB drag whilst in select tool
    
    * Convert calculate_pivot into a seperate function
    
    * rename start_vec to start_offset
    
    * Fix typo
    
    * Remove Undo transaction on <10px mouse move
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

--- editor/src/viewport_tools/tools/shared/resize.rs
+++ editor/src/viewport_tools/tools/shared/resize.rs
@@ -18,7 +18,7 @@ pub struct Resize {
 impl Resize {
 	/// Starts a resize, assigning the snap targets and snapping the starting position.
 	pub fn start(&mut self, responses: &mut VecDeque<Message>, viewport_bounds: DVec2, document: &DocumentMessageHandler, mouse_position: DVec2) {
-		self.snap_handler.start_snap(document, document.visible_layers());
+		self.snap_handler.start_snap(document, document.visible_layers(), true, true);
 		self.drag_start = self.snap_handler.snap_position(responses, viewport_bounds, document, mouse_position);
 	}
 
