96b1e72668b637dac2e66df99354d670f644edfe
0hypercube <0hypercube@gmail.com>
2022-02-13 12:56:32 +0000

    Reduce cage rotate tolerance

--- editor/src/consts.rs
+++ editor/src/consts.rs
@@ -32,7 +32,7 @@ pub const SELECTION_DRAG_ANGLE: f64 = 90.;
 
 // Transformation cage
 pub const BOUNDS_SELECT_THRESHOLD: f64 = 10.;
-pub const BOUNDS_ROTATE_THRESHOLD: f64 = 40.;
+pub const BOUNDS_ROTATE_THRESHOLD: f64 = 20.;
 
 // Path tool
 pub const VECTOR_MANIPULATOR_ANCHOR_MARKER_SIZE: f64 = 5.;
