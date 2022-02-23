b7c6eea983123519fdc6231141415312e6aaab97
Dennis <dennis@kobert.dev>
2022-02-23 09:54:01 +0100

    Add function to turn handles into bez_paths

--- editor/src/viewport_tools/vector_editor/vector_shape.rs
+++ editor/src/viewport_tools/vector_editor/vector_shape.rs
@@ -175,11 +175,39 @@ impl VectorShape {
 		responses.push_back(
 			Operation::SetShapePathInViewport {
 				path: self.layer_path.clone(),
-				bez_path: BezPath::from_vec(edited_bez_path),
+				bez_path: self.to_bezpath(), //BezPath::from_vec(edited_bez_path),
 				transform: self.transform.to_cols_array(),
 			}
 			.into(),
 		);
+	}
+
+	/// Delete the selected point
+	/// A wrapper around move_point to handle mirror state / submit the changes
+	pub fn to_bezpath(&self) -> BezPath {
+		if self.anchors.is_empty() {
+			return BezPath::new();
+		}
+		let point_to_kurbo = |x: &VectorControlPoint| kurbo::Point::new(x.position.x, x.position.y);
+		let point = self.anchors[0].points[0].as_ref().unwrap().position;
+		let mut bez_path = vec![PathEl::MoveTo((point.x, point.y).into())];
+
+		for elements in self.anchors.windows(2) {
+			let first = &elements[0];
+			let second = &elements[1];
+			let new_segment = match [&first.points[2], &second.points[1], &second.points[0]] {
+				[None, None, Some(p)] => PathEl::LineTo(point_to_kurbo(p)),
+				[None, Some(a), Some(p)] => PathEl::QuadTo(point_to_kurbo(a), point_to_kurbo(p)),
+				[Some(a1), Some(a2), Some(p)] => PathEl::CurveTo(point_to_kurbo(a1), point_to_kurbo(a2), point_to_kurbo(p)),
+				_ => panic!("unexpected path found"),
+			};
+			bez_path.push(new_segment);
+		}
+		if self.closed {
+			bez_path.push(PathEl::ClosePath);
+		}
+		log::debug!("path: {:?}", bez_path);
+		BezPath::from_vec(bez_path)
 	}
 
 	/// Update the anchors and segments to match the kurbo shape
