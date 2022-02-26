92caf612531f6d210a0ccae8e7dd424a458377f3
Dennis <dennis@kobert.dev>
2022-02-26 18:30:35 +0100

    Fix red tinge in transparent parts

--- frontend/wasm/src/canvas.rs
+++ frontend/wasm/src/canvas.rs
@@ -277,7 +277,7 @@ impl RenderingContext {
 }
 
 fn draw(context: &WebGl2RenderingContext, vert_count: i32) {
-	context.clear_color(1.0, 0.0, 0.0, 0.0);
+	context.clear_color(0.0, 0.0, 0.0, 0.0);
 	//context.clear_color(1.0, 0.5, 1.0, 1.0);
 	context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
 	context.disable(WebGl2RenderingContext::DEPTH_TEST);
