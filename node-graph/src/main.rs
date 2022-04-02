e999d6610809108b8a34799a908c01f43a21ee97
Dennis <dennis@kobert.dev>
2022-04-02 11:00:45 +0200

    Add Fst- and SndNode to node graph

--- node-graph/src/main.rs
+++ node-graph/src/main.rs
@@ -39,25 +39,32 @@ impl<T: Node> AnyRef for T {
     }
 }
 
-trait DefaultNode: Default {
+pub trait DefaultNode: Default {
     fn default_node() -> ValueNode<Self> {
         ValueNode::new(Self::default())
     }
 }
 impl<T: std::default::Default> DefaultNode for T {}
 
-trait After: Sized {
+pub trait After: Sized {
     fn after<'a, First: Node>(&'a self, first: &'a First) -> ComposeNode<'a, First, Self> {
         ComposeNode::new(first, self)
     }
 }
 impl<Second: Node> After for Second {}
 
+pub trait DynamicInput {
+    fn set_kwarg_by_name(&mut self, name: &str, value: &dyn Any);
+    fn set_arg_by_index(&mut self, index: usize, value: &dyn Any);
+}
+
 fn main() {
     let int = IntNode::<32>;
     let add: u32 = AddNode::<u32>::default().eval((int.eval(&()), int.eval(&())));
     let fnode = FnNode::new(|(a, b): &(i32, i32)| a - b);
-    let sub = fnode.any(&("a", 2));
+    //let sub = fnode.any(&("a", 2));
+    let cache = CacheNode::new(&fnode);
+    let foo = cache.eval(&(2, 3));
 
     /*
     let curry: CurryNthArgNode<'_, _, _, u32, u32, 0> = CurryNthArgNode::new(&AddNode, &int);
@@ -65,5 +72,5 @@ fn main() {
     let n = ValueNode::new(10_u32);
     let curry: CurryNthArgNode<'_, _, _, u32, _, 0> = CurryNthArgNode::new(&composition, &n);
     */
-    println!("{}", sub)
+    println!("{}", foo)
 }
