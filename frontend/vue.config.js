45d43573a8ac35a49546470e16392e61901a1c98
0hypercube <0hypercube@gmail.com>
2022-04-16 21:25:34 +0100

    Fix svg importing

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -85,6 +85,9 @@ module.exports = {
 			.rule("svg")
 			.uses.clear()
 			.end()
+			// Required (since upgrading vue-cli) to stop default importing behaviours as documented in
+			/// https://webpack.js.org/configuration/module/#ruletype
+			.type("javascript/auto")
 			// Add vue-loader as a loader for Vue single-file components
 			// https://www.npmjs.com/package/vue-loader
 			.use("vue-loader")
