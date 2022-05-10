1cc5612969a016855e6a154ae1a6648aa95e2bde
Caleb Dennis <caleb.dennis429@gmail.com>
2022-05-10 12:39:40 -0700

    simpler alternate way to create a data structure which manages elemens by ID, the drawback is that elements can't be accessed by index, but it seems like we don't really need to access by index and by ID, we should choose one or the other and stick with it

--- graphene/src/layers/id_storage.rs
+++ graphene/src/layers/id_storage.rs
@@ -1,4 +1,5 @@
 use std::{
+	cell::RefCell,
 	collections::HashMap,
 	ops::{Deref, DerefMut},
 };
@@ -16,14 +17,10 @@ type ElementIndex = i64;
 
 // TODO: Default is a bit weird because Layer does not implement Default. but we should not care because the empty vec is the default
 pub struct UniqueElements<T> {
-	/// The IDs of the [Elements] contained within this
-	keys: Vec<ElementId>,
-	/// The data contained in this
-	values: Vec<T>,
 	/// The ID that will be assigned to the next element that is added to this
 	next_assignment_id: ElementId,
 	/// Map from element ids to array positions
-	id_to_index: HashMap<ElementId, ElementIndex>,
+	id_map: HashMap<ElementId, RefCell<T>>,
 }
 
 impl<T> UniqueElements<T> {
@@ -47,200 +44,36 @@ impl<T> UniqueElements<T> {
 	/// folder.add(shape_layer.into(), None, -1);
 	/// folder.add(folder_layer.into(), Some(123), 0);
 	/// ```
-	pub fn add(&mut self, element: T, id: Option<ElementId>, insert_index: isize) -> Option<ElementId> {
-		let mut insert_index = insert_index as ElementIndex;
-
-		// If the insert index is negative, it is relative to the end
-		if insert_index < 0 {
-			insert_index = self.values.len() as ElementIndex + insert_index as ElementIndex + 1;
+	pub fn insert(&mut self, element: T, id: Option<ElementId>) -> Option<ElementId> {
+		if let Some(new_id) = id {
+			self.next_assignment_id = new_id
 		}
-
-		if insert_index <= self.values.len() as ElementIndex && insert_index >= 0 {
-			if let Some(id) = id {
-				self.next_assignment_id = id;
-
-				// If the chosen ID is already used, return None
-				if self.id_to_index.contains_key(&self.next_assignment_id) {
-					return None;
-				}
-			}
-
-			self.values.insert(insert_index as usize, element);
-			self.keys.insert(insert_index as usize, self.next_assignment_id);
-			self.id_to_index.insert(self.next_assignment_id, insert_index);
-
-			// Linear probing for collision avoidance
-			while self.id_to_index.contains_key(&self.next_assignment_id) {
-				self.next_assignment_id += 1;
-			}
-
-			Some(self.next_assignment_id)
-		} else {
-			None
+		match self.id_map.insert(self.next_assignment_id, RefCell::new(element)) {
+			None => None,
+			Some(_) => Some(self.next_assignment_id),
 		}
-	}
-
-	/// Remove an element with a given element ID from the within this container.
-	/// This operation will fail if `id` is not present within this container.
-	///
-	/// # Example
-	/// ```
-	/// # use graphite_graphene::layers::UniqueElements;
-	/// let mut folder = PathStorage::default();
-	///
-	/// // Try to remove a layer that does not exist
-	/// assert!(folder.remove_layer(123).is_err());
-	///
-	/// // Add another folder to the folder
-	/// folder.add_layer(PathStorage::default().into(), Some(123), -1);
-	///
-	/// // Try to remove that folder again
-	/// assert!(folder.remove_layer(123).is_ok());
-	/// assert_eq!(folder.layers().len(), 0)
-	/// ```
-	pub fn remove(&mut self, id: ElementId) -> Result<(), DocumentError> {
-		let pos = self.position_of_element(id)?;
-		self.values.remove(pos);
-		self.keys.remove(pos);
-		self.id_to_index.remove(&id);
-		Ok(())
-	}
-
-	/// Returns a list of [ElementId]s in the within this container.
-	pub fn keys(&self) -> &[ElementId] {
-		self.keys.as_slice()
-	}
-
-	/// These can be replaced by implementing Deref I think
-	// /// Get references to all the [T]s in the within this container.
-	// pub fn values(&self) -> &[T] {
-	// 	self.values.as_slice()
-	// }
-
-	// /// Get mutable references to all the [T]s in the within this container.
-	// pub fn values_mut(&mut self) -> &mut [T] {
-	// 	self.values.as_mut_slice()
-	// }
-
-	/// Get a single element with a given element ID from the within this container.
-	pub fn by_id(&self, id: ElementId) -> Option<&T> {
-		let pos = self.position_of_element(id).ok()?;
-		Some(&self.values[pos])
-	}
-
-	/// Get a mutable reference to a single element with a given element ID from the within this container.
-	pub fn by_id_mut(&mut self, id: ElementId) -> Option<&mut T> {
-		let pos = self.position_of_element(id).ok()?;
-		Some(&mut self.values[pos])
-	}
-
-	/// Get an element based on its index
-	pub fn by_index(&self, index: usize) -> Option<&T> {
-		self.values.get(index)
-	}
-
-	/// Get a mutable element based on its index
-	pub fn by_index_mut(&mut self, index: usize) -> Option<&mut T> {
-		self.values.get_mut(index)
-	}
-
-	pub fn last(&self) -> Option<&T> {
-		self.values.last()
-	}
-
-	pub fn last_mut(&mut self) -> Option<&mut T> {
-		self.values.last_mut()
-	}
-
-	pub fn len(&self) -> usize {
-		self.values.len()
-	}
-
-	pub fn is_empty(&self) -> bool {
-		self.values.is_empty()
-	}
-
-	pub fn clear(&mut self) {
-		self.values.clear();
-		self.keys.clear();
-		self.id_to_index.clear();
-	}
-
-	pub fn enumerate(&self) -> impl Iterator<Item = (&ElementId, &T)> {
-		self.keys.iter().zip(self.values.iter())
-	}
-
-	pub fn enumerate_mut(&mut self) -> impl Iterator<Item = (&ElementId, &mut T)> {
-		self.keys.iter().zip(self.values.iter_mut())
-	}
-
-	/// Returns `true` if this contains an element with the given [ElementId].
-	///
-	/// # Example
-	/// ```
-	/// # use graphite_graphene::layers::UniqueElements;
-	/// let mut folder = UniqueElements::default();
-	///
-	/// // Search for an id that does not exist
-	/// assert!(!folder.contains(123));
-	///
-	/// // Add layer with the id "123" to the folder
-	/// folder.add_layer(UniqueElements::default().into(), Some(123), -1);
-	///
-	/// // Search for the id "123"
-	/// assert!(folder.contains(123));
-	/// ```
-	pub fn contains(&self, id: ElementId) -> bool {
-		self.id_to_index.contains_key(&id)
-	}
-
-	/// Tries to find the index of a layer with the given [ElementId] within the folder.
-	/// This operation will fail if no layer with a matching ID is present in the folder.
-	///
-	/// # Example
-	/// ```
-	/// # use graphite_graphene::layers::UniqueElements;
-	/// let mut folder = UniqueElements::default();
-	///
-	/// // Search for an id that does not exist
-	/// assert!(folder.position_of_element(123).is_err());
-	///
-	/// // Add layer with the id "123" to the folder
-	/// folder.add_layer(UniqueElements::default().into(), Some(123), -1);
-	/// folder.add_layer(UniqueElements::default().into(), Some(42), -1);
-	///
-	/// assert_eq!(folder.position_of_element(123), Ok(0));
-	/// assert_eq!(folder.position_of_element(42), Ok(1));
-	/// ```
-	pub fn position_of_element(&self, element_id: ElementId) -> Result<usize, DocumentError> {
-		if let Some(position) = self.id_to_index.get(&element_id) {
-			return Ok((*position) as usize);
-		}
-		Err(DocumentError::LayerNotFound([element_id].into()))
 	}
 }
 
 impl<T> Default for UniqueElements<T> {
 	fn default() -> Self {
 		UniqueElements {
-			keys: vec![],
-			values: vec![],
 			next_assignment_id: 0,
-			id_to_index: HashMap::new(),
+			id_map: HashMap::new(),
 		}
 	}
 }
 
 impl<T> Deref for UniqueElements<T> {
-	type Target = [T];
+	type Target = HashMap<ElementId, RefCell<T>>;
 	fn deref(&self) -> &Self::Target {
-		&self.values
+		&self.id_map
 	}
 }
 
 impl<T> DerefMut for UniqueElements<T> {
 	fn deref_mut(&mut self) -> &mut Self::Target {
-		&mut self.values
+		&mut self.id_map
 	}
 }
 
@@ -249,7 +82,7 @@ impl<T> DerefMut for UniqueElements<T> {
 impl<A> FromIterator<A> for UniqueElements<A> {
 	fn from_iter<T: IntoIterator<Item = A>>(iter: T) -> Self {
 		let mut new = UniqueElements::default();
-		iter.into_iter().for_each(|element| match new.add(element, None, -1) {
+		iter.into_iter().for_each(|element| match new.insert(element, None) {
 			_ => (), // attempt to add all elements, even if one fails
 		});
 		new
@@ -259,7 +92,7 @@ impl<A> FromIterator<A> for UniqueElements<A> {
 impl<'b, 'a: 'b, A: 'a + Clone> FromIterator<&'b A> for UniqueElements<A> {
 	fn from_iter<T: IntoIterator<Item = &'b A>>(iter: T) -> Self {
 		let mut new = UniqueElements::default();
-		iter.into_iter().for_each(|element| match new.add(element.clone(), None, -1) {
+		iter.into_iter().for_each(|element| match new.insert(element.clone(), None) {
 			_ => (), // attempt to add all elements, even if one fails
 		});
 		new
