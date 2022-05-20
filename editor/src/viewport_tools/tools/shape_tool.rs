bf8a5d6ce093624c565bf6a172b4845bc1716f1c
Thomas Cheng <contact.chengthomas@gmail.com>
2022-05-20 13:38:11 -0400

    Include unwrap for vertices input

--- editor/src/viewport_tools/tools/shape_tool.rs
+++ editor/src/viewport_tools/tools/shape_tool.rs
@@ -65,7 +65,7 @@ impl PropertyHolder for ShapeTool {
 				is_integer: true,
 				min: Some(3.),
 				max: Some(1000.),
-				on_update: WidgetCallback::new(|number_input: &NumberInput| ShapeToolMessage::UpdateOptions(ShapeOptionsUpdate::Vertices(number_input.value as u32)).into()),
+				on_update: WidgetCallback::new(|number_input: &NumberInput| ShapeToolMessage::UpdateOptions(ShapeOptionsUpdate::Vertices(number_input.value.unwrap() as u32)).into()),
 				..NumberInput::default()
 			}))],
 		}])
