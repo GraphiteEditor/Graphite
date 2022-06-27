24ed088ffbcb49d0668958da4e309a69bc74e618
Hannah Li <hannahli2010@gmail.com>
2022-06-27 19:33:56 -0400

    Bezier split and trim implementation (#680)
    * Added split implementation and UI
    
    Co-authored-by: Thomas Cheng <Androxium@users.noreply.github.com>
    
    * Added bezier split impl
    
    * Adjust struct traits
    
    * Implement trim and adjust FE code to handle multiple sliders per feature
    
    Co-authored-by: Thomas Cheng <contact.chengthomas@gmail.com>
    Co-authored-by: Rob Nadal <robnadal44@gmail.com>
    
    * Fix edge case in trim and add rust doc comments
    
    * Stylistic changes per review
    
    * More stylistic changes per review
    
    * replaced last explicit color usages
    
    Co-authored-by: Robert Nadal <Robnadal44@gmail.com>
    Co-authored-by: Thomas Cheng <Androxium@users.noreply.github.com>
    Co-authored-by: Thomas Cheng <contact.chengthomas@gmail.com>

--- bezier-rs/docs/interactive-docs/src/components/SliderExample.vue
+++ bezier-rs/docs/interactive-docs/src/components/SliderExample.vue
@@ -1,15 +1,17 @@
 <template>
 	<div>
-		<Example :title="title" :bezier="bezier" :callback="callback" :options="value.toString()" />
-		<div class="slider_label">{{ templateOptions.variable }} = {{ value }}</div>
-		<input class="slider" v-model="value" type="range" :step="templateOptions.step" :min="templateOptions.min" :max="templateOptions.max" />
+		<Example :title="title" :bezier="bezier" :callback="callback" :options="sliderData" />
+		<div v-for="(slider, index) in templateOptions.sliders" :key="index">
+			<div class="slider_label">{{ slider.variable }} = {{ sliderData[slider.variable] }}</div>
+			<input class="slider" v-model.number="sliderData[slider.variable]" type="range" :step="slider.step" :min="slider.min" :max="slider.max" />
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 
-import { BezierCallback } from "@/utils/types";
+import { BezierCallback, SliderOption } from "@/utils/types";
 import { WasmBezierInstance } from "@/utils/wasm-comm";
 
 import Example from "@/components/Example.vue";
@@ -35,8 +37,9 @@ export default defineComponent({
 		},
 	},
 	data() {
+		const sliders: SliderOption[] = this.templateOptions.sliders;
 		return {
-			value: this.templateOptions.default,
+			sliderData: Object.assign({}, ...sliders.map((s) => ({ [s.variable]: s.default }))),
 		};
 	},
 });
