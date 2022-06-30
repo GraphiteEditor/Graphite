2191b09f9f311313975fb0818ff5c6f53beef467
0HyperCube <78500760+0HyperCube@users.noreply.github.com>
2022-06-30 02:18:01 +0100

    Image and text bug fixes (#685)
    * Image and text bugfixes
    
    * Mark only the required layer types as dirty
    
    * Fix doctest
    
    * Disable selection if empty
    
    * Cleanup naming
    
    * Simplify cache deleting on export
    
    * Minor css style change
    
    * Nit
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

--- graphene/src/layers/folder_layer.rs
+++ graphene/src/layers/folder_layer.rs
@@ -1,5 +1,5 @@
 use super::layer_info::{Layer, LayerData, LayerDataType};
-use super::style::ViewMode;
+use super::style::RenderData;
 use crate::intersection::Quad;
 use crate::layers::text_layer::FontCache;
 use crate::{DocumentError, LayerId};
@@ -22,9 +22,9 @@ pub struct FolderLayer {
 }
 
 impl LayerData for FolderLayer {
-	fn render(&mut self, svg: &mut String, svg_defs: &mut String, transforms: &mut Vec<glam::DAffine2>, view_mode: ViewMode, font_cache: &FontCache, culling_bounds: Option<[DVec2; 2]>) {
+	fn render(&mut self, svg: &mut String, svg_defs: &mut String, transforms: &mut Vec<glam::DAffine2>, render_data: RenderData) {
 		for layer in &mut self.layers {
-			let _ = writeln!(svg, "{}", layer.render(transforms, view_mode, svg_defs, font_cache, culling_bounds));
+			let _ = writeln!(svg, "{}", layer.render(transforms, svg_defs, render_data));
 		}
 	}
 
