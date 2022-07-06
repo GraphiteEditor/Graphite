703c0fbaeb89b7638b08e90682e953a69d0a98db
Hannah Li <hannahli2010@gmail.com>
2022-07-06 18:00:02 -0400

    Update consts.rs comments

--- bezier-rs/lib/src/consts.rs
+++ bezier-rs/lib/src/consts.rs
@@ -1,19 +1,19 @@
 // Implementation constants
 
-/// Constants used to determine if `f64`'s are equivalent
+/// Constants used to determine if `f64`'s are equivalent.
 pub const MAX_ABSOLUTE_DIFFERENCE: f64 = 1e-3;
-/// Number of distances used in search algorithm for `project`
+/// Number of distances used in search algorithm for `project`.
 pub const NUM_DISTANCES: usize = 5;
-/// Constant for reduce
+/// Maximum allowed angle that the normal of the `start` or `end` point can make with the normal of the corresponding handle for a curve to be considered scalable/simple.
 pub const SCALABLE_CURVE_MAX_ENDPOINT_NORMAL_ANGLE: f64 = std::f64::consts::PI / 3.;
 
 // Method argument defaults
 
-/// Default `t` value used for the `curve_through_points` functions
+/// Default `t` value used for the `curve_through_points` functions.
 pub const DEFAULT_T_VALUE: f64 = 0.5;
-/// Default LUT step size in `compute_lookup_table` function
+/// Default LUT step size in `compute_lookup_table` function.
 pub const DEFAULT_LUT_STEP_SIZE: i32 = 10;
-/// Default number of subdivisions used in `length` calculation
+/// Default number of subdivisions used in `length` calculation.
 pub const DEFAULT_LENGTH_SUBDIVISIONS: i32 = 1000;
-/// Default step size for `reduce` function
+/// Default step size for `reduce` function.
 pub const DEFAULT_REDUCE_STEP_SIZE: f64 = 0.01;
