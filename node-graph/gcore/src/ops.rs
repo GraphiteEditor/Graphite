5ccde4509e8c68ebc251fcede37bc5c40c523966
Dennis <dennis@kobert.dev>
2022-08-13 14:54:12 +0200

    Implement cons node

--- node-graph/gcore/src/ops.rs
+++ node-graph/gcore/src/ops.rs
@@ -1,158 +1,83 @@
-use core::{marker::PhantomData, ops::Add};
+use core::ops::Add;
 
-use crate::{Node, NodeInput};
+use crate::Node;
 
-#[repr(C)]
-pub struct AddNode<'n, L: Add<R>, R, I1: Node<'n, Output = L>, I2: Node<'n, Output = R>>(pub I1, pub I2, PhantomData<&'n (L, R)>);
-impl<'n, L: Add<R>, R, I1: Node<'n, Output = L>, I2: Node<'n, Output = R>> Node<'n> for AddNode<'n, L, R, I1, I2> {
+pub struct AddNode;
+impl<'n, L: Add<R>, R> Node<'n, (L, R)> for AddNode {
 	type Output = <L as Add<R>>::Output;
-	fn eval(&'n self) -> Self::Output {
-		self.0.eval() + self.1.eval()
-	}
-}
-impl<'n, L: Add<R>, R, I1: Node<'n, Output = L>, I2: Node<'n, Output = R>> AddNode<'n, L, R, I1, I2> {
-	pub fn new(input: (I1, I2)) -> AddNode<'n, L, R, I1, I2> {
-		AddNode(input.0, input.1, PhantomData)
+	fn eval(&'n self, input: (L, R)) -> Self::Output {
+		input.0 + input.1
 	}
 }
 
-#[repr(C)]
-pub struct CloneNode<'n, N: Node<'n, Output = &'n O>, O: Clone + 'n>(pub N, PhantomData<&'n ()>);
-impl<'n, N: Node<'n, Output = &'n O>, O: Clone> Node<'n> for CloneNode<'n, N, O> {
+pub struct CloneNode;
+impl<'n, O: Clone> Node<'n, &'n O> for CloneNode {
 	type Output = O;
-	fn eval(&'n self) -> Self::Output {
-		self.0.eval().clone()
-	}
-}
-impl<'n, N: Node<'n, Output = &'n O>, O: Clone> CloneNode<'n, N, O> {
-	pub const fn new(node: N) -> CloneNode<'n, N, O> {
-		CloneNode(node, PhantomData)
+	fn eval(&'n self, input: &'n O) -> Self::Output {
+		input.clone()
 	}
 }
 
-#[repr(C)]
-pub struct FstNode<'n, N: Node<'n>>(pub N, PhantomData<&'n ()>);
-impl<'n, T: 'n, U, N: Node<'n, Output = (T, U)>> Node<'n> for FstNode<'n, N> {
+pub struct FstNode;
+impl<'n, T: 'n, U> Node<'n, (T, U)> for FstNode {
 	type Output = T;
-	fn eval(&'n self) -> Self::Output {
-		let (a, _) = self.0.eval();
+	fn eval(&'n self, input: (T, U)) -> Self::Output {
+		let (a, _) = input;
 		a
 	}
 }
 
-#[repr(C)]
 /// Destructures a Tuple of two values and returns the first one
-pub struct SndNode<'n, N: Node<'n>>(pub N, PhantomData<&'n ()>);
-impl<'n, T, U: 'n, N: Node<'n, Output = (T, U)>> Node<'n> for SndNode<'n, N> {
+pub struct SndNode;
+impl<'n, T, U: 'n> Node<'n, (T, U)> for SndNode {
 	type Output = U;
-	fn eval(&'n self) -> Self::Output {
-		let (_, b) = self.0.eval();
+	fn eval(&'n self, input: (T, U)) -> Self::Output {
+		let (_, b) = input;
 		b
 	}
 }
 
-#[repr(C)]
 /// Return a tuple with two instances of the input argument
-pub struct DupNode<'n, N: Node<'n>>(N, PhantomData<&'n ()>);
-impl<'n, N: Node<'n>> Node<'n> for DupNode<'n, N> {
-	type Output = (N::Output, N::Output);
-	fn eval(&'n self) -> Self::Output {
-		(self.0.eval(), self.0.eval()) //TODO: use Copy/Clone implementation
-	}
-}
-impl<'n, N: Node<'n>> NodeInput for DupNode<'n, N> {
-	type Nodes = N;
-
-	fn new(input: Self::Nodes) -> Self {
-		Self(input, PhantomData)
+pub struct DupNode;
+impl<'n, T: Clone> Node<'n, T> for DupNode {
+	type Output = (T, T);
+	fn eval(&'n self, input: T) -> Self::Output {
+		(input.clone(), input) //TODO: use Copy/Clone implementation
 	}
 }
 
-#[repr(C)]
 /// Return the Input Argument
-pub struct IdNode<'n, N: Node<'n>>(N, PhantomData<&'n ()>);
-impl<'n, N: Node<'n>> Node<'n> for IdNode<'n, N> {
-	type Output = N::Output;
-	fn eval(&'n self) -> Self::Output {
-		self.0.eval()
-	}
-}
-impl<'n, N: Node<'n>> NodeInput for IdNode<'n, N> {
-	type Nodes = N;
-
-	fn new(input: Self::Nodes) -> Self {
-		Self(input, PhantomData)
+pub struct IdNode;
+impl<'n, T> Node<'n, T> for IdNode {
+	type Output = T;
+	fn eval(&'n self, input: T) -> Self::Output {
+		input
 	}
 }
 
-pub fn foo() {
-	let unit = crate::value::UnitNode;
-	let value = IdNode(crate::value::ValueNode(2u32), PhantomData);
-	let value2 = crate::value::ValueNode(4u32);
-	let dup = DupNode(&value, PhantomData);
-	fn int(_: (), state: &u32) -> &u32 {
-		state
-	}
-	fn swap<'n>(input: (&'n u32, &'n u32)) -> (&'n u32, &'n u32) {
-		(input.1, input.0)
-	}
-	let fnn = crate::generic::FnNode::new(swap, &dup);
-	let fns = crate::generic::FnNodeWithState::new(int, &unit, 42u32);
-	let _ = fnn.eval();
-	let _ = fns.eval();
-	let snd = SndNode(&fnn, PhantomData);
-	let _ = snd.eval();
-	let add = AddNode(&snd, value2, PhantomData);
-	let _ = add.eval();
-}
-
-#[cfg(target_arch = "spirv")]
-pub mod gpu {
-	//#![deny(warnings)]
-	#[repr(C)]
-	pub struct PushConsts {
-		n: u32,
-		node: u32,
-	}
+#[cfg(test)]
+mod test {
 	use super::*;
-	use crate::{structural::ComposeNodeOwned, Node};
-	//use crate::Node;
-	use spirv_std::glam::UVec3;
-	const ADD: AddNode<u32> = AddNode(PhantomData);
-	const OPERATION: ComposeNodeOwned<'_, (u32, u32), u32, FstNode<u32, u32>, DupNode<u32>> = ComposeNodeOwned::new(FstNode(PhantomData, PhantomData), DupNode(PhantomData));
+	use crate::{generic::*, structural::*, value::*};
 
-	#[allow(unused)]
-	#[spirv(compute(threads(64)))]
-	pub fn spread(
-		#[spirv(global_invocation_id)] global_id: UVec3,
-		#[spirv(storage_buffer, descriptor_set = 0, binding = 0)] a: &[(u32, u32)],
-		#[spirv(storage_buffer, descriptor_set = 0, binding = 1)] y: &mut [(u32, u32)],
-		#[spirv(push_constant)] push_consts: &PushConsts,
-	) {
-		fn node_graph(input: Input) -> Output {
-			let n0 = ValueNode::new(input);
-			let n1 = IdNode::new(n0);
-			let n2 = IdNode::new(n1);
-			return n2.eval();
+	#[test]
+	pub fn foo() {
+		let value = ComposeNode::new(ValueNode(4u32), IdNode);
+		let value2 = ValueNode(5u32);
+		let dup = DupNode.after(value);
+		fn int(_: (), state: &u32) -> &u32 {
+			state
 		}
-		let gid = global_id.x as usize;
-		// Only process up to n, which is the length of the buffers.
-		if global_id.x < push_consts.n {
-			y[gid] = node_graph(a[gid]);
+		fn swap(input: (u32, u32)) -> (u32, u32) {
+			(input.1, input.0)
 		}
-	}
-	#[allow(unused)]
-	#[spirv(compute(threads(64)))]
-	pub fn add(
-		#[spirv(global_invocation_id)] global_id: UVec3,
-		#[spirv(storage_buffer, descriptor_set = 0, binding = 0)] a: &[(u32, u32)],
-		#[spirv(storage_buffer, descriptor_set = 0, binding = 1)] y: &mut [u32],
-		#[spirv(push_constant)] push_consts: &PushConsts,
-	) {
-		let gid = global_id.x as usize;
-		// Only process up to n, which is the length of the buffers.
-		if global_id.x < push_consts.n {
-			y[gid] = ADD.eval(a[gid]);
-		}
+		let fnn = FnNode::new(&swap);
+		let fns = FnNodeWithState::new(int, 42u32);
+		assert_eq!(fnn.eval((1u32, 2u32)), (2, 1));
+		let _ = fns.eval(());
+		let snd = SndNode.after(dup);
+		assert_eq!(snd.eval(()), &4u32);
+		let sum = AddNode.after(ConsNode(snd)).eval(value2.eval(()));
+		assert_eq!(sum, 9);
 	}
 }
--- node-graph/gcore/src/raster/color.rs
+++ node-graph/gcore/src/raster/color.rs
@@ -1,11 +1,11 @@
-// use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
 /// Structure that represents a color.
 /// Internally alpha is stored as `f32` that ranges from `0.0` (transparent) to `1.0` (opaque).
 /// The other components (RGB) are stored as `f32` that range from `0.0` up to `f32::MAX`,
 /// the values encode the brightness of each channel proportional to the light intensity in cd/m² (nits) in HDR, and `0.0` (black) to `1.0` (white) in SDR color.
 #[repr(C)]
-#[derive(Debug, Clone, Copy, PartialEq, Default)] //, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Default, Serialize, Deserialize)]
 pub struct Color {
 	red: f32,
 	green: f32,
