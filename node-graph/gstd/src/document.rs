5fbce0a5bd22c91d6b3921ef3ca7833155fb6bfb
Dennis <dennis@kobert.dev>
2022-10-04 20:18:00 +0200

    Add concept of construction arguments

--- node-graph/gstd/src/document.rs
+++ node-graph/gstd/src/document.rs
@@ -7,6 +7,14 @@ fn gen_node_id() -> NodeId {
 	todo!()
 }
 
+fn merge_ids(a: u64, b: u64) -> u64 {
+	use std::hash::{Hash, Hasher};
+	let mut hasher = std::collections::hash_map::DefaultHasher::new();
+	a.hash(&mut hasher);
+	b.hash(&mut hasher);
+	hasher.finish()
+}
+
 #[derive(Debug, Clone)]
 pub struct DocumentNode {
 	name: String,
@@ -40,18 +48,20 @@ pub enum DocumentNodeImplementation {
 pub struct NodeNetwork {
 	inputs: Vec<NodeId>,
 	output: NodeId,
-	edges: Vec<Connection>,
 	nodes: HashMap<NodeId, DocumentNode>,
 }
 
-#[derive(Debug, Clone)]
-pub struct Connection {
-	from: NodeId,
-	to: NodeId,
+#[derive(Debug, Clone, Default)]
+pub enum ConstructionArgs {
+	#[default]
+	None,
+	Value,
+	Nodes(Vec<NodeId>),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct ProtoNode {
+	construction_args: ConstructionArgs,
 	name: String,
 }
 
@@ -66,40 +76,45 @@ impl NodeInput {
 
 impl ProtoNode {
 	pub fn id() -> Self {
-		Self { name: "id".into() }
+		Self {
+			name: "id".into(),
+			..Default::default()
+		}
 	}
 	pub fn new(name: String) -> Self {
-		Self { name }
+		Self { name, ..Default::default() }
 	}
 }
 
 impl NodeNetwork {
 	pub fn map_ids(&mut self, f: impl Fn(NodeId) -> NodeId + Copy) {
 		self.inputs.iter_mut().for_each(|id| *id = f(*id));
-		self.edges.iter_mut().for_each(|conn| *conn = Connection { from: f(conn.from), to: f(conn.to) });
 		self.nodes = self
 			.nodes
 			.iter()
 			.map(|(id, node)| {
 				let mut node = node.clone();
 				node.inputs.iter_mut().for_each(|input| input.map_ids(f));
+				node.id = f(node.id);
 				(f(*id), node)
 			})
 			.collect();
 	}
 
 	/// Recursively dissolve non primitive document nodes and return a single flattened network of nodes.
-	pub fn flatten(&self, node: NodeId, network: &mut NodeNetwork, nodes: &mut HashMap<NodeId, DocumentNode>) {
-		let (id, mut node) = nodes.remove_entry(&node).unwrap();
+	pub fn flatten(&mut self, node: NodeId) {
+		let (id, mut node) = self.nodes.remove_entry(&node).unwrap();
 
 		match node.implementation {
 			DocumentNodeImplementation::Network(mut inner_network) => {
 				// Connect all network inputs to either the parent network nodes, or newly created value nodes.
-				inner_network.map_ids(|_| gen_node_id());
-				network.edges.extend(inner_network.edges);
-				for (document_input, network_input) in node.inputs.iter().zip(network.inputs.clone().iter()) {
+				inner_network.map_ids(|inner_id| merge_ids(id, inner_id));
+				for (document_input, network_input) in node.inputs.iter().zip(self.inputs.clone().iter()) {
 					match document_input {
-						NodeInput::Node(node) => network.edges.push(Connection { from: *node, to: *network_input }),
+						NodeInput::Node(node) => {
+							let network_input = self.nodes.get_mut(network_input).unwrap();
+							network_input.inputs.insert(0, NodeInput::Node(*node));
+						}
 						NodeInput::Value(widget) => {
 							let new_id = gen_node_id();
 							let value_node = DocumentNode {
@@ -108,31 +123,26 @@ impl NodeNetwork {
 								inputs: Vec::new(),
 								implementation: DocumentNodeImplementation::ProtoNode(ProtoNode::new(format!("{}-{}", node.name, widget))),
 							};
-							nodes.insert(new_id, value_node);
-
-							network.edges.push(Connection { from: new_id, to: *network_input });
+							self.nodes.insert(new_id, value_node);
+							let network_input = self.nodes.get_mut(network_input).unwrap();
+							network_input.inputs.push(NodeInput::Node(new_id));
 						}
 					}
 				}
+				let new_nodes = inner_network.nodes.keys().cloned().collect::<Vec<_>>();
 				// Copy nodes from the inner network into the parent network
-				for node_id in inner_network.nodes.keys().cloned().collect::<Vec<_>>() {
-					let (_old_id, mut node) = inner_network.nodes.remove_entry(&node_id).unwrap();
-					node.inputs = network
-						.edges
-						.iter()
-						.filter(|Connection { to, .. }| *to == node_id)
-						.map(|Connection { from, .. }| NodeInput::Node(*from))
-						.collect();
-					assert_eq!(node.inputs.len(), 1);
-					nodes.insert(node.id, node);
-				}
+				self.nodes.extend(inner_network.nodes);
 				node.implementation = DocumentNodeImplementation::ProtoNode(ProtoNode::id());
+				self.inputs = vec![inner_network.output];
+				for node_id in new_nodes {
+					self.flatten(node_id);
+				}
 			}
 			DocumentNodeImplementation::ProtoNode(proto_node) => {
 				node.implementation = DocumentNodeImplementation::ProtoNode(proto_node);
 			}
 		}
-		nodes.insert(id, node);
+		self.nodes.insert(id, node);
 	}
 }
 
@@ -150,97 +160,40 @@ impl Display for Map<i32, String> {
 	}
 }
 
-/*
-use core::marker::PhantomData;
-
-use graphene_core::{structural::After, structural::ComposeNode, value::ValueNode, Node, RefNode};
-
-use crate::any::Any;
-use crate::any::DynAnyNode;
-
-pub trait DocumentNode<I>: RefNode<I> {
-	fn input_hints(&self) -> &'static [&'static str];
-	fn input_types(&self) -> &'static [&'static str];
-	fn inputs(&self) -> Vec<String> {
-		self.input_hints().iter().zip(self.input_types()).map(|(a, b)| format!("{}{}", a, b)).collect()
-	}
-}
-
-struct InjectNode<N: Node<I> + Copy, I>(N, PhantomData<I>);
-
-impl<'n, N: Node<I> + Copy, I> Node<&'n [&'n AnyNode<'n>]> for &'n InjectNode<N, I> {
-	type Output = Box<dyn RefNode<Any<'n>, Output = Any<'n>> + 'n>;
-	fn eval(self, input: &'n [&'n AnyNode<'n>]) -> Self::Output {
-		assert_eq!(input.len(), 1);
-		Box::new(ComposeNode::new(&DynAnyNode(input[0]), &DynAnyNode(self.0)))
-	}
-}
-
-impl<N: Node<I> + Copy, I> InjectNode<N, I> {
-	const TYPES: &'static [&'static str] = &[core::any::type_name::<I>()];
-	const HINTS: &'static [&'static str] = &["input: "];
-}
-impl<'n, N: Node<I> + Copy, I> DocumentNode<&'n [&'n AnyNode<'n>]> for &'n InjectNode<N, I> {
-	fn input_hints(&self) -> &'static [&'static str] {
-		InjectNode::<N, I>::HINTS
-	}
-	fn input_types(&self) -> &'static [&'static str] {
-		InjectNode::<N, I>::TYPES
-	}
-}
-
-pub type NodeId = u32;
-
-type AnyNode<'n> = dyn RefNode<Any<'n>, Output = Any<'n>>;
-
-pub struct DocumentGraphNode<'n> {
-	pub id: NodeId,
-	pub inputs: Vec<NodeInput>,
-	pub node: NodeWrapper<'n>,
-}
-
-impl<'n> DocumentGraphNode<'n> {
-	pub fn new(id: NodeId, inputs: Vec<NodeInput>, node: NodeWrapper<'n>) -> Self {
-		Self { id, inputs, node }
-	}
-}
-
-pub struct NodeWrapper<'n> {
-	pub node: &'n (dyn Node<Any<'n>, Output = Any<'n>> + 'n),
-
-	pub path: &'static str,
-}
-
-impl<'n> NodeWrapper<'n> {
-	pub fn new(node: &'n (dyn Node<Any<'n>, Output = Any<'n>> + 'n), path: &'static str) -> Self {
-		Self { node, path }
-	}
-}
-
-pub enum NodeInput {
-	Node(NodeId),
-	Default(ValueNode<Any<'static>>),
-}
-
 #[cfg(test)]
 mod test {
-	use crate::any::DynAnyNode;
-
 	use super::*;
-	use graphene_core::value::ValueNode;
 
 	#[test]
-	fn inject_node() {
-		let inject_node = InjectNode(&ValueNode(4u32), PhantomData);
-		use super::DocumentNode;
-		/*assert_eq!(
-			(&inject_node as &dyn DocumentNode<&[&AnyNode], Output = ComposeNode<&AnyNode, ValueNode<u32>, ()>>).inputs(),
-			vec!["input: ()"]
-		);*/
-		let any_inject = DynAnyNode(&inject_node, PhantomData);
-		let any_inject = Box::leak(Box::new(any_inject));
-		let wrapped = NodeWrapper::new((&any_inject) as &(dyn Node<&[&AnyNode], Output = Any>), "grahpene_core::document::InjectNode");
-		let document_node = DocumentGraphNode::new(0, vec![], wrapped);
+	fn map_ids() {
+		let mut network = NodeNetwork {
+			inputs: vec![0],
+			output: 1,
+			nodes: [
+				(
+					0,
+					DocumentNode {
+						name: "cons".into(),
+						id: 0,
+						inputs: vec![NodeInput::Value(InputWidget)],
+						implementation: DocumentNodeImplementation::ProtoNode(ProtoNode::new("cons".into())),
+					},
+				),
+				(
+					1,
+					DocumentNode {
+						name: "add".into(),
+						id: 1,
+						inputs: vec![NodeInput::Node(0)],
+						implementation: DocumentNodeImplementation::ProtoNode(ProtoNode::new("add".into())),
+					},
+				),
+			]
+			.iter()
+			.cloned()
+			.collect(),
+		};
+		network.map_ids(|id| id + 1);
+		panic!("{:#?}", network);
 	}
 }
-*/
