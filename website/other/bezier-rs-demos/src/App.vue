b3e9f655bd02ab076bd1803b2e4d009da4f82a57
Rob Nadal <Robnadal44@gmail.com>
2022-12-21 14:44:11 -0800

    Bezier-rs: Added function to find intersections between a bezier and a rectangle (#897)
    * added function to find intersections for a rectangle
    
    * Added is_contained_within function

--- website/other/bezier-rs-demos/src/App.vue
+++ website/other/bezier-rs-demos/src/App.vue
@@ -555,6 +555,10 @@ export default defineComponent({
 					},
 				},
 				{
+					name: "Intersect (Rectangle)",
+					callback: (bezier: WasmBezierInstance): string => bezier.intersect_rectangle([[50, 50], [150, 150]]),
+				},
+				{
 					name: "Rotate",
 					callback: (bezier: WasmBezierInstance, options: Record<string, number>): string => bezier.rotate(options.angle * Math.PI, 100, 100),
 					exampleOptions: {
