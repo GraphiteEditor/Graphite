22c3d06167b54d836e2dc3b688d4fda54f2c754c
Dennis Kobert <dennis@kobert.dev>
2022-12-31 17:22:11 +0100

    Address review comments

--- node-graph/gstd/src/quantization.rs
+++ node-graph/gstd/src/quantization.rs
@@ -1,47 +1,18 @@
 use graphene_core::raster::{Color, Image};
 use graphene_core::Node;
 
-pub struct GenerateQuantizationNode<N: Node<(), Output = u32>, M: Node<(), Output = u32>>(N, M);
-
-impl<N: Node<(), Output = u32>, M: Node<(), Output = u32>> Node<Image> for GenerateQuantizationNode<N, M> {
-	type Output = Image;
-
-	fn eval(self, input: Image) -> Self::Output {
-		let samples = self.0.eval(());
-		let i = self.1.eval(());
-		generate_quantization_fn(samples, i, input)
-	}
+pub struct GenerateQuantizationNode<N: Node<(), Output = u32>, M: Node<(), Output = u32>> {
+	samples: N,
+	function: M,
 }
 
-impl<N: Node<(), Output = u32> + Copy, M: Node<(), Output = u32> + Copy> Node<Image> for &GenerateQuantizationNode<N, M> {
-	type Output = Image;
-
-	fn eval(self, input: Image) -> Self::Output {
-		let samples = self.0.eval(());
-		let i = self.1.eval(());
-		generate_quantization_fn(samples, i, input)
-	}
-}
-
-impl<N: Node<(), Output = u32>, M: Node<(), Output = u32>> GenerateQuantizationNode<N, M> {
-	pub fn new(node: N, index: M) -> Self {
-		Self(node, index)
-	}
-}
-
-fn generate_quantization_fn(samples: u32, function: u32, input: Image) -> Image {
+#[node_macro::node_fn(GenerateQuantizationNode)]
+fn generate_quantization_fn(image: Image, samples: u32, function: u32) -> Image {
+	// Scale the input image, this can be removed by adding an extra parameter to the fit function.
 	let max_energy = 16380.;
-	let data: Vec<f64> = input
-		.data
-		.iter()
-		.map(|x| vec![x.r() as f64, x.g() as f64, x.b() as f64])
-		.reduce(|mut acc, x| {
-			acc.extend_from_slice(&x);
-			acc
-		})
-		.unwrap_or_default();
+	let data: Vec<f64> = image.data.iter().flat_map(|x| vec![x.r() as f64, x.g() as f64, x.b() as f64]).collect();
 	let data: Vec<f64> = data.iter().map(|x| x * max_energy).collect();
-	let mut dist = autoquant::integrate_distribution(data.clone());
+	let mut dist = autoquant::integrate_distribution(data);
 	autoquant::drop_duplicates(&mut dist);
 	let dist = autoquant::normalize_distribution(dist.as_slice());
 	let max = dist.last().unwrap().0;
@@ -50,34 +21,21 @@ fn generate_quantization_fn(samples: u32, function: u32, input: Image) -> Image 
 		inverse: move |x| x * max,
 		name: "identity",
 	});
-	//log::info!("dist: {:?}", dist);
-
-	//let power = autoquant::models::VarPro::<autoquant::models::PowerTwo>::new(dist.clone());
-	//let log = autoquant::models::VarPro::<autoquant::models::PowerTwo>::new(dist);
-	//let log = autoquant::models::OptimizedLog::new(dist.clone(), 200);
-	//log::debug!("log: {:?}", log);
-	//let functions = autoquant::fit_functions(dist);
 	let best = match function {
 		0 => linear as Box<dyn autoquant::FitFn>,
 		1 => linear as Box<dyn autoquant::FitFn>,
-		2 => Box::new(autoquant::models::OptimizedLog::new(dist.clone(), 20)) as Box<dyn autoquant::FitFn>,
+		2 => Box::new(autoquant::models::OptimizedLog::new(dist, 20)) as Box<dyn autoquant::FitFn>,
 		_ => linear as Box<dyn autoquant::FitFn>,
 	};
-	let best = (0, &best);
-	/*
-			.into_iter()
-			.map(|f| (autoquant::calculate_sampled_error(&data, f.as_ref(), samples), f))
-			.min_by(|a, b| a.0.partial_cmp(&b.0).unwrap())
-			.unwrap();
-	*/
+
 	let roundtrip = |sample: f32| -> f32 {
-		let encoded = autoquant::encode(sample as f64 * max_energy, best.1.as_ref(), samples);
-		let decoded = autoquant::decode(encoded, best.1.as_ref(), samples) / max_energy;
+		let encoded = autoquant::encode(sample as f64 * max_energy, best.as_ref(), samples);
+		let decoded = autoquant::decode(encoded, best.as_ref(), samples) / max_energy;
 		log::trace!("{} enc: {} dec: {}", sample, encoded, decoded);
 		decoded as f32
 	};
 
-	let new_data = input
+	let new_data = image
 		.data
 		.iter()
 		.map(|c| {
@@ -89,5 +47,5 @@ fn generate_quantization_fn(samples: u32, function: u32, input: Image) -> Image 
 			Color::from_rgbaf32_unchecked(r, g, b, a)
 		})
 		.collect();
-	Image { data: new_data, ..input }
+	Image { data: new_data, ..image }
 }
