a205d86ae65d6fb3b6feed94854231fde08b9be4
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-03 15:26:45 +0000

    Make `tauri build` just work

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -66,7 +66,9 @@ async fn main() {
 	tauri::Builder::default()
 		.invoke_handler(tauri::generate_handler![set_random_seed, handle_message])
 		.setup(|app| {
-			app.get_window("main").unwrap().open_devtools();
+			if cfg!(debug) {
+				app.get_window("main").unwrap().open_devtools();
+			}
 			Ok(())
 		})
 		.run(tauri::generate_context!())
