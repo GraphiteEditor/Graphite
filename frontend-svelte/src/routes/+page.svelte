e09b159117330e70e3e41aee9e1bce2c51fc054e
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-04 21:14:37 +0000

    Move wasm-communication into seperate npm package

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+import { JsEditorHandle } from "$lib/shim"
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
--- frontend-svelte/vite.config.js
+++ frontend-svelte/vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+            wasm(),
+            topLevelAwait(),
+            sveltekit()
+	]
 };
 
 export default config;
