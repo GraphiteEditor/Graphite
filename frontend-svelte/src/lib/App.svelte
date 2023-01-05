db7eea07ca682af842f4b1863428f5272bd2749f
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-05 08:26:54 +0000

    translate LayoutCol.vue

--- frontend-svelte/src/lib/App.svelte
+++ frontend-svelte/src/lib/App.svelte
@@ -3,16 +3,16 @@ export type ApplicationPlatform = "Windows" | "Mac" | "Linux" | "Web"
 </script>
 
 <script lang="ts">
-import LayoutCol from "$lib/c/layout/LayoutCol.svelte"
-import Workspace from "$lib/c/window/workspace/Workspace.svelte"
-import TitleBar from "$lib/c/window/title-bar/TitleBar.svelte"
-import StatusBar from "$lib/c/window/status-bar/StatusBar.svelte"
+import LayoutCol from "$lib/layout/LayoutCol.svelte"
+// import Workspace from "$lib/window/workspace/Workspace.svelte"
+// import TitleBar from "$lib/window/title-bar/TitleBar.svelte"
+// import StatusBar from "$lib/window/status-bar/StatusBar.svelte"
 </script>
 
 <LayoutCol>
-  <TitleBar platform="Web" maximized="{true}" />
+  <!-- <TitleBar platform="Web" maximized="{true}" /> -->
 
-  <Workspace />
+  <!-- <Workspace />
 
-  <StatusBar />
+  <StatusBar /> -->
 </LayoutCol>
---
+++ frontend-svelte/src/lib/layout/LayoutCol.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+export let tooltip: string | undefined = undefined
+
+export let scrollableX: boolean = false
+export let scrollableY: boolean = false
+</script>
+
+<div
+  class="layout-col"
+  class:scrollable-x="{scrollableX}"
+  class:scrollable-y="{scrollableY}"
+  title="{tooltip}"
+>
+  <slot />
+</div>
+
+<style lang="scss">
+.layout-col {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+
+  .spacer {
+    flex: 1 1 100%;
+  }
+}
+</style>
