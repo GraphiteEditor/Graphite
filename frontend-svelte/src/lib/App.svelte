91502007e78973ce38396e66d88023000bf0883b
Keavon Chambers <keavon@keavon.com>
2023-01-05 03:56:51 -0800

    WIP port TitleBar

--- frontend-svelte/src/lib/App.svelte
+++ frontend-svelte/src/lib/App.svelte
@@ -1,18 +1,18 @@
 <script lang="ts" context="module">
-export type ApplicationPlatform = "Windows" | "Mac" | "Linux" | "Web"
+  export type ApplicationPlatform = "Windows" | "Mac" | "Linux" | "Web";
 </script>
 
 <script lang="ts">
-import LayoutCol from "$lib/layout/LayoutCol.svelte"
-// import Workspace from "$lib/panel/Workspace.svelte"
-// import TitleBar from "$lib/panel/TitleBar.svelte"
-// import StatusBar from "$lib/panel/StatusBar.svelte"
+  import LayoutCol from "$lib/layout/LayoutCol.svelte";
+  // import Workspace from "$lib/panel/Workspace.svelte"
+  import TitleBar from "$lib/panel/TitleBar.svelte";
+  // import StatusBar from "$lib/panel/StatusBar.svelte"
 </script>
 
 <LayoutCol>
-  <!-- <TitleBar platform="Web" maximized="{true}" /> -->
+  <TitleBar platform="Web" maximized={true} />
 
   <!-- <Workspace /> -->
-  
+
   <!-- <StatusBar /> -->
 </LayoutCol>
