4f4d68051d3bfb113fc833ae9f685f2f8b1939f3
Hannah Li <hannahli2010@gmail.com>
2023-01-06 15:16:28 -0500

    Indent with tabs

--- website/other/bezier-rs-demos/src/components/BezierExample.ts
+++ website/other/bezier-rs-demos/src/components/BezierExample.ts
@@ -13,118 +13,117 @@ const MANIPULATOR_KEYS_FROM_BEZIER_TYPE: { [key in BezierCurveType]: WasmBezierM
 };
 
 class BezierExample extends HTMLElement {
+	// Props
+	title!: string;
 
-    // Props
-    title!: string;
+	points!: number[][];
 
-    points!: number[][];
+	name!: keyof typeof bezierFeatures;
 
-    name!: keyof typeof bezierFeatures;
-    
-    sliderOptions!: SliderOption[];
-    
-    triggerOnMouseMove!: boolean;
+	sliderOptions!: SliderOption[];
 
-    computeTypeChoice!: ComputeType;
+	triggerOnMouseMove!: boolean;
 
-    callback!: BezierCallback;
+	computeTypeChoice!: ComputeType;
 
-    // Data
-    bezier!: WasmBezier;
+	callback!: BezierCallback;
 
-    manipulatorKeys!: WasmBezierManipulatorKey[];
+	// Data
+	bezier!: WasmBezier;
 
-    activeIndex!: number | undefined;
+	manipulatorKeys!: WasmBezierManipulatorKey[];
 
-    mutablePoints!: number[][];
+	activeIndex!: number | undefined;
 
-    sliderData: any;
-    
-    sliderUnits?: string | string[];
+	mutablePoints!: number[][];
 
-    static get observedAttributes(): string[] {
-        return ["title", "points", "name", "sliderOptions", "triggerOnMouseMove", "computeType"];
-    }
+	sliderData: any;
 
-    connectedCallback(): void {
-        this.title = this.getAttribute("title") || "";
-        this.points = JSON.parse(this.getAttribute("points") || "")
-        this.name = this.getAttribute("name") as keyof typeof bezierFeatures;
-        this.sliderOptions = JSON.parse(this.getAttribute("sliderOptions") || "")
-        this.triggerOnMouseMove = this.getAttribute("triggerOnMouseMove") === "true";
-        this.computeTypeChoice = (this.getAttribute("computeTypeChoice") || "Parametric") as ComputeType;
-        
-        this.callback = bezierFeatures[this.name].callback as BezierCallback
-        const curveType = getCurveType(this.points.length);
-        
+	sliderUnits?: string | string[];
+
+	static get observedAttributes(): string[] {
+		return ["title", "points", "name", "sliderOptions", "triggerOnMouseMove", "computeType"];
+	}
+
+	connectedCallback(): void {
+		this.title = this.getAttribute("title") || "";
+		this.points = JSON.parse(this.getAttribute("points") || "");
+		this.name = this.getAttribute("name") as keyof typeof bezierFeatures;
+		this.sliderOptions = JSON.parse(this.getAttribute("sliderOptions") || "");
+		this.triggerOnMouseMove = this.getAttribute("triggerOnMouseMove") === "true";
+		this.computeTypeChoice = (this.getAttribute("computeTypeChoice") || "Parametric") as ComputeType;
+
+		this.callback = bezierFeatures[this.name].callback as BezierCallback;
+		const curveType = getCurveType(this.points.length);
+
 		this.manipulatorKeys = MANIPULATOR_KEYS_FROM_BEZIER_TYPE[curveType];
 		this.bezier = WasmBezier[getConstructorKey(curveType)](this.points);
 		this.sliderData = Object.assign({}, ...this.sliderOptions.map((s) => ({ [s.variable]: s.default })));
-        this.activeIndex = undefined as number | undefined;
-        this.mutablePoints = JSON.parse(JSON.stringify(this.points));
-        this.sliderUnits = Object.assign({}, ...this.sliderOptions.map((s) => ({ [s.variable]: s.unit })));
+		this.activeIndex = undefined as number | undefined;
+		this.mutablePoints = JSON.parse(JSON.stringify(this.points));
+		this.sliderUnits = Object.assign({}, ...this.sliderOptions.map((s) => ({ [s.variable]: s.unit })));
 
-        this.onMouseDown.bind(this);
-        this.onMouseUp.bind(this);
-        this.onMouseMove.bind(this);
-        this.render();
+		this.onMouseDown.bind(this);
+		this.onMouseUp.bind(this);
+		this.onMouseMove.bind(this);
+		this.render();
 
-        const figure = this.querySelector("figure") as HTMLElement;
-        figure.innerHTML = this.callback(this.bezier, this.sliderData, undefined, this.computeTypeChoice)
-    }
+		const figure = this.querySelector("figure") as HTMLElement;
+		figure.innerHTML = this.callback(this.bezier, this.sliderData, undefined, this.computeTypeChoice);
+	}
 
-    //   attributeChangedCallback(name, oldValue, newValue) {
-    //       this.render();
-    //   }
+	//   attributeChangedCallback(name, oldValue, newValue) {
+	//       this.render();
+	//   }
 
-    render(): void {
-        const header = document.createElement("h4");
-        header.className = "example-header";
-        header.innerText = this.title;
-        
-        const figure = document.createElement("figure");
-        figure.className = "example-figure"
-        figure.addEventListener("mousedown", this.onMouseDown.bind(this))
-        figure.addEventListener("mouseup", this.onMouseUp.bind(this))
-        figure.addEventListener("mousemove", this.onMouseMove.bind(this))
-        
-        this.append(header);
-        this.append(figure);
-    }
-    
-    onMouseDown(event: MouseEvent): void {
-        const mx = event.offsetX;
-        const my = event.offsetY;
-        for (let pointIndex = 0; pointIndex < this.points.length; pointIndex += 1) {
-            const point = this.mutablePoints[pointIndex];
-            if (point && Math.abs(mx - point[0]) < SELECTABLE_RANGE && Math.abs(my - point[1]) < SELECTABLE_RANGE) {
-                this.activeIndex = pointIndex;
-                return;
-            }
-        }
-    }
+	render(): void {
+		const header = document.createElement("h4");
+		header.className = "example-header";
+		header.innerText = this.title;
 
-    onMouseUp(): void {
-        this.activeIndex = undefined;
-    }
+		const figure = document.createElement("figure");
+		figure.className = "example-figure";
+		figure.addEventListener("mousedown", this.onMouseDown.bind(this));
+		figure.addEventListener("mouseup", this.onMouseUp.bind(this));
+		figure.addEventListener("mousemove", this.onMouseMove.bind(this));
 
-    onMouseMove(event: MouseEvent): void {
-        const mx = event.offsetX;
-        const my = event.offsetY;
-        const figure = event.currentTarget as HTMLElement;
+		this.append(header);
+		this.append(figure);
+	}
 
-        if (this.activeIndex !== undefined) {
-            this.bezier[this.manipulatorKeys[this.activeIndex]](mx, my);
-            this.mutablePoints[this.activeIndex] = [mx, my];
-            figure.innerHTML = this.callback(this.bezier, this.sliderData, undefined, this.computeTypeChoice);
-        } else if (this.triggerOnMouseMove) {
-            figure.innerHTML = this.callback(this.bezier, this.sliderData, [mx, my], this.computeTypeChoice)
-        }
-    }
+	onMouseDown(event: MouseEvent): void {
+		const mx = event.offsetX;
+		const my = event.offsetY;
+		for (let pointIndex = 0; pointIndex < this.points.length; pointIndex += 1) {
+			const point = this.mutablePoints[pointIndex];
+			if (point && Math.abs(mx - point[0]) < SELECTABLE_RANGE && Math.abs(my - point[1]) < SELECTABLE_RANGE) {
+				this.activeIndex = pointIndex;
+				return;
+			}
+		}
+	}
 
-    static getSliderValue (sliderValue: number, sliderUnit?: string | string[]): string | undefined {
-        return Array.isArray(sliderUnit) ? sliderUnit[sliderValue] : sliderUnit
-    }
+	onMouseUp(): void {
+		this.activeIndex = undefined;
+	}
+
+	onMouseMove(event: MouseEvent): void {
+		const mx = event.offsetX;
+		const my = event.offsetY;
+		const figure = event.currentTarget as HTMLElement;
+
+		if (this.activeIndex !== undefined) {
+			this.bezier[this.manipulatorKeys[this.activeIndex]](mx, my);
+			this.mutablePoints[this.activeIndex] = [mx, my];
+			figure.innerHTML = this.callback(this.bezier, this.sliderData, undefined, this.computeTypeChoice);
+		} else if (this.triggerOnMouseMove) {
+			figure.innerHTML = this.callback(this.bezier, this.sliderData, [mx, my], this.computeTypeChoice);
+		}
+	}
+
+	static getSliderValue(sliderValue: number, sliderUnit?: string | string[]): string | undefined {
+		return Array.isArray(sliderUnit) ? sliderUnit[sliderValue] : sliderUnit;
+	}
 }
 
 window.customElements.define("bezier-example", BezierExample);
--- website/other/bezier-rs-demos/src/features/subpathFeatures.ts
+++ website/other/bezier-rs-demos/src/features/subpathFeatures.ts
@@ -2,46 +2,46 @@ import { tSliderOptions } from "@/utils/options";
 import { ComputeType, WasmSubpathInstance } from "@/utils/types";
 
 const subpathFeatures = {
-  "Constructor": {
-    callback: (subpath: WasmSubpathInstance): string => subpath.to_svg(),
-  },
-  "Length": {
-    callback: (subpath: WasmSubpathInstance): string => subpath.length(),
-  },
-  "Evaluate": {
-    callback: (subpath: WasmSubpathInstance, options: Record<string, number>, _: undefined, computeType: ComputeType): string => subpath.evaluate(options.computeArgument, computeType),
-    sliderOptions: [{ ...tSliderOptions, variable: "computeArgument" }],
-    chooseComputeType: true,
-  },
-  "Project": {
-    callback: (subpath: WasmSubpathInstance, _: Record<string, number>, mouseLocation?: [number, number]): string =>
-      mouseLocation ? subpath.project(mouseLocation[0], mouseLocation[1]) : subpath.to_svg(),
-    triggerOnMouseMove: true,
-  },
-  "Intersect (Line Segment)": {
-    callback: (subpath: WasmSubpathInstance): string =>
-      subpath.intersect_line_segment([
-        [150, 150],
-        [20, 20],
-      ]),
-  },
-  "Intersect (Quadratic segment)": {
-    callback: (subpath: WasmSubpathInstance): string =>
-      subpath.intersect_quadratic_segment([
-        [20, 80],
-        [180, 10],
-        [90, 120],
-      ]),
-  },
-  "Intersect (Cubic segment)": {
-    callback: (subpath: WasmSubpathInstance): string =>
-      subpath.intersect_cubic_segment([
-        [40, 20],
-        [100, 40],
-        [40, 120],
-        [175, 140],
-      ]),
-  },
+	Constructor: {
+		callback: (subpath: WasmSubpathInstance): string => subpath.to_svg(),
+	},
+	Length: {
+		callback: (subpath: WasmSubpathInstance): string => subpath.length(),
+	},
+	Evaluate: {
+		callback: (subpath: WasmSubpathInstance, options: Record<string, number>, _: undefined, computeType: ComputeType): string => subpath.evaluate(options.computeArgument, computeType),
+		sliderOptions: [{ ...tSliderOptions, variable: "computeArgument" }],
+		chooseComputeType: true,
+	},
+	Project: {
+		callback: (subpath: WasmSubpathInstance, _: Record<string, number>, mouseLocation?: [number, number]): string =>
+			mouseLocation ? subpath.project(mouseLocation[0], mouseLocation[1]) : subpath.to_svg(),
+		triggerOnMouseMove: true,
+	},
+	"Intersect (Line Segment)": {
+		callback: (subpath: WasmSubpathInstance): string =>
+			subpath.intersect_line_segment([
+				[150, 150],
+				[20, 20],
+			]),
+	},
+	"Intersect (Quadratic segment)": {
+		callback: (subpath: WasmSubpathInstance): string =>
+			subpath.intersect_quadratic_segment([
+				[20, 80],
+				[180, 10],
+				[90, 120],
+			]),
+	},
+	"Intersect (Cubic segment)": {
+		callback: (subpath: WasmSubpathInstance): string =>
+			subpath.intersect_cubic_segment([
+				[40, 20],
+				[100, 40],
+				[40, 120],
+				[175, 140],
+			]),
+	},
 };
 
 export default subpathFeatures;
