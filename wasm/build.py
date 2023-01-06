6c95629bb780c9ccf3d39c83966e0e390e5394b5
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-06 22:41:48 +0000

    Fix wasm build script (python)

--- wasm/build.py
+++ wasm/build.py
@@ -1,13 +1,7 @@
 import os, json
 
 def patch_json():
-    manifest = json.load(open("pkg/package.json"))
-    if "dependencies" in manifest:
-        return
-    manifest["dependencies"] = {
-        "graphite-frontend": "file:../../frontend"
-    }
-    json.dump(manifest, open("pkg/package.json", "w"))
-
+    os.chdir("pkg")
+    assert os.system("npm add ../../frontend-svelte") == 0
 assert os.system("wasm-pack build") == 0
 patch_json()
