70baec972e429080440eba2c8a3502f577ed1761
Keavon Chambers <keavon@keavon.com>
2023-01-06 23:35:33 -0800

    Port Properties

---
+++ frontend-svelte/src/lib/components/panels/Properties.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	import { defaultWidgetLayout, patchWidgetLayout, UpdatePropertyPanelOptionsLayout, UpdatePropertyPanelSectionsLayout } from "@/wasm-communication/messages";
+
+	import LayoutCol from "$lib/components/layout/LayoutCol.svelte";
+	import LayoutRow from "$lib/components/layout/LayoutRow.svelte";
+	import WidgetLayout from "$lib/components/widgets/WidgetLayout.svelte";
+
+	// inject: ["editor", "dialog"],
+
+	let propertiesOptionsLayout = defaultWidgetLayout();
+	let propertiesSectionsLayout = defaultWidgetLayout();
+
+	onMount(() => {
+		editor.subscriptions.subscribeJsMessage(UpdatePropertyPanelOptionsLayout, (updatePropertyPanelOptionsLayout) => {
+			patchWidgetLayout(propertiesOptionsLayout, updatePropertyPanelOptionsLayout);
+		});
+
+		editor.subscriptions.subscribeJsMessage(UpdatePropertyPanelSectionsLayout, (updatePropertyPanelSectionsLayout) => {
+			patchWidgetLayout(propertiesSectionsLayout, updatePropertyPanelSectionsLayout);
+		});
+	});
+</script>
+
+<LayoutCol class="properties">
+	<LayoutRow class="options-bar">
+		<WidgetLayout layout={propertiesOptionsLayout} />
+	</LayoutRow>
+	<LayoutRow class="sections" scrollableY={true}>
+		<WidgetLayout layout={propertiesSectionsLayout} />
+	</LayoutRow>
+</LayoutCol>
+
+<style lang="scss" global>
+	.properties {
+		height: 100%;
+
+		.widget-layout {
+			flex: 1 1 100%;
+			margin: 0 4px;
+		}
+
+		.options-bar {
+			height: 32px;
+			flex: 0 0 auto;
+		}
+
+		.sections {
+			flex: 1 1 100%;
+		}
+	}
+</style>
