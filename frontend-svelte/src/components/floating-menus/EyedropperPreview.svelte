d4b46d6807d93d78f9baa77e201dfe788eb73d76
Keavon Chambers <keavon@keavon.com>
2023-01-13 01:05:20 -0800

    Port frontend from Vue to Svelte (WIP in separate folder, many bugs) (#964)
    * Make `tauri build` just work
    
    * Move folder: frontend/wasm -> wasm
    
    * Create SvelteKit project with 'npx create-svelte'
    
    * Move wasm-communication into seperate npm package
    
    * Use wasm-pack directly and pack package.json
    
    * Got it to work
    
    * Add primitive build script for wasm npm module
    
    * Fix wasm build script (python)
    
    * Clean up glue code
    
    * Rewrite wasm build script in node.js
    
    * Add serde-reflection to trace types
    
    * Add traced types
    
    * Generate typescript (.d.ts) from Rust types
    
    * Update .d.ts
    
    * Finalize TS types
    
    * Add script to update .d.ts
    
    * Add watch command to build wasm-bindgen
    
    * Make wasm work again
    
    * Add sass; fix build script for windows
    
    * Describe requirement for wasm-pack
    
    * Add license
    
    * Copy and reorganize vue components
    
    * translate LayoutCol.vue
    
    * Split app.scss into pieces
    
    * Translate LayoutRow.svelte
    
    * Rename scss files
    
    * Fix compile issues on Windows
    
    * WIP port TitleBar
    
    * Support classes for LayoutCol/Row
    
    * Restructure based on Vue codebase
    
    * Port all components in window folder
    
    * Port FloatingMenu
    
    * Port Document panel component
    
    * Update readme after folder move
    
    * Update typegen: print discriminant by default
    
    * Update typegen: Merge from branch 'tailwind' 4f14fedb
    
    Fixes bigint & bytes
    
    * Made Vue/webpack/eslint to accept wasm package at new location
    
    This is quite a hack.
    
    Those two packages are both named the same. Yes, it's an npm package inside another npm package.
    
    - frontend/src/wasm-communication/
    - frontend-svelte/glue/
    
    'wasm/pkg/index.js' imports the correct one registered when linking.
    
    * Port LayerTree
    
    * Port NodeGraph
    
    * Port Properties
    
    * Port components in /floating-menus
    
    * Finish porting all Vue -> Svelte components
    
    * Change import prefix
    
    * Revert type generation
    
    * Revert moved wasm folder
    
    * Revert all of @locriacyber's work on this branch
    
    - Remove Vite and restore Webpack
    - Remove SvelteKit
    - Remove everything except the components I ported to Svelte
    - Restore all frontend files from Vue code, now altered for Svelte
    
    * Convert Vue's 'reactive' and 'provide' to Svelte's stores and contexts
    
    * Fix event emitting and bi-di data flow
    
    * Undo removal of 'update:' events
    
    * Fix 'update:' event dispatching
    
    * Fix usage in parent of bi-di component props
    
    * Fix component typing, more progress towards no errors
    
    * The page builds and opens!
    
    * Add loading spinner and remove postcss dependency
    
    * Make the basics of document editing work
    
    * Fix rebase history
    
    Co-authored-by: Locria Cyber <74560659+locriacyber@users.noreply.github.com>

---
+++ frontend-svelte/src/components/floating-menus/EyedropperPreview.svelte
@@ -0,0 +1,134 @@
+<script lang="ts" context="module">
+	// Should be equal to the width and height of the canvas in the CSS
+	const ZOOM_WINDOW_DIMENSIONS_EXPANDED = 110;
+	// Should be equal to the width and height of the `.pixel-outline` div in the CSS, and should be evenly divisible into the number above
+	const UPSCALE_FACTOR = 10;
+
+	export const ZOOM_WINDOW_DIMENSIONS = ZOOM_WINDOW_DIMENSIONS_EXPANDED / UPSCALE_FACTOR;
+</script>
+
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	import FloatingMenu from "@/components/layout/FloatingMenu.svelte";
+
+	const temporaryCanvas = document.createElement("canvas");
+
+	let zoomPreviewCanvas: HTMLCanvasElement;
+
+	export let imageData: ImageData | undefined = undefined;
+	export let colorChoice: string;
+	export let primaryColor: string;
+	export let secondaryColor: string;
+	export let x: number;
+	export let y: number;
+
+	$: watchImageData(imageData);
+
+	function watchImageData(imageData: ImageData | undefined) {
+		displayImageDataPreview(imageData);
+	}
+
+	function displayImageDataPreview(imageData: ImageData | undefined) {
+		zoomPreviewCanvas.width = ZOOM_WINDOW_DIMENSIONS;
+		zoomPreviewCanvas.height = ZOOM_WINDOW_DIMENSIONS;
+		const context = zoomPreviewCanvas.getContext("2d");
+
+		temporaryCanvas.width = ZOOM_WINDOW_DIMENSIONS;
+		temporaryCanvas.height = ZOOM_WINDOW_DIMENSIONS;
+		const temporaryContext = temporaryCanvas.getContext("2d");
+
+		if (!imageData || !context || !temporaryContext) return;
+
+		temporaryContext.putImageData(imageData, 0, 0, 0, 0, ZOOM_WINDOW_DIMENSIONS, ZOOM_WINDOW_DIMENSIONS);
+
+		context.fillStyle = "black";
+		context.fillRect(0, 0, ZOOM_WINDOW_DIMENSIONS, ZOOM_WINDOW_DIMENSIONS);
+
+		context.drawImage(temporaryCanvas, 0, 0);
+	}
+
+	onMount(() => {
+		displayImageDataPreview(imageData);
+	});
+</script>
+
+<FloatingMenu
+	open={true}
+	class="eyedropper-preview"
+	type="Cursor"
+	styles={{ "--ring-color-primary": primaryColor, "--ring-color-secondary": secondaryColor, "--ring-color-choice": colorChoice, left: x + "px", top: y + "px" }}
+>
+	<div class="ring">
+		<div class="canvas-container">
+			<canvas bind:this={zoomPreviewCanvas} />
+			<div class="pixel-outline" />
+		</div>
+	</div>
+</FloatingMenu>
+
+<style lang="scss" global>
+	.eyedropper-preview {
+		pointer-events: none;
+
+		.ring {
+			transform: translate(0, -50%) rotate(45deg);
+			position: relative;
+			background: var(--ring-color-choice);
+			padding: 16px;
+			border: 8px solid;
+			border-radius: 50%;
+			border-top-color: var(--ring-color-primary);
+			border-left-color: var(--ring-color-primary);
+			border-bottom-color: var(--ring-color-secondary);
+			border-right-color: var(--ring-color-secondary);
+
+			&::after {
+				content: "";
+				position: absolute;
+				width: 100%;
+				height: 100%;
+				top: -8px;
+				left: -8px;
+				padding: 8px;
+				border-radius: 50%;
+				box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.5), 0 0 8px rgba(0, 0, 0, 0.25);
+			}
+
+			.canvas-container {
+				transform: rotate(-45deg);
+
+				canvas {
+					display: block;
+					width: 110px;
+					height: 110px;
+					border-radius: 50%;
+					image-rendering: pixelated;
+				}
+
+				&::after {
+					content: "";
+					position: absolute;
+					top: 0;
+					left: 0;
+					width: 100%;
+					height: 100%;
+					border-radius: 50%;
+					box-shadow: inset 0 0 0 1px rgba(255, 255, 255, 0.5), inset 0 0 8px rgba(0, 0, 0, 0.25);
+				}
+
+				.pixel-outline {
+					position: absolute;
+					left: 50%;
+					top: 50%;
+					transform: translate(-50%, -50%);
+					--outline-width: 2;
+					margin-top: calc(-1px * (var(--outline-width) / 2));
+					width: calc(10px - (var(--outline-width) * 1px));
+					height: calc(10px - var(--outline-width) * 1px);
+					border: calc(var(--outline-width) * 1px) solid var(--color-0-black);
+				}
+			}
+		}
+	}
+</style>
