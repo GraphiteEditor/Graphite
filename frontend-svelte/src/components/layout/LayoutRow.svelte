dcc3eadf448392a1e3957dc02dc09c90bb4c712a
Keavon Chambers <keavon@keavon.com>
2023-01-13 01:05:20 -0800

    Port frontend from Vue to Svelte (WIP in separate folder, many bugs) (#964)
    * Make `tauri build` just work
    
    * Move folder: frontend/wasm -> wasm
    
    * Create SvelteKit project with 'npx create-svelte'
    
    * Move wasm-communication into seperate npm package
    
    * Use wasm-pack directly and pack package.json
    
    * Got it to work
    
    * Add primitive build script for wasm npm module
    
    * Fix wasm build script (python)
    
    * Clean up glue code
    
    * Rewrite wasm build script in node.js
    
    * Add serde-reflection to trace types
    
    * Add traced types
    
    * Generate typescript (.d.ts) from Rust types
    
    * Update .d.ts
    
    * Finalize TS types
    
    * Add script to update .d.ts
    
    * Add watch command to build wasm-bindgen
    
    * Make wasm work again
    
    * Add sass; fix build script for windows
    
    * Describe requirement for wasm-pack
    
    * Add license
    
    * Copy and reorganize vue components
    
    * translate LayoutCol.vue
    
    * Split app.scss into pieces
    
    * Translate LayoutRow.svelte
    
    * Rename scss files
    
    * Fix compile issues on Windows
    
    * WIP port TitleBar
    
    * Support classes for LayoutCol/Row
    
    * Restructure based on Vue codebase
    
    * Port all components in window folder
    
    * Port FloatingMenu
    
    * Port Document panel component
    
    * Update readme after folder move
    
    * Update typegen: print discriminant by default
    
    * Update typegen: Merge from branch 'tailwind' 4f14fedb
    
    Fixes bigint & bytes
    
    * Made Vue/webpack/eslint to accept wasm package at new location
    
    This is quite a hack.
    
    Those two packages are both named the same. Yes, it's an npm package inside another npm package.
    
    - frontend/src/wasm-communication/
    - frontend-svelte/glue/
    
    'wasm/pkg/index.js' imports the correct one registered when linking.
    
    * Port LayerTree
    
    * Port NodeGraph
    
    * Port Properties
    
    * Port components in /floating-menus
    
    * Finish porting all Vue -> Svelte components
    
    * Change import prefix
    
    * Revert type generation
    
    * Revert moved wasm folder
    
    * Revert all of @locriacyber's work on this branch
    
    - Remove Vite and restore Webpack
    - Remove SvelteKit
    - Remove everything except the components I ported to Svelte
    - Restore all frontend files from Vue code, now altered for Svelte
    
    * Convert Vue's 'reactive' and 'provide' to Svelte's stores and contexts
    
    * Fix event emitting and bi-di data flow
    
    * Undo removal of 'update:' events
    
    * Fix 'update:' event dispatching
    
    * Fix usage in parent of bi-di component props
    
    * Fix component typing, more progress towards no errors
    
    * The page builds and opens!
    
    * Add loading spinner and remove postcss dependency
    
    * Make the basics of document editing work
    
    * Fix rebase history
    
    Co-authored-by: Locria Cyber <74560659+locriacyber@users.noreply.github.com>

---
+++ frontend-svelte/src/components/layout/LayoutRow.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	let className = "";
+	export { className as class };
+	export let classes: Record<string, boolean> = {};
+	let styleName = "";
+	export { styleName as style };
+	export let styles: Record<string, string | number | undefined> = {};
+	export let tooltip: string | undefined = undefined;
+	export let scrollableX: boolean = false;
+	export let scrollableY: boolean = false;
+
+	let divElement: HTMLDivElement;
+
+	$: extraClasses = Object.entries(classes)
+		.flatMap((classAndState) => (classAndState[1] ? [classAndState[0]] : []))
+		.join(" ");
+	$: extraStyles = Object.entries(styles)
+		.flatMap((styleAndValue) => (styleAndValue[1] !== undefined ? [`${styleAndValue[0]}: ${styleAndValue[1]};`] : []))
+		.join(" ");
+
+	export function div(): HTMLDivElement {
+		return divElement;
+	}
+</script>
+
+<div
+	class={`layout-row ${className} ${extraClasses}`.trim()}
+	class:scrollable-x={scrollableX}
+	class:scrollable-y={scrollableY}
+	style={`${styleName} ${extraStyles}`.trim() || undefined}
+	title={tooltip}
+	bind:this={divElement}
+	on:click
+	on:dblclick
+	on:pointerdown
+	on:pointermove
+	on:pointerup
+	on:dragleave
+	on:dragover
+	on:dragstart
+	on:dragend
+	on:drop
+	on:wheel
+	on:scroll
+	on:focus
+	on:blur
+	{...$$restProps}
+>
+	<slot />
+</div>
+
+<style lang="scss" global>
+	.layout-row {
+		display: flex;
+		flex-direction: row;
+		flex-grow: 1;
+	}
+</style>
---
+++ frontend-svelte/src/components/widgets/inputs/OptionalInput.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
+	import { type IconName } from "@/utility-functions/icons";
+
+	import LayoutRow from "@/components/layout/LayoutRow.svelte";
+	import CheckboxInput from "@/components/widgets/inputs/CheckboxInput.svelte";
+
+	// emits: ["update:checked"],
+	const dispatch = createEventDispatcher<{ checked: boolean }>();
+
+	export let checked: boolean;
+	export let disabled = false;
+	export let icon: IconName = "Checkmark";
+	export let tooltip: string | undefined = undefined;
+
+	let checkboxInput: CheckboxInput;
+</script>
+
+<LayoutRow class="optional-input" classes={{ disabled }}>
+	<CheckboxInput {checked} on:checked {disabled} {icon} {tooltip} bind:this={checkboxInput} />
+</LayoutRow>
+
+<style lang="scss" global>
+	.optional-input {
+		flex-grow: 0;
+
+		label {
+			align-items: center;
+			justify-content: center;
+			white-space: nowrap;
+			width: 24px;
+			height: 24px;
+			border: 1px solid var(--color-5-dullgray);
+			border-radius: 2px 0 0 2px;
+			box-sizing: border-box;
+		}
+
+		&.disabled label {
+			border: 1px solid var(--color-4-dimgray);
+		}
+	}
+</style>
