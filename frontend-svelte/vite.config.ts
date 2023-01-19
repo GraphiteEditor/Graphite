5beaade0cb48a08301ecd3d7f759be24b5535b06
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2023-01-19 18:20:48 +0000

    Add license generator with rollup

--- frontend-svelte/vite.config.ts
+++ frontend-svelte/vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import wasm from "vite-plugin-wasm";
+import toplevelawait from "vite-plugin-top-level-await"
+import license from "rollup-plugin-license"
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte(), wasm()],
+	plugins: [svelte(), toplevelawait(), wasm()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			plugins: [
+				license({
+					thirdParty: {
+						output: path.resolve(__dirname, "dist/third-party-licenses.txt"),
+					},
+				}),
+			],
 		},
 	},
 });
