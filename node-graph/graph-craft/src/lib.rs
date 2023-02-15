5dab7de68dc9b0b47cde7a9e3f614563e7ca260f
Dennis Kobert <dennis@kobert.dev>
2023-02-15 23:31:30 +0100

    Add type checking to the node graph (#1025)
    * Implement type inference
    
    Add type hints to node trait
    
    Add type annotation infrastructure
    
    Refactor type ascription infrastructure
    
    Run cargo fix
    
    Insert infer types stub
    
    Remove types from node identifier
    
    * Implement covariance
    
    * Disable rejection of generic inputs + parameters
    
    * Fix lints
    
    * Extend type checking to cover Network inputs
    
    * Implement generic specialization
    
    * Relax covariance rules
    
    * Fix type annotations for TypErasedComposeNode
    
    * Fix type checking errors
    
    * Keep connection information during node resolution
    * Fix TypeDescriptor PartialEq implementation
    
    * Apply review suggestions
    
    * Add documentation to type inference
    
    * Add Imaginate node to document node types
    
    * Fix whitespace in macros
    
    * Add types to imaginate node
    
    * Fix type declaration for imaginate node + add console logging
    
    * Use fully qualified type names as fallback during comparison
    
    ---------
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

--- node-graph/graph-craft/src/lib.rs
+++ node-graph/graph-craft/src/lib.rs
@@ -1,6 +1,10 @@
 #[macro_use]
 extern crate log;
 
+#[macro_use]
+extern crate graphene_core;
+pub use graphene_core::{concrete, generic, NodeIdentifier, Type, TypeDescriptor};
+
 pub mod document;
 pub mod proto;
 
