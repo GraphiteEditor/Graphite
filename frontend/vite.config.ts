23a748c24b757decbc1ca1bc71d3d3bd638f5906
Dennis Kobert <dennis@kobert.dev>
2023-02-16 21:48:54 +0100

    Migrate vue code base to vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,12 +6,14 @@ import wasm from "vite-plugin-wasm";
 import toplevelawait from "vite-plugin-top-level-await"
 import license from "rollup-plugin-license"
 import * as path from "path";
+import svgLoader from 'vite-svg-loader'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), wasm(), toplevelawait()],
+  plugins: [vue(), toplevelawait(), wasm() , svgLoader()],
   resolve: {
+		extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
