b4a265a51297db7a1fc81200e4139c4f21f4959c
Keavon Chambers <keavon@keavon.com>
2023-03-10 03:55:13 -0800

    Delete the separate Svelte frontend folder

--- frontend-svelte/src/imports.d.ts
+++
@@ -1,7 +0,0 @@
-// Allow `import` statements to work with SVG files in the eyes of the TypeScript compiler.
-// This prevents red underlines from showing and lets it know the types of imported variables are strings.
-// The actual import is performed by Webpack when building, as configured in the module rules in `webpack.config.ts`.
-declare module "*.svg" {
-	const content: string;
-	export default content;
-}
