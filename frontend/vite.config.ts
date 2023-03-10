87eb2515b9f8fbfa58fec92caf5ef8007dc83da8
Dennis Kobert <dennis@kobert.dev>
2023-03-11 00:21:47 +0100

    Fix compilation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,30 +2,27 @@
 
 import * as path from "path";
 
+
+import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import license from "rollup-plugin-license";
-import { defineConfig } from "vite";
 import toplevelawait from "vite-plugin-top-level-await";
 import wasm from "vite-plugin-wasm";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte({prebundleSvelteLibraries: false}), toplevelawait(), wasm()],
+	plugins: [svelte(), toplevelawait(), wasm()],
 	resolve: {
-		extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".svelte"],
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 		},
-	},
-	optimizeDeps: {
-		disabled: true
 	},
 	build: {
 		rollupOptions: {
 			plugins: [
 				license({
 					thirdParty: {
-						output: path.resolve(__dirname, "dist/third-party-licenses.txt"),
+						output: path.resolve(__dirname, "public/third-party-licenses.txt"),
 					},
 				}),
 			],
