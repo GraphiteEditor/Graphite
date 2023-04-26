ba34cab279f203d2ea90374a30a4b6b62ed0a940
Keavon Chambers <keavon@keavon.com>
2023-04-26 16:35:36 -0700

    Fix Parcel's index.html entry point

--- frontend/src/imports.d.ts
+++ frontend/src/imports.d.ts
@@ -1,6 +1,6 @@
 // Allow `import` statements to work with SVG files in the eyes of the TypeScript compiler.
 // This prevents red underlines from showing and lets it know the types of imported variables are strings.
-// The actual import is performed by Webpack when building, as configured in the module rules in `webpack.config.ts`.
+// The actual import is performed by the bundler when building.
 declare module "*.svg" {
 	const content: string;
 	export default content;
