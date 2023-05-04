9282174cb65d354176b2171e6eec02559d6c1bcd
Ollie Dolan <olliedolan10@gmail.com>
2023-05-03 17:23:58 -0700

    formatted // snapping with select tool working

--- editor/src/messages/portfolio/document/utility_types/transformation.rs
+++ editor/src/messages/portfolio/document/utility_types/transformation.rs
@@ -373,9 +373,13 @@ impl<'a> Selected<'a> {
 	pub fn update_transforms(&mut self, delta: DAffine2, grid: bool) {
 		if !self.selected.is_empty() {
 			let pivot_point = self.document.root.transform.transform_point2(*self.pivot);
+			// let mut new_pivot = *self.pivot;
+			// if grid {
+			// 	new_pivot = new_pivot.round();
+			// }
 			let pivot = DAffine2::from_translation(pivot_point);
 			let transformation = pivot * delta * pivot.inverse();
-			debug!("pivot_point {:?}", pivot_point);
+			// debug!("pivot_point {:?}", pivot_point);
 			debug!("*self.pivot {:?}", *self.pivot);
 			debug!("transformation {:?}", transformation);
 
@@ -417,14 +421,26 @@ impl<'a> Selected<'a> {
 					};
 					for (point_id, position) in original {
 						let mut viewport_point = viewspace.transform_point2(*position);
-						
 
 						if grid {
+							let rounded = position.round();
+							debug!("rounded {:?}", rounded);
+							viewport_point = viewspace.transform_point2(rounded);
 							viewport_point = viewport_point.round();
 						}
+						let doc_transform = self.document.root.transform;
+						let doc_space_pos = doc_transform.inverse().transform_point2(viewport_point).round();
+						debug!("doc_space_pos {:?}", doc_space_pos);
+
+						// let doc_transform = document_message_handler.document_legacy.root.transform;
+						// 			let doc_space_pos = doc_transform.inverse().transform_point2(snapped_position).round();
+						// 			debug!("doc_space_pos {:?}", doc_space_pos);
+						let rounded_position = doc_transform.transform_point2(doc_space_pos).round();
+						debug!("rounded_position {:?}", rounded_position);
 						let new_pos_viewport = layerspace_rotation.transform_point2(viewport_point);
 						debug!("viewport_point {:?}", viewport_point);
 						debug!("new_pos_viewport {:?}", new_pos_viewport);
+
 						let point = *point_id;
 						let position = new_pos_viewport;
 
