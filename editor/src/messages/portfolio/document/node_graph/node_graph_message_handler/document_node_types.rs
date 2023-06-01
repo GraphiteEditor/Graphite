ef2e4223977ef343be2068905ced63f06a24c471
Orson Peters <orsonpeters@gmail.com>
2023-06-01 17:23:07 +0200

    Added blit caching.
    Added bulk memory target feature.
    
    Added brush texture caching.
    
    Removed dead format call.
    
    Fix brush_texture_cache crashing on serialization.

--- editor/src/messages/portfolio/document/node_graph/node_graph_message_handler/document_node_types.rs
+++ editor/src/messages/portfolio/document/node_graph/node_graph_message_handler/document_node_types.rs
@@ -9,6 +9,7 @@ use graph_craft::document::*;
 use graph_craft::imaginate_input::ImaginateSamplingMethod;
 use graph_craft::NodeIdentifier;
 use graphene_core::raster::{BlendMode, Color, Image, ImageFrame, LuminanceCalculation, RedGreenBlue, RelativeAbsolute, SelectiveColorChoice};
+use graphene_core::raster::brush_cache::BrushCache;
 use graphene_core::text::Font;
 use graphene_core::vector::VectorData;
 use graphene_core::*;
@@ -669,11 +670,12 @@ fn static_nodes() -> Vec<DocumentNodeType> {
 		DocumentNodeType {
 			name: "Brush",
 			category: "Brush",
-			identifier: NodeImplementation::proto("graphene_std::brush::BrushNode<_, _>"),
+			identifier: NodeImplementation::proto("graphene_std::brush::BrushNode<_, _, _>"),
 			inputs: vec![
 				DocumentInputType::value("Background", TaggedValue::ImageFrame(ImageFrame::empty()), true),
 				DocumentInputType::value("Bounds", TaggedValue::ImageFrame(ImageFrame::empty()), true),
 				DocumentInputType::value("Trace", TaggedValue::BrushStrokes(Vec::new()), false),
+				DocumentInputType::value("Cache", TaggedValue::BrushCache(BrushCache::new_proto()), false),
 			],
 			outputs: vec![DocumentOutputType {
 				name: "Image",
