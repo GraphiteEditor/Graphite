80788697e1b36e9dcfa2590f0822b83403ad00f5
0hypercube <0hypercube@gmail.com>
2023-07-17 18:23:31 +0100

    Do not render old artboards

--- frontend/src/components/panels/Document.svelte
+++ frontend/src/components/panels/Document.svelte
@@ -454,9 +454,9 @@
 						/>
 					{/if}
 					<div class="canvas" on:pointerdown={(e) => canvasPointerDown(e)} on:dragover={(e) => e.preventDefault()} on:drop={(e) => pasteFile(e)} bind:this={canvasContainer} data-canvas>
-						<svg class="artboards" style:width={canvasWidthCSS} style:height={canvasHeightCSS}>
+						<!-- <svg class="artboards" style:width={canvasWidthCSS} style:height={canvasHeightCSS}>
 							{@html artboardSvg}
-						</svg>
+						</svg> -->
 						<svg class="artboards" style:width={canvasWidthCSS} style:height={canvasHeightCSS}>
 							{@html nodeRenderSvg}
 						</svg>
