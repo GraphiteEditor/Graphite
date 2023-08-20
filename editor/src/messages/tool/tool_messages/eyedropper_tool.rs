f4418d05b5a4c61220cbf896533b8ee3cd39bd85
0hypercube <0hypercube@gmail.com>
2023-08-20 13:43:53 +0100

    Fix freehand and spline tool

--- editor/src/messages/tool/tool_messages/eyedropper_tool.rs
+++ editor/src/messages/tool/tool_messages/eyedropper_tool.rs
@@ -84,7 +84,7 @@ impl Fsm for EyedropperToolFsmState {
 	fn transition(self, event: ToolMessage, _tool_data: &mut Self::ToolData, tool_action_data: &mut ToolActionHandlerData, _tool_options: &(), responses: &mut VecDeque<Message>) -> Self {
 		let ToolActionHandlerData { global_tool_data, input, .. } = tool_action_data;
 
-		let ToolMessage::Eyedropper(event) = event else{
+		let ToolMessage::Eyedropper(event) = event else {
 			return self;
 		};
 		match (self, event) {
