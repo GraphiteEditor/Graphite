8055e85f382c2c552549c3c74753a62375f65b78
Iago-lito <iago-lito@users.noreply.github.com>
2023-11-26 16:51:30 +0100

    Auto-create `frontend/dist` on first build. (#1478)

--- frontend/src-tauri/build.rs
+++ frontend/src-tauri/build.rs
@@ -1,3 +1,8 @@
+use std::{fs, path::PathBuf};
+
 fn main() {
+	// Directory required for compilation, but not tracked by git if empty.
+	let dist_dir: PathBuf = ["..", "dist"].iter().collect();
+	fs::create_dir_all(dist_dir).unwrap();
 	tauri_build::build()
 }
