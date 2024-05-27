25f3b71c5cf6008034eaad5eb9b06ef4ba176a91
skoriop <karthikp.ivy@gmail.com>
2024-05-27 22:01:58 +0530

    add interpolation

--- node-graph/gcore/src/animation.rs
+++ node-graph/gcore/src/animation.rs
@@ -30,6 +30,35 @@ impl KeyframesF64 {
 	pub fn new(keyframes: Vec<KeyframeF64>) -> Self {
 		Self { keyframes }
 	}
+
+	pub fn get_value(&self, time: f64) -> f64 {
+		if self.keyframes.len() == 0 {
+			return 0.;
+		}
+		if time <= self.keyframes[0].time {
+			return self.keyframes[0].value;
+		}
+		if time >= self.keyframes[self.keyframes.len() - 1].time {
+			return self.keyframes[self.keyframes.len() - 1].value;
+		}
+		// `partition_point` returns the first index for which the predicate is false
+		// so, `ind` is the first index for which k.time >= time
+		let ind = self.keyframes.partition_point(|k| k.time < time);
+		if self.keyframes[ind].time == time {
+			return self.keyframes[ind].value;
+		}
+		// ind > 0 because we already checked the first keyframe
+		assert!(ind > 0);
+		let k1 = &self.keyframes[ind - 1];
+		let k2 = &self.keyframes[ind];
+		return Self::interpolate(k1, k2, time);
+	}
+
+	fn interpolate(k1: &KeyframeF64, k2: &KeyframeF64, time: f64) -> f64 {
+		assert!(k1.time < time && time < k2.time);
+		let t = (time - k1.time) / (k2.time - k1.time);
+		return k1.value + (k2.value - k1.value) * t;
+	}
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
