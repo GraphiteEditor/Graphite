GraphiteEditor/Graphite

commit 6d74abb4deca159add4dd0569e79b54b05181f7f

Node network subgraph editing (#1750)
* Breadcrumb visualization, nested network consistency, create definitions for Merge internal nodes

* Add index to network inputs, remove imports usage from flatten network

* Replace NodeOutput with NodeInput::Node

* Fully remove imports field, remove unnecessary identity nodes, move Output node to encapsulating network

* Replace previous_outputs with root_node, fix adding artboard/layer to empty network

* Import/Export UI nodes

* Display input/output types dynamically from compiled network

* Add LayerNodeIdentifer::ROOT_PARENT

* Prevent .to_node() on ROOT_PARENT

* Separate NodeGraphMessage and GraphOperationMessage

* General bug fixes with nested networks

* Change layer color, various bug fixes and improvements

* Fix disconnect and set node input for proto nodes and UI export node

* Dashed line to export for previewed node

* Fix deleting proto nodes and nodes that feed into export

* Allow modifications to nodes outside of nested network

* Get network from Node Id parameter

* Change root_node to previous_root_node

* Get TaggedValue from proto node implementation type when disconnecting

* Improve preview functionality and state

* Artboard position and delete children fix

* Name inputs/outputs based on DocumentNodeDefinition or type, fix new artboard/layer insertion

* replace "Link" with "Wire", adjust previewing

* Various bug fixes and improvements

* Modify Sample and Poisson-Disk points, fix incorrect input index and deleting currently viewed node

* Open demo artwork

* Fix opening already upgraded documents and refactor FrontendGraphDataType usages

* Fix deleting within network and other bugs

* Get default node input from compiled network when copying, fix previews, tests, demo artwork

* Code cleanup

* Hide EditorApi and add a comment describing unresolved Import node input types

* Code review

editor/src/messages/portfolio/document/graph_operation/graph_operation_message.rs
@@ -4,10 +4,8 @@ use crate::messages::portfolio::document::utility_types::document_metadata::Laye
 use crate::messages::prelude::*;
 
 use bezier_rs::Subpath;
-use graph_craft::document::DocumentNode;
-use graph_craft::document::NodeId;
-use graphene_core::raster::BlendMode;
-use graphene_core::raster::ImageFrame;
+use graph_craft::document::{DocumentNode, NodeId, NodeInput};
+use graphene_core::raster::{BlendMode, ImageFrame};
 use graphene_core::text::Font;
 use graphene_core::uuid::ManipulatorGroupId;
 use graphene_core::vector::brush_stroke::BrushStroke;
@@ -30,17 +28,27 @@ pub enum GraphOperationMessage {
 		node_id: NodeId,
 		operation: BooleanOperation,
 	},
+	DeleteLayer {
+		layer: LayerNodeIdentifier,
+		reconnect: bool,
+	},
+	// TODO: Eventually remove this (probably starting late 2024)
+	DeleteLegacyOutputNode,
 	DisconnectInput {
 		node_id: NodeId,
 		input_index: usize,
+	},
+	DisconnectNodeFromStack {
+		node_id: NodeId,
+		reconnect_to_sibling: bool,
 	},
 	FillSet {
 		layer: LayerNodeIdentifier,
 		fill: Fill,
 	},
-	InsertLayerAtStackIndex {
-		layer_id: NodeId,
-		parent: NodeId,
+	InsertNodeAtStackIndex {
+		node_id: NodeId,
+		parent: LayerNodeIdentifier,
 		insert_index: usize,
 	},
 	InsertBooleanOperation {
@@ -59,7 +67,7 @@ pub enum GraphOperationMessage {
 		pre_node_output_index: usize,
 	},
 	MoveSelectedSiblingsToChild {
-		new_parent: NodeId,
+		new_parent: LayerNodeIdentifier,
 	},
 	OpacitySet {
 		layer: LayerNodeIdentifier,
@@ -145,5 +153,44 @@ pub enum GraphOperationMessage {
 		transform: DAffine2,
 		parent: LayerNodeIdentifier,
 		insert_index: isize,
+	},
+	ShiftUpstream {
+		node_id: NodeId,
+		shift: IVec2,
+		shift_self: bool,
+	},
+	SetNodePosition {
+		node_id: NodeId,
+		position: IVec2,
+	},
+	SetName {
+		layer: LayerNodeIdentifier,
+		name: String,
+	},
+	SetNameImpl {
+		layer: LayerNodeIdentifier,
+		name: String,
+	},
+	SetNodeInput {
+		node_id: NodeId,
+		input_index: usize,
+		input: NodeInput,
+	},
+	ToggleSelectedVisibility,
+	ToggleVisibility {
+		node_id: NodeId,
+	},
+	SetVisibility {
+		node_id: NodeId,
+		visible: bool,
+	},
+	StartPreviewingWithoutRestore,
+	ToggleSelectedLocked,
+	ToggleLocked {
+		node_id: NodeId,
+	},
+	SetLocked {
+		node_id: NodeId,
+		locked: bool,
 	},
 }

editor/src/messages/portfolio/portfolio_message_handler.rs
@@ -205,7 +205,13 @@ impl MessageHandler<PortfolioMessage, PortfolioMessageData<'_>> for PortfolioMes
 						};
 
 						buffer.push(CopyBufferEntry {
-							nodes: NodeGraphMessageHandler::copy_nodes(active_document.network(), &copy_ids).collect(),
+							nodes: NodeGraphMessageHandler::copy_nodes(
+								active_document.network(),
+								&active_document.node_graph_handler.network,
+								&active_document.node_graph_handler.resolved_types,
+								&copy_ids,
+							)
+							.collect(),
 							selected: active_document.selected_nodes.selected_layers_contains(layer, active_document.metadata()),
 							visible: active_document.selected_nodes.layer_visible(layer, active_document.metadata()),
 							locked: active_document.selected_nodes.layer_locked(layer, active_document.metadata()),
@@ -378,6 +384,9 @@ impl MessageHandler<PortfolioMessage, PortfolioMessageData<'_>> for PortfolioMes
 						}
 					}
 				}
+
+				// TODO: Eventually remove this (probably starting late 2024)
+				responses.add(GraphOperationMessage::DeleteLegacyOutputNode);
 			}
 			PortfolioMessage::PasteIntoFolder { clipboard, parent, insert_index } => {
 				let paste = |entry: &CopyBufferEntry, responses: &mut VecDeque<_>| {

frontend/wasm/src/editor_api.rs
@@ -532,8 +532,8 @@ impl EditorHandle {
 	/// Set the name for the layer
 	#[wasm_bindgen(js_name = setLayerName)]
 	pub fn set_layer_name(&self, id: u64, name: String) {
-		let id = NodeId(id);
-		let message = NodeGraphMessage::SetName { node_id: id, name };
+		let layer = LayerNodeIdentifier::new_unchecked(NodeId(id));
+		let message = GraphOperationMessage::SetName { layer, name };
 		self.dispatch(message);
 	}
 
@@ -552,11 +552,11 @@ impl EditorHandle {
 	}
 
 	/// Notifies the backend that the user connected a node's primary output to one of another node's inputs
-	#[wasm_bindgen(js_name = connectNodesByLink)]
-	pub fn connect_nodes_by_link(&self, output_node: u64, output_node_connector_index: usize, input_node: u64, input_node_connector_index: usize) {
+	#[wasm_bindgen(js_name = connectNodesByWire)]
+	pub fn connect_nodes_by_wire(&self, output_node: u64, output_node_connector_index: usize, input_node: u64, input_node_connector_index: usize) {
 		let output_node = NodeId(output_node);
 		let input_node = NodeId(input_node);
-		let message = NodeGraphMessage::ConnectNodesByLink {
+		let message = NodeGraphMessage::ConnectNodesByWire {
 			output_node,
 			output_node_connector_index,
 			input_node,
@@ -601,7 +601,7 @@ impl EditorHandle {
 	#[wasm_bindgen(js_name = disconnectNodes)]
 	pub fn disconnect_nodes(&self, node_id: u64, input_index: usize) {
 		let node_id = NodeId(node_id);
-		let message = NodeGraphMessage::DisconnectNodes { node_id, input_index };
+		let message = NodeGraphMessage::DisconnectInput { node_id, input_index };
 		self.dispatch(message);
 	}
 
@@ -649,6 +649,13 @@ impl EditorHandle {
 		self.dispatch(message);
 	}
 
+	/// Go back a certain number of nested levels
+	#[wasm_bindgen(js_name = exitNestedNetwork)]
+	pub fn exit_nested_network(&self, steps_back: usize) {
+		let message = NodeGraphMessage::ExitNestedNetwork { steps_back };
+		self.dispatch(message);
+	}
+
 	/// Notifies the backend that the selected nodes have been moved
 	#[wasm_bindgen(js_name = moveSelectedNodes)]
 	pub fn move_selected_nodes(&self, displacement_x: i32, displacement_y: i32) {
@@ -684,8 +691,16 @@ impl EditorHandle {
 	}
 
 	/// Toggle visibility of a layer or node given its node ID
-	#[wasm_bindgen(js_name = toggleNodeVisibility)]
-	pub fn toggle_node_visibility(&self, id: u64) {
+	#[wasm_bindgen(js_name = toggleNodeVisibilityLayerPanel)]
+	pub fn toggle_node_visibility_layer(&self, id: u64) {
+		let node_id = NodeId(id);
+		let message = GraphOperationMessage::ToggleVisibility { node_id };
+		self.dispatch(message);
+	}
+
+	/// Toggle visibility of a layer or node given its node ID
+	#[wasm_bindgen(js_name = toggleNodeVisibilityGraph)]
+	pub fn toggle_node_visibility_graph(&self, id: u64) {
 		let node_id = NodeId(id);
 		let message = NodeGraphMessage::ToggleVisibility { node_id };
 		self.dispatch(message);
@@ -698,15 +713,15 @@ impl EditorHandle {
 		self.dispatch(message);
 
 		let id = NodeId(id);
-		let message = DocumentMessage::DeleteLayer { id };
+		let message = NodeGraphMessage::DeleteNodes { node_ids: vec![id], reconnect: true };
 		self.dispatch(message);
 	}
 
 	/// Toggle lock state of a layer from the layer list
 	#[wasm_bindgen(js_name = toggleLayerLock)]
 	pub fn toggle_layer_lock(&self, id: u64) {
-		let id = NodeId(id);
-		let message = NodeGraphMessage::ToggleLocked { node_id: id };
+		let node_id = NodeId(id);
+		let message = GraphOperationMessage::ToggleLocked { node_id };
 		self.dispatch(message);
 	}
 

commit 621f469a150d4a14c86ba9be87df82aae5dded74

Restore backwards compatibility broken with #1750 to prepare next release

editor/src/messages/portfolio/document/document_message_handler.rs
@@ -27,7 +27,6 @@ use graphene_core::raster::BlendMode;
 use graphene_core::raster::ImageFrame;
 use graphene_core::renderer::ClickTarget;
 use graphene_core::vector::style::ViewMode;
-use graphene_std::vector::style::{Fill, FillType, Gradient};
 
 use glam::{DAffine2, DVec2, IVec2};
 
@@ -1312,81 +1311,7 @@ impl DocumentMessageHandler {
 	}
 
 	pub fn deserialize_document(serialized_content: &str) -> Result<Self, EditorError> {
-		// serde_json::from_str(serialized_content).map_err(|e| EditorError::DocumentDeserialization(e.to_string()))
-
-		match serde_json::from_str::<Self>(serialized_content).map_err(|e| EditorError::DocumentDeserialization(e.to_string())) {
-			Ok(mut document) => {
-				for (_, node) in &mut document.network.nodes {
-					// Upgrade Fill nodes to the format change in #1778
-					// TODO: Eventually remove this (probably starting late 2024)
-					if node.name == "Fill" && node.inputs.len() == 8 {
-						let node_definition = crate::messages::portfolio::document::node_graph::document_node_types::resolve_document_node_type(&node.name).unwrap();
-						let default_definition_node = node_definition.default_document_node();
-
-						node.implementation = default_definition_node.implementation.clone();
-						let old_inputs = std::mem::replace(&mut node.inputs, default_definition_node.inputs.clone());
-
-						node.inputs[0] = old_inputs[0].clone();
-
-						let Some(fill_type) = old_inputs[1].as_value().cloned() else { continue };
-						let TaggedValue::FillType(fill_type) = fill_type else { continue };
-						let Some(solid_color) = old_inputs[2].as_value().cloned() else { continue };
-						let TaggedValue::OptionalColor(solid_color) = solid_color else { continue };
-						let Some(gradient_type) = old_inputs[3].as_value().cloned() else { continue };
-						let TaggedValue::GradientType(gradient_type) = gradient_type else { continue };
-						let Some(start) = old_inputs[4].as_value().cloned() else { continue };
-						let TaggedValue::DVec2(start) = start else { continue };
-						let Some(end) = old_inputs[5].as_value().cloned() else { continue };
-						let TaggedValue::DVec2(end) = end else { continue };
-						let Some(transform) = old_inputs[6].as_value().cloned() else { continue };
-						let TaggedValue::DAffine2(transform) = transform else { continue };
-						let Some(positions) = old_inputs[7].as_value().cloned() else { continue };
-						let TaggedValue::GradientStops(positions) = positions else { continue };
-
-						let fill = match (fill_type, solid_color) {
-							(FillType::Solid, None) => Fill::None,
-							(FillType::Solid, Some(color)) => Fill::Solid(color),
-							(FillType::Gradient, _) => Fill::Gradient(Gradient {
-								stops: positions,
-								gradient_type,
-								start,
-								end,
-								transform,
-							}),
-						};
-						node.inputs[1] = NodeInput::value(TaggedValue::Fill(fill.clone()), false);
-						match fill {
-							Fill::None => {
-								node.inputs[2] = NodeInput::value(TaggedValue::OptionalColor(None), false);
-							}
-							Fill::Solid(color) => {
-								node.inputs[2] = NodeInput::value(TaggedValue::OptionalColor(Some(color)), false);
-							}
-							Fill::Gradient(gradient) => {
-								node.inputs[3] = NodeInput::value(TaggedValue::Gradient(gradient), false);
-							}
-						}
-					}
-				}
-				Ok(document)
-			}
-			Err(e) => Err(e),
-		}
-
-		// TODO: This can be used, if uncommented, to upgrade demo artwork with outdated document node internals from their definitions. Delete when it's no longer needed.
-		// Used for upgrading old internal networks for demo artwork nodes. Will reset all node internals for any opened file
-		// match serde_json::from_str::<Self>(serialized_content).map_err(|e| EditorError::DocumentDeserialization(e.to_string())) {
-		// 	Ok(mut document) => {
-		// 		for (_, node) in &mut document.network.nodes {
-		// 			let node_definition = crate::messages::portfolio::document::node_graph::document_node_types::resolve_document_node_type(&node.name).unwrap();
-		// 			let default_definition_node = node_definition.default_document_node();
-
-		// 			node.implementation = default_definition_node.implementation.clone();
-		// 		}
-		// 		Ok(document)
-		// 	}
-		// 	Err(e) => Err(e),
-		// }
+		serde_json::from_str(serialized_content).map_err(|e| EditorError::DocumentDeserialization(e.to_string()))
 	}
 
 	pub fn with_name(name: String, ipp: &InputPreprocessorMessageHandler, responses: &mut VecDeque<Message>) -> Self {

editor/src/messages/portfolio/document/graph_operation/graph_operation_message.rs
@@ -32,8 +32,6 @@ pub enum GraphOperationMessage {
 		layer: LayerNodeIdentifier,
 		reconnect: bool,
 	},
-	// TODO: Eventually remove this (probably starting late 2024)
-	DeleteLegacyOutputNode,
 	DisconnectInput {
 		node_id: NodeId,
 		input_index: usize,

editor/src/messages/portfolio/document/graph_operation/graph_operation_message_handler.rs
@@ -141,12 +141,6 @@ impl MessageHandler<GraphOperationMessage, GraphOperationMessageData<'_>> for Gr
 				load_network_structure(document_network, document_metadata, collapsed);
 				responses.add(NodeGraphMessage::RunDocumentGraph);
 			}
-			// TODO: Eventually remove this (probably starting late 2024)
-			GraphOperationMessage::DeleteLegacyOutputNode => {
-				if document_network.nodes.iter().any(|(node_id, node)| node.name == "Output" && *node_id == NodeId(0)) {
-					ModifyInputsContext::delete_nodes(node_graph, document_network, selected_nodes, vec![NodeId(0)], true, responses, Vec::new());
-				}
-			}
 			// Make sure to also update NodeGraphMessage::DisconnectInput when changing this
 			GraphOperationMessage::DisconnectInput { node_id, input_index } => {
 				let Some(existing_input) = document_network

editor/src/messages/portfolio/portfolio_message_handler.rs
@@ -4,14 +4,18 @@ use crate::consts::DEFAULT_DOCUMENT_NAME;
 use crate::messages::dialog::simple_dialogs;
 use crate::messages::frontend::utility_types::FrontendDocumentDetails;
 use crate::messages::layout::utility_types::widget_prelude::*;
+use crate::messages::portfolio::document::graph_operation::utility_types::ModifyInputsContext;
 use crate::messages::portfolio::document::utility_types::clipboards::{Clipboard, CopyBufferEntry, INTERNAL_CLIPBOARD_COUNT};
+use crate::messages::portfolio::document::utility_types::nodes::SelectedNodes;
 use crate::messages::portfolio::document::DocumentMessageData;
 use crate::messages::prelude::*;
 use crate::messages::tool::utility_types::{HintData, HintGroup};
 use crate::node_graph_executor::{ExportConfig, NodeGraphExecutor};
 
-use graph_craft::document::NodeId;
+use graph_craft::document::value::TaggedValue;
+use graph_craft::document::{NodeId, NodeInput};
 use graphene_core::text::Font;
+use graphene_std::vector::style::{Fill, FillType, Gradient};
 
 use std::sync::Arc;
 
@@ -375,10 +379,104 @@ impl MessageHandler<PortfolioMessage, PortfolioMessageData<'_>> for PortfolioMes
 				document_is_saved,
 				document_serialized_content,
 			} => {
-				// TODO: Eventually remove this (probably starting late 2024)
-				let do_not_upgrade = document_name.contains("__DO_NOT_UPGRADE__");
+				let upgrade_from_before_editable_subgraphs = document_serialized_content.contains("node_output_index");
+				let upgrade_vector_manipulation_format = document_serialized_content.contains("ManipulatorGroupIds") && !document_name.contains("__DO_NOT_UPGRADE__");
 				let document_name = document_name.replace("__DO_NOT_UPGRADE__", "");
-				if document_serialized_content.contains("ManipulatorGroupIds") && !do_not_upgrade {
+
+				let document = DocumentMessageHandler::with_name_and_content(document_name.clone(), document_serialized_content);
+				let mut document = match document {
+					Ok(document) => document,
+					Err(e) => {
+						if !document_is_auto_saved {
+							responses.add(DialogMessage::DisplayDialogError {
+								title: "Failed to open document".to_string(),
+								description: e.to_string(),
+							});
+						}
+
+						return;
+					}
+				};
+
+				// TODO: Eventually remove this (probably starting late 2024)
+				// Upgrade all old nodes to support editable subgraphs introduced in #1750
+				if upgrade_from_before_editable_subgraphs {
+					for (_, node) in &mut document.network.nodes {
+						let node_definition = crate::messages::portfolio::document::node_graph::document_node_types::resolve_document_node_type(&node.name).unwrap();
+						let default_definition_node = node_definition.default_document_node();
+
+						node.implementation = default_definition_node.implementation.clone();
+					}
+				}
+				if document.network.nodes.iter().any(|(node_id, node)| node.name == "Output" && *node_id == NodeId(0)) {
+					ModifyInputsContext::delete_nodes(
+						&mut document.node_graph_handler,
+						&mut document.network,
+						&mut SelectedNodes(vec![]),
+						vec![NodeId(0)],
+						true,
+						responses,
+						Vec::new(),
+					);
+				}
+
+				// TODO: Eventually remove this (probably starting late 2024)
+				// Upgrade Fill nodes to the format change in #1778
+				for (_, node) in &mut document.network.nodes {
+					if node.name == "Fill" && node.inputs.len() == 8 {
+						let node_definition = crate::messages::portfolio::document::node_graph::document_node_types::resolve_document_node_type(&node.name).unwrap();
+						let default_definition_node = node_definition.default_document_node();
+
+						node.implementation = default_definition_node.implementation.clone();
+						let old_inputs = std::mem::replace(&mut node.inputs, default_definition_node.inputs.clone());
+
+						node.inputs[0] = old_inputs[0].clone();
+
+						let Some(fill_type) = old_inputs[1].as_value().cloned() else { continue };
+						let TaggedValue::FillType(fill_type) = fill_type else { continue };
+						let Some(solid_color) = old_inputs[2].as_value().cloned() else { continue };
+						let TaggedValue::OptionalColor(solid_color) = solid_color else { continue };
+						let Some(gradient_type) = old_inputs[3].as_value().cloned() else { continue };
+						let TaggedValue::GradientType(gradient_type) = gradient_type else { continue };
+						let Some(start) = old_inputs[4].as_value().cloned() else { continue };
+						let TaggedValue::DVec2(start) = start else { continue };
+						let Some(end) = old_inputs[5].as_value().cloned() else { continue };
+						let TaggedValue::DVec2(end) = end else { continue };
+						let Some(transform) = old_inputs[6].as_value().cloned() else { continue };
+						let TaggedValue::DAffine2(transform) = transform else { continue };
+						let Some(positions) = old_inputs[7].as_value().cloned() else { continue };
+						let TaggedValue::GradientStops(positions) = positions else { continue };
+
+						let fill = match (fill_type, solid_color) {
+							(FillType::Solid, None) => Fill::None,
+							(FillType::Solid, Some(color)) => Fill::Solid(color),
+							(FillType::Gradient, _) => Fill::Gradient(Gradient {
+								stops: positions,
+								gradient_type,
+								start,
+								end,
+								transform,
+							}),
+						};
+						node.inputs[1] = NodeInput::value(TaggedValue::Fill(fill.clone()), false);
+						match fill {
+							Fill::None => {
+								node.inputs[2] = NodeInput::value(TaggedValue::OptionalColor(None), false);
+							}
+							Fill::Solid(color) => {
+								node.inputs[2] = NodeInput::value(TaggedValue::OptionalColor(Some(color)), false);
+							}
+							Fill::Gradient(gradient) => {
+								node.inputs[3] = NodeInput::value(TaggedValue::Gradient(gradient), false);
+							}
+						}
+					}
+				}
+
+				// TODO: Eventually remove this (probably starting late 2024)
+				// Upgrade document to the new vector manipulation format introduced in #1676
+				let document_serialized_content = document.serialize_document();
+				if upgrade_vector_manipulation_format && document_serialized_content != "" {
 					responses.add(FrontendMessage::TriggerUpgradeDocumentToVectorManipulationFormat {
 						document_id,
 						document_name,
@@ -389,25 +487,9 @@ impl MessageHandler<PortfolioMessage, PortfolioMessageData<'_>> for PortfolioMes
 					return;
 				}
 
-				let document = DocumentMessageHandler::with_name_and_content(document_name, document_serialized_content);
-				match document {
-					Ok(mut document) => {
-						document.set_auto_save_state(document_is_auto_saved);
-						document.set_save_state(document_is_saved);
-						self.load_document(document, document_id, responses);
-					}
-					Err(e) => {
-						if !document_is_auto_saved {
-							responses.add(DialogMessage::DisplayDialogError {
-								title: "Failed to open document".to_string(),
-								description: e.to_string(),
-							});
-						}
-					}
-				}
-
-				// TODO: Eventually remove this (probably starting late 2024)
-				responses.add(GraphOperationMessage::DeleteLegacyOutputNode);
+				document.set_auto_save_state(document_is_auto_saved);
+				document.set_save_state(document_is_saved);
+				self.load_document(document, document_id, responses);
 			}
 			PortfolioMessage::PasteIntoFolder { clipboard, parent, insert_index } => {
 				let paste = |entry: &CopyBufferEntry, responses: &mut VecDeque<_>| {

frontend/wasm/src/editor_api.rs
@@ -660,8 +660,6 @@ impl EditorHandle {
 		let (response_sender, _) = std::sync::mpsc::channel();
 		let old_runtime = replace_node_runtime(NodeRuntime::new(request_receiver, response_sender));
 
-		let document_serialized_content = document_serialized_content.replace("\"ManipulatorGroupIds\"", "\"PointIds\"");
-
 		let mut editor = Editor::new();
 		let document_id = DocumentId(document_id);
 		editor.handle_message(PortfolioMessage::OpenDocumentFileWithId {

node-graph/graph-craft/src/document/value.rs
@@ -155,6 +155,7 @@ tagged_value! {
 	GradientStops(graphene_core::vector::style::GradientStops),
 	Quantization(graphene_core::quantization::QuantizationChannels),
 	OptionalColor(Option<graphene_core::raster::color::Color>),
+	#[serde(alias = "ManipulatorGroupIds")] // TODO: Eventually remove this alias (probably starting late 2024)
 	PointIds(Vec<graphene_core::vector::PointId>),
 	Font(graphene_core::text::Font),
 	BrushStrokes(Vec<graphene_core::vector::brush_stroke::BrushStroke>),