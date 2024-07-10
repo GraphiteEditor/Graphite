6cf020b28da6ac6cf4f6b0dc811db9d403b063a3
Dennis Kobert <dennis@kobert.dev>
2024-07-10 11:26:48 +0200

    Use stable toolchain in nix shell again

--- shell.nix
+++ shell.nix
@@ -28,10 +28,10 @@ let
   };
 
   # Define the rustc we need
-  rustc-wasm = pkgs.rust-bin.nightly.latest.default.override {
+  rustc-wasm = pkgs.rust-bin.stable.latest.default.override {
     targets = [ "wasm32-unknown-unknown" ];
     # wasm-pack needs this
-    extensions = [ "rust-src" "rust-analyzer" "clippy" "miri"];
+    extensions = [ "rust-src" "rust-analyzer" "clippy"];
   };
 in
   # Make a shell with the dependencies we need
