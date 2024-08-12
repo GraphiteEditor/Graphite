fe28b42977638a51d0b5dd3a81693914c1d8351a
dennis@kobert.dev <dennis@kobert.dev>
2024-08-12 12:58:43 +0200

    Add profile run to ci

--- node-graph/graph-craft/benches/compile_demo_art.rs
+++ node-graph/graph-craft/benches/compile_demo_art.rs
@@ -1,28 +1,57 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use graph_craft::document::NodeNetwork;
 use graph_craft::graphene_compiler::Compiler;
 use graph_craft::proto::ProtoNetwork;
+use std::path::PathBuf;
 
-pub fn compile_to_proto(c: &mut Criterion) {
-	let artworks = glob::glob("../../demo-artwork/*.graphite").expect("failed to read glob pattern");
-	for path in artworks {
-		let Ok(path) = path else { continue };
-		let content = std::fs::read(&path).expect("failed to read file");
-		let network = load_network(std::str::from_utf8(&content).unwrap());
-		let name = path.file_stem().unwrap().to_str().unwrap();
+#[cfg(feature = "criterion")]
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-		c.bench_function(name, |b| b.iter_batched(|| network.clone(), |network| compile(black_box(network)), criterion::BatchSize::SmallInput));
-	}
-}
+#[cfg(feature = "iai")]
+use iai_callgrind::{black_box, library_benchmark, library_benchmark_group, main};
 
 fn load_network(document_string: &str) -> NodeNetwork {
 	let document: serde_json::Value = serde_json::from_str(document_string).expect("Failed to parse document");
 	serde_json::from_value::<NodeNetwork>(document["network_interface"]["network"].clone()).expect("Failed to parse document")
 }
+
 fn compile(network: NodeNetwork) -> ProtoNetwork {
 	let compiler = Compiler {};
 	compiler.compile_single(network).unwrap()
 }
 
+fn bench_compile(path: PathBuf) {
+	let content = std::fs::read(&path).expect("failed to read file");
+	let network = load_network(std::str::from_utf8(&content).unwrap());
+	black_box(compile(black_box(network)));
+}
+
+#[cfg(feature = "criterion")]
+fn compile_to_proto(c: &mut Criterion) {
+	let artworks = glob::glob("../../demo-artwork/*.graphite").expect("failed to read glob pattern");
+	for path in artworks {
+		let Ok(path) = path else { continue };
+		let name = path.file_stem().unwrap().to_str().unwrap();
+		c.bench_function(name, |b| b.iter(|| bench_compile(path.clone())));
+	}
+}
+
+#[cfg_attr(feature = "iai", library_benchmark)]
+fn iai_compile_to_proto() {
+	let artworks = glob::glob("../../demo-artwork/*.graphite").expect("failed to read glob pattern");
+	for path in artworks {
+		let Ok(path) = path else { continue };
+		bench_compile(path);
+	}
+}
+
+#[cfg(feature = "criterion")]
 criterion_group!(benches, compile_to_proto);
+
+#[cfg(feature = "criterion")]
 criterion_main!(benches);
+
+#[cfg(feature = "iai")]
+library_benchmark_group!(name = compile_group; benchmarks = iai_compile_to_proto);
+
+#[cfg(feature = "iai")]
+main!(library_benchmark_groups = compile_group);
