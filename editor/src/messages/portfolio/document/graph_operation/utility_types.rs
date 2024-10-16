GraphiteEditor/Graphite

commit e09f5ecaecad5d33b8ca20894961428b9ab479dc

Make Copy to Points and (Circular) Repeat and nodes output group data, and add flattening nodes (#2011)
* Output group from repeat, add flatten vector elements

* Fix tests

* Fix demo artwork

* Output group from copy to points, add repeat for graphic groups, fix editor freeze on render fail

* Restore painted dreams

* WIP: Fix demo artwork

* Fix demo artwork, add ungroup node

* Incorrect scaling

* fix test

* Fix demo art

---------

Co-authored-by: Keavon Chambers <keavon@keavon.com>

editor/src/messages/portfolio/document/graph_operation/utility_types.rs
@@ -239,18 +239,28 @@ impl<'a> ModifyInputsContext<'a> {
 	// Gets the node id of a node with a specific reference that is upstream from the layer node, and creates it if it does not exist
 	pub fn existing_node_id(&mut self, reference: &'static str) -> Option<NodeId> {
 		// Start from the layer node or export
-		let node_id = self.get_output_layer().map_or(Vec::new(), |output_layer| vec![output_layer.to_node()]);
+		let output_layer = self.get_output_layer()?;
+		let layer_input_type = self.network_interface.input_type(&InputConnector::node(output_layer.to_node(), 1), &[]).0.nested_type();
 
-		let upstream = self.network_interface.upstream_flow_back_from_nodes(node_id, &[], network_interface::FlowType::HorizontalFlow);
-
-		let is_traversal_start = |node_id: NodeId| {
-			self.layer_node.map(|layer| layer.to_node()) == Some(node_id) || self.network_interface.network(&[]).unwrap().exports.iter().any(|export| export.as_node() == Some(node_id))
-		};
+		let upstream = self
+			.network_interface
+			.upstream_flow_back_from_nodes(vec![output_layer.to_node()], &[], network_interface::FlowType::HorizontalFlow);
 
 		// Take until another layer node is found (but not the first layer node)
-		let existing_node_id = upstream
-			.take_while(|node_id| is_traversal_start(*node_id) || !self.network_interface.is_layer(node_id, &[]))
-			.find(|node_id| self.network_interface.reference(node_id, &[]).is_some_and(|node_reference| node_reference == reference));
+		let mut existing_node_id = None;
+		for upstream_node in upstream.collect::<Vec<_>>() {
+			let upstream_node_input_type = self.network_interface.input_type(&InputConnector::node(upstream_node, 0), &[]).0.nested_type();
+			let is_traversal_start = |node_id: NodeId| {
+				self.layer_node.map(|layer| layer.to_node()) == Some(node_id) || self.network_interface.network(&[]).unwrap().exports.iter().any(|export| export.as_node() == Some(node_id))
+			};
+			if !is_traversal_start(upstream_node) && (self.network_interface.is_layer(&upstream_node, &[]) || upstream_node_input_type != layer_input_type) {
+				break;
+			}
+			if self.network_interface.reference(&upstream_node, &[]).is_some_and(|node_reference| node_reference == reference) {
+				existing_node_id = Some(upstream_node);
+				break;
+			}
+		}
 
 		// Create a new node if the node does not exist and update its inputs
 		existing_node_id.or_else(|| {

editor/src/messages/portfolio/document/node_graph/node_graph_message_handler.rs
@@ -752,8 +752,6 @@ impl<'a> MessageHandler<NodeGraphMessage, NodeGraphHandlerData<'a>> for NodeGrap
 					return;
 				};
 
-				responses.add(DocumentMessage::EndTransaction);
-
 				if let Some(preview_node) = self.preview_on_mouse_up {
 					responses.add(NodeGraphMessage::TogglePreview { node_id: preview_node });
 					self.preview_on_mouse_up = None;
@@ -992,6 +990,7 @@ impl<'a> MessageHandler<NodeGraphMessage, NodeGraphHandlerData<'a>> for NodeGrap
 				self.box_selection_start = None;
 				self.wire_in_progress_from_connector = None;
 				self.wire_in_progress_to_connector = None;
+				responses.add(DocumentMessage::EndTransaction);
 				responses.add(FrontendMessage::UpdateWirePathInProgress { wire_path: None });
 				responses.add(FrontendMessage::UpdateBox { box_selection: None })
 			}

editor/src/messages/portfolio/document/utility_types/network_interface.rs
@@ -3603,7 +3603,7 @@ impl NodeNetworkInterface {
 		};
 
 		// Check whether the being-deleted node's first (primary) input is a node
-		let mut reconnect_to_input = network.nodes.get(node_id).and_then(|node| {
+		let reconnect_to_input = network.nodes.get(node_id).and_then(|node| {
 			node.inputs
 				.iter()
 				.find(|input| input.is_exposed_to_frontend(network_path.is_empty()))
@@ -3625,15 +3625,22 @@ impl NodeNetworkInterface {
 
 		let mut reconnect_node = None;
 
+		// Don't reconnect if the upstream node is a layer and there are multiple downstream inputs to reconnect
+		let multiple_disconnect_and_upstream_is_layer = downstream_inputs_to_disconnect.len() > 1
+			&& reconnect_to_input
+				.as_ref()
+				.is_some_and(|upstream_input| upstream_input.as_node().map_or(false, |node_id| self.is_layer(&node_id, network_path)));
+
 		for downstream_input in &downstream_inputs_to_disconnect {
 			self.disconnect_input(downstream_input, network_path);
 			// Prevent reconnecting export to import until https://github.com/GraphiteEditor/Graphite/issues/1762 is solved
 			if !(matches!(reconnect_to_input, Some(NodeInput::Network { .. })) && matches!(downstream_input, InputConnector::Export(_))) {
-				if let Some(reconnect_input) = reconnect_to_input.take() {
-					// Get the reconnect node position only if it is in a stack
+				if let Some(reconnect_input) = &reconnect_to_input {
 					reconnect_node = reconnect_input.as_node().and_then(|node_id| if self.is_stack(&node_id, network_path) { Some(node_id) } else { None });
 					self.disconnect_input(&InputConnector::node(*node_id, 0), network_path);
-					self.set_input(downstream_input, reconnect_input, network_path);
+					if !multiple_disconnect_and_upstream_is_layer {
+						self.set_input(downstream_input, reconnect_input.clone(), network_path);
+					}
 				}
 			}
 		}

editor/src/messages/portfolio/portfolio_message_handler.rs
@@ -963,6 +963,7 @@ impl PortfolioMessageHandler {
 				/text>"#
 				// It's a mystery why the `/text>` tag above needs to be missing its `<`, but when it exists it prints the `<` character in the text. However this works with it removed.
 				.to_string();
+			responses.add(Message::EndBuffer(graphene_std::renderer::RenderMetadata::default()));
 			responses.add(FrontendMessage::UpdateDocumentArtwork { svg: error });
 		}
 		result

commit 9f7b3936f412ad88253ba38f3a9203827ed8b119

Fix brush tool broken by #2011 (#2045)
Brush tool fix

editor/src/messages/portfolio/document/graph_operation/utility_types.rs
@@ -250,14 +250,19 @@ impl<'a> ModifyInputsContext<'a> {
 		let mut existing_node_id = None;
 		for upstream_node in upstream.collect::<Vec<_>>() {
 			let upstream_node_input_type = self.network_interface.input_type(&InputConnector::node(upstream_node, 0), &[]).0.nested_type();
+
+			// Check if this is the node we have been searching for.
+			if self.network_interface.reference(&upstream_node, &[]).is_some_and(|node_reference| node_reference == reference) {
+				existing_node_id = Some(upstream_node);
+				break;
+			}
+
 			let is_traversal_start = |node_id: NodeId| {
 				self.layer_node.map(|layer| layer.to_node()) == Some(node_id) || self.network_interface.network(&[]).unwrap().exports.iter().any(|export| export.as_node() == Some(node_id))
 			};
+
+			// If the type changes then break?? This should at least be after checking if the node is correct (otherwise the brush tool breaks.)
 			if !is_traversal_start(upstream_node) && (self.network_interface.is_layer(&upstream_node, &[]) || upstream_node_input_type != layer_input_type) {
-				break;
-			}
-			if self.network_interface.reference(&upstream_node, &[]).is_some_and(|node_reference| node_reference == reference) {
-				existing_node_id = Some(upstream_node);
 				break;
 			}
 		}