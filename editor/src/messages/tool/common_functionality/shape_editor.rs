b0892598942c9e9ac94bc42c13cd470208754955
Keavon Chambers <keavon@keavon.com>
2024-11-05 19:35:40 -0800

    Make the code simpler and more self-descriptive

--- editor/src/messages/tool/common_functionality/shape_editor.rs
+++ editor/src/messages/tool/common_functionality/shape_editor.rs
@@ -619,7 +619,7 @@ impl ShapeState {
 		delta: DVec2,
 		equidistant: bool,
 		responses: &mut VecDeque<Message>,
-		keyboard: bool,
+		in_viewport_space: bool,
 	) {
 		for (&layer, state) in &self.selected_shape_state {
 			let Some(vector_data) = document.network_interface.compute_modified_vector(layer) else {
@@ -627,11 +627,8 @@ impl ShapeState {
 			};
 			let opposing_handles = handle_lengths.as_ref().and_then(|handle_lengths| handle_lengths.get(&layer));
 
-			let delta_keyboard = delta.clone();
 			let transform = document.metadata().transform_to_viewport(layer);
-			let delta_mouse = transform.inverse().transform_vector2(delta);
-
-			let delta = if keyboard { delta_keyboard } else { delta_mouse };
+			let delta = if in_viewport_space { transform.inverse().transform_vector2(delta) } else { delta };
 
 			for &point in state.selected_points.iter() {
 				let handle = match point {
