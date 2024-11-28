0a6fed99c84950d584712c01973ecb0e106b8b73
jacinyan <jacinjiyan@gmail.com>
2024-11-29 00:05:15 +1100

    Register Expression Node

--- node-graph/graph-craft/src/expression.rs
+++ node-graph/graph-craft/src/expression.rs
@@ -1,59 +1,52 @@
 use crate::document::value::TaggedValue;
-use dyn_any::DynAny;
 use graphene_core::Node;
 use math_parser::evaluate;
 use math_parser::value::{Number, Value};
-use std::future::Future;
-use std::pin::Pin;
 
 /// A node that evaluates mathematical expressions during graph runtime
 #[derive(Debug)]
-pub struct ExpressionNode {
-    /// The mathematical expression to evaluate
-    expression: String,
+pub struct ExpressionNode<T> {
+    expression: T,
 }
 
-impl ExpressionNode {
-    pub fn new(expression: String) -> Self {
-        Self { expression }
+impl<'i, T: Node<'i, ()>> Node<'i, ()> for ExpressionNode<T>
+where
+    T::Output: AsRef<str>,
+{
+    type Output = TaggedValue;
+
+    fn eval(&'i self, _input: ()) -> Self::Output {
+        let expression = self.expression.eval(());
+        match evaluate(expression.as_ref()) {
+            Ok((Ok(value), _)) => {
+                let Value::Number(num) = value;
+                match num {
+                    Number::Real(val) => TaggedValue::F64(val),
+                    Number::Complex(c) => TaggedValue::F64(c.re),
+                }
+            }
+            _ => TaggedValue::None,
+        }
     }
 }
 
-impl<'input> Node<'input, Box<dyn DynAny<'input> + Send>> for ExpressionNode {
-    type Output = Pin<Box<dyn Future<Output = Box<dyn DynAny<'input> + Send>> + Send + 'input>>;
-
-    fn eval(&'input self, _: Box<dyn DynAny<'input> + Send>) -> Self::Output {
-        let expression = self.expression.clone();
-        Box::pin(async move {
-            match evaluate(&expression) {
-                Ok((Ok(value), _)) => {
-                    let Value::Number(num) = value;
-                    match num {
-                        Number::Real(val) => Box::new(TaggedValue::F64(val)) as Box<dyn DynAny<'input> + Send>,
-                        Number::Complex(c) => Box::new(TaggedValue::F64(c.re)) as Box<dyn DynAny<'input> + Send>,
-                    }
-                }
-                _ => Box::new(TaggedValue::None) as Box<dyn DynAny<'input> + Send>,
-            }
-        })
+impl<T> ExpressionNode<T> {
+    pub fn new(expression: T) -> Self {
+        Self { expression }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use dyn_any::downcast_ref;
+    use graphene_core::value::ValueNode;
 
     #[test]
     fn test_expression_evaluation() {
-        let node = ExpressionNode::new("2 + 2".to_string());
-        let future = node.eval(Box::<()>::default());
-        let result = futures::executor::block_on(future);
-        let tagged_value = downcast_ref::<TaggedValue>(result.as_ref()).unwrap();
-        match tagged_value {
-            TaggedValue::F64(value) => {
-                assert_eq!(value, 4.0);
-            }
+        let node = ExpressionNode::new(ValueNode::new("2 + 2".to_string()));
+        let result = node.eval(());
+        match result {
+            TaggedValue::F64(value) => assert_eq!(value, 4.0),
             _ => panic!("Expected F64 value"),
         }
     }
