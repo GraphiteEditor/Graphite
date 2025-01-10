GraphiteEditor/Graphite

commit 3be7505faf0463dee0e48c7862741fd64eda63ac

Automatically place layers into the artboard they're drawn inside of (#2110)
* place new layer from basic tools within an artboard bounds

Signed-off-by: James Ryans <james.ryans2012@gmail.com>

* add for Text Tool

Signed-off-by: James Ryans <james.ryans2012@gmail.com>

* use click_xray function

Signed-off-by: James Ryans <james.ryans2012@gmail.com>

* support for freehand tool

Signed-off-by: James Ryans <james.ryans2012@gmail.com>

* support spline tool

Signed-off-by: James Ryans <james.ryans2012@gmail.com>

---------

Signed-off-by: James Ryans <james.ryans2012@gmail.com>

editor/src/messages/portfolio/document/document_message_handler.rs
@@ -1652,6 +1652,14 @@ impl DocumentMessageHandler {
 		}
 	}
 
+	/// Finds the artboard that bounds the point in viewport space and be the container of any newly added layers.
+	pub fn new_layer_bounding_artboard(&self, ipp: &InputPreprocessorMessageHandler) -> LayerNodeIdentifier {
+		self.click_xray(ipp)
+			.filter(|layer| self.network_interface.is_artboard(&layer.to_node(), &[]))
+			.next()
+			.unwrap_or(LayerNodeIdentifier::ROOT_PARENT)
+	}
+
 	/// Finds the parent folder which, based on the current selections, should be the container of any newly added layers.
 	pub fn new_layer_parent(&self, include_self: bool) -> LayerNodeIdentifier {
 		self.network_interface

editor/src/messages/tool/tool_messages/ellipse_tool.rs
@@ -205,7 +205,7 @@ impl Fsm for EllipseToolFsmState {
 				let node = node_type.node_template_input_override([None, Some(NodeInput::value(TaggedValue::F64(0.5), false)), Some(NodeInput::value(TaggedValue::F64(0.5), false))]);
 				let nodes = vec![(NodeId(0), node)];
 
-				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_parent(true), responses);
+				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_bounding_artboard(input), responses);
 				responses.add(Message::StartBuffer);
 				responses.add(GraphOperationMessage::TransformSet {
 					layer,

editor/src/messages/tool/tool_messages/freehand_tool.rs
@@ -238,7 +238,7 @@ impl Fsm for FreehandToolFsmState {
 
 				responses.add(DocumentMessage::DeselectAllLayers);
 
-				let parent = document.new_layer_parent(true);
+				let parent = document.new_layer_bounding_artboard(input);
 
 				let node_type = resolve_document_node_type("Path").expect("Path node does not exist");
 				let node = node_type.default_node_template();

editor/src/messages/tool/tool_messages/line_tool.rs
@@ -186,7 +186,7 @@ impl Fsm for LineToolFsmState {
 				]);
 				let nodes = vec![(NodeId(0), node)];
 
-				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_parent(false), responses);
+				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_bounding_artboard(input), responses);
 				responses.add(Message::StartBuffer);
 				responses.add(GraphOperationMessage::TransformSet {
 					layer,

editor/src/messages/tool/tool_messages/pen_tool.rs
@@ -492,7 +492,7 @@ impl PenToolData {
 		let node_type = resolve_document_node_type("Path").expect("Path node does not exist");
 		let nodes = vec![(NodeId(0), node_type.default_node_template())];
 
-		let parent = document.new_layer_parent(true);
+		let parent = document.new_layer_bounding_artboard(input);
 		let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, parent, responses);
 		tool_options.fill.apply_fill(layer, responses);
 		tool_options.stroke.apply_stroke(tool_options.line_weight, layer, responses);

editor/src/messages/tool/tool_messages/polygon_tool.rs
@@ -264,7 +264,7 @@ impl Fsm for PolygonToolFsmState {
 
 				let nodes = vec![(NodeId(0), node)];
 
-				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_parent(false), responses);
+				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_bounding_artboard(input), responses);
 				responses.add(Message::StartBuffer);
 				responses.add(GraphOperationMessage::TransformSet {
 					layer,

editor/src/messages/tool/tool_messages/rectangle_tool.rs
@@ -211,7 +211,7 @@ impl Fsm for RectangleToolFsmState {
 				let node = node_type.node_template_input_override([None, Some(NodeInput::value(TaggedValue::F64(1.), false)), Some(NodeInput::value(TaggedValue::F64(1.), false))]);
 				let nodes = vec![(NodeId(0), node)];
 
-				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_parent(true), responses);
+				let layer = graph_modification_utils::new_custom(NodeId::new(), nodes, document.new_layer_bounding_artboard(input), responses);
 				responses.add(Message::StartBuffer);
 				responses.add(GraphOperationMessage::TransformSet {
 					layer,

editor/src/messages/tool/tool_messages/spline_tool.rs
@@ -203,7 +203,7 @@ impl Fsm for SplineToolFsmState {
 				responses.add(DocumentMessage::StartTransaction);
 				responses.add(DocumentMessage::DeselectAllLayers);
 
-				let parent = document.new_layer_parent(true);
+				let parent = document.new_layer_bounding_artboard(input);
 
 				tool_data.weight = tool_options.line_weight;
 

commit 51d1c4eeacec6e4baf095fae8c5095b768391e93

Fix drawing new layers not being put next to nested selected layers
A regression introduced in #2110

editor/src/messages/portfolio/document/document_message_handler.rs
@@ -1654,9 +1654,18 @@ impl DocumentMessageHandler {
 
 	/// Finds the artboard that bounds the point in viewport space and be the container of any newly added layers.
 	pub fn new_layer_bounding_artboard(&self, ipp: &InputPreprocessorMessageHandler) -> LayerNodeIdentifier {
-		self.click_xray(ipp)
+		let container_based_on_selection = self.new_layer_parent(true);
+
+		let container_based_on_clicked_artboard = self
+			.click_xray(ipp)
 			.find(|layer| self.network_interface.is_artboard(&layer.to_node(), &[]))
-			.unwrap_or(LayerNodeIdentifier::ROOT_PARENT)
+			.unwrap_or(LayerNodeIdentifier::ROOT_PARENT);
+
+		if container_based_on_selection.ancestors(self.metadata()).any(|ancestor| ancestor == container_based_on_clicked_artboard) {
+			container_based_on_selection
+		} else {
+			container_based_on_clicked_artboard
+		}
 	}
 
 	/// Finds the parent folder which, based on the current selections, should be the container of any newly added layers.