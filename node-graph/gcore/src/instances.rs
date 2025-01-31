8cc623c3712f3340e2d655d3f973294bded3fe81
Keavon Chambers <keavon@keavon.com>
2025-01-30 21:47:40 -0800

    Fix domain data structure field plural naming

--- node-graph/gcore/src/instances.rs
+++ node-graph/gcore/src/instances.rs
@@ -11,34 +11,35 @@ where
 	T: Into<GraphicElement> + StaticType + 'static,
 {
 	id: Vec<InstanceId>,
-	instances: Vec<T>,
+	#[serde(alias = "instances")]
+	instance: Vec<T>,
 }
 
 impl<T: Into<GraphicElement> + StaticType + 'static> Instances<T> {
 	pub fn new(instance: T) -> Self {
 		Self {
 			id: vec![InstanceId::generate()],
-			instances: vec![instance],
+			instance: vec![instance],
 		}
 	}
 
 	pub fn one_item(&self) -> &T {
-		self.instances.first().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {} (one_item)", self.instances.len()))
+		self.instance.first().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {} (one_item)", self.instance.len()))
 	}
 
 	pub fn one_item_mut(&mut self) -> &mut T {
-		let length = self.instances.len();
-		self.instances.first_mut().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {} (one_item_mut)", length))
+		let length = self.instance.len();
+		self.instance.first_mut().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {} (one_item_mut)", length))
 	}
 
 	pub fn instances(&self) -> impl Iterator<Item = &T> {
-		assert!(self.instances.len() == 1, "ONE INSTANCE EXPECTED, FOUND {} (instances)", self.instances.len());
-		self.instances.iter()
+		assert!(self.instance.len() == 1, "ONE INSTANCE EXPECTED, FOUND {} (instances)", self.instance.len());
+		self.instance.iter()
 	}
 
 	pub fn instances_mut(&mut self) -> impl Iterator<Item = &mut T> {
-		assert!(self.instances.len() == 1, "ONE INSTANCE EXPECTED, FOUND {} (instances_mut)", self.instances.len());
-		self.instances.iter_mut()
+		assert!(self.instance.len() == 1, "ONE INSTANCE EXPECTED, FOUND {} (instances_mut)", self.instance.len());
+		self.instance.iter_mut()
 	}
 
 	// pub fn id(&self) -> impl Iterator<Item = InstanceId> + '_ {
@@ -70,7 +71,7 @@ impl<T: Into<GraphicElement> + Default + Hash + StaticType + 'static> Default fo
 impl<T: Into<GraphicElement> + Hash + StaticType + 'static> core::hash::Hash for Instances<T> {
 	fn hash<H: core::hash::Hasher>(&self, state: &mut H) {
 		self.id.hash(state);
-		for instance in &self.instances {
+		for instance in &self.instance {
 			instance.hash(state);
 		}
 	}
@@ -78,7 +79,7 @@ impl<T: Into<GraphicElement> + Hash + StaticType + 'static> core::hash::Hash for
 
 impl<T: Into<GraphicElement> + PartialEq + StaticType + 'static> PartialEq for Instances<T> {
 	fn eq(&self, other: &Self) -> bool {
-		self.id == other.id && self.instances.len() == other.instances.len() && { self.instances.iter().zip(other.instances.iter()).all(|(a, b)| a == b) }
+		self.id == other.id && self.instance.len() == other.instance.len() && { self.instance.iter().zip(other.instance.iter()).all(|(a, b)| a == b) }
 	}
 }
 
