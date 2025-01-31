d59651ee38997c102042ee41c704e436d0bd5958
Keavon Chambers <keavon@keavon.com>
2025-01-30 23:07:07 -0800

    Add transform and alpha_blending fields to Instances<T>

--- node-graph/gcore/src/instances.rs
+++ node-graph/gcore/src/instances.rs
@@ -1,17 +1,22 @@
 use crate::vector::InstanceId;
-use crate::GraphicElement;
+use crate::{AlphaBlending, GraphicElement};
 
 use dyn_any::StaticType;
+use glam::DAffine2;
 use std::hash::Hash;
 
 #[derive(Copy, Clone, Debug)]
 pub struct Instance<'a, T> {
 	pub id: &'a InstanceId,
 	pub instance: &'a T,
+	pub transform: &'a DAffine2,
+	pub alpha_blending: &'a AlphaBlending,
 }
 pub struct InstanceMut<'a, T> {
 	pub id: &'a mut InstanceId,
 	pub instance: &'a mut T,
+	pub transform: &'a mut DAffine2,
+	pub alpha_blending: &'a mut AlphaBlending,
 }
 
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
@@ -22,6 +27,10 @@ where
 	id: Vec<InstanceId>,
 	#[serde(alias = "instances")]
 	instance: Vec<T>,
+	#[serde(default = "one_daffine2_default")]
+	transform: Vec<DAffine2>,
+	#[serde(default = "one_alpha_blending_default")]
+	alpha_blending: Vec<AlphaBlending>,
 }
 
 impl<T: Into<GraphicElement> + StaticType + 'static> Instances<T> {
@@ -29,6 +38,8 @@ impl<T: Into<GraphicElement> + StaticType + 'static> Instances<T> {
 		Self {
 			id: vec![InstanceId::generate()],
 			instance: vec![instance],
+			transform: vec![DAffine2::IDENTITY],
+			alpha_blending: vec![AlphaBlending::default()],
 		}
 	}
 
@@ -36,6 +47,8 @@ impl<T: Into<GraphicElement> + StaticType + 'static> Instances<T> {
 		Instance {
 			id: self.id.first().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", self.instance.len())),
 			instance: self.instance.first().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", self.instance.len())),
+			transform: self.transform.first().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", self.instance.len())),
+			alpha_blending: self.alpha_blending.first().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", self.instance.len())),
 		}
 	}
 
@@ -48,17 +61,39 @@ impl<T: Into<GraphicElement> + StaticType + 'static> Instances<T> {
 		InstanceMut {
 			id: self.id.first_mut().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", length)),
 			instance: self.instance.first_mut().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", length)),
+			transform: self.transform.first_mut().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", length)),
+			alpha_blending: self.alpha_blending.first_mut().unwrap_or_else(|| panic!("ONE INSTANCE EXPECTED, FOUND {}", length)),
 		}
 	}
 
 	pub fn instances(&self) -> impl Iterator<Item = Instance<T>> {
 		assert!(self.instance.len() == 1, "ONE INSTANCE EXPECTED, FOUND {} (instances)", self.instance.len());
-		self.id.iter().zip(self.instance.iter()).map(|(id, instance)| Instance { id, instance })
+		self.id
+			.iter()
+			.zip(self.instance.iter())
+			.zip(self.transform.iter())
+			.zip(self.alpha_blending.iter())
+			.map(|(((id, instance), transform), alpha_blending)| Instance {
+				id,
+				instance,
+				transform,
+				alpha_blending,
+			})
 	}
 
 	pub fn instances_mut(&mut self) -> impl Iterator<Item = InstanceMut<T>> {
 		assert!(self.instance.len() == 1, "ONE INSTANCE EXPECTED, FOUND {} (instances_mut)", self.instance.len());
-		self.id.iter_mut().zip(self.instance.iter_mut()).map(|(id, instance)| InstanceMut { id, instance })
+		self.id
+			.iter_mut()
+			.zip(self.instance.iter_mut())
+			.zip(self.transform.iter_mut())
+			.zip(self.alpha_blending.iter_mut())
+			.map(|(((id, instance), transform), alpha_blending)| InstanceMut {
+				id,
+				instance,
+				transform,
+				alpha_blending,
+			})
 	}
 
 	// pub fn id(&self) -> impl Iterator<Item = InstanceId> + '_ {
@@ -104,4 +139,11 @@ impl<T: Into<GraphicElement> + PartialEq + StaticType + 'static> PartialEq for I
 
 unsafe impl<T: Into<GraphicElement> + StaticType + 'static> dyn_any::StaticType for Instances<T> {
 	type Static = Instances<T>;
+}
+
+fn one_daffine2_default() -> Vec<DAffine2> {
+	vec![DAffine2::IDENTITY]
+}
+fn one_alpha_blending_default() -> Vec<AlphaBlending> {
+	vec![AlphaBlending::default()]
 }
