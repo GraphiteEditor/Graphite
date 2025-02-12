b861becd497ca7c721d2567d23827e5cc6c2b87a
Dennis Kobert <dennis@kobert.dev>
2025-02-13 00:54:13 +0100

    Remove unused plugins

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -66,14 +66,7 @@ async fn main() {
 	});
 
 	tauri::Builder::default()
-		.plugin(tauri_plugin_notification::init())
-		.plugin(tauri_plugin_dialog::init())
-		.plugin(tauri_plugin_os::init())
-		.plugin(tauri_plugin_clipboard_manager::init())
 		.plugin(tauri_plugin_http::init())
-		.plugin(tauri_plugin_process::init())
-		.plugin(tauri_plugin_fs::init())
-		.plugin(tauri_plugin_global_shortcut::Builder::new().build())
 		.plugin(tauri_plugin_shell::init())
 		.invoke_handler(tauri::generate_handler![set_random_seed, handle_message])
 		.setup(|_app| {
