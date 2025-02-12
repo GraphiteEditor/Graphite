3e6fa7f1ead32130654fccfcb9ac95708240e728
Dennis Kobert <dennis@kobert.dev>
2025-02-12 19:34:40 +0100

    Migrate tauri app to v2

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -66,10 +66,19 @@ async fn main() {
 	});
 
 	tauri::Builder::default()
+		.plugin(tauri_plugin_notification::init())
+		.plugin(tauri_plugin_dialog::init())
+		.plugin(tauri_plugin_os::init())
+		.plugin(tauri_plugin_clipboard_manager::init())
+		.plugin(tauri_plugin_http::init())
+		.plugin(tauri_plugin_process::init())
+		.plugin(tauri_plugin_fs::init())
+		.plugin(tauri_plugin_global_shortcut::Builder::new().build())
+		.plugin(tauri_plugin_shell::init())
 		.invoke_handler(tauri::generate_handler![set_random_seed, handle_message])
 		.setup(|_app| {
 			use tauri::Manager;
-			_app.get_window("main").unwrap().open_devtools();
+			_app.get_webview_window("main").unwrap().open_devtools();
 			Ok(())
 		})
 		.run(tauri::generate_context!())
