GraphiteEditor/Graphite

commit ec8c8d648539a1f3ab1c63d2b937a87882e90f14

Polish and add aborting to several input widgets: no Esc closing parent menus; color picker axis align; repeat on arrow buttons (#2276)
* Remove color input outline; reduce antialiasing compositing artifacts in color widgets

* Rename ColorButton to ColorInput

* Add features and aborting to several other widgets

- Prevent Esc from closing parent floating menus when aborting
- Fix missing icon regression
- Gutter resizing abort
- Color picker aborts, Shift axis alignment, improve click/drag behavior for gradient spectrum
- Scrollbar abort, repeat when held, fix directional arrows when viewport is zoomed
- Number input abort, repeat when held

* Move ColorInput into the inputs folder

* Fix tiny logo

frontend/src/components/widgets/inputs/NumberInput.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { createEventDispatcher, onMount, onDestroy } from "svelte";
 
+	import { PRESS_REPEAT_DELAY_MS, PRESS_REPEAT_INTERVAL_MS } from "@graphite/io-managers/input";
 	import { type NumberInputMode, type NumberInputIncrementBehavior } from "@graphite/messages";
 	import { evaluateMathExpression } from "@graphite-frontend/wasm/pkg/graphite_wasm.js";
 
+	import { preventEscapeClosingParentFloatingMenu } from "@graphite/components/layout/FloatingMenu.svelte";
 	import FieldInput from "@graphite/components/widgets/inputs/FieldInput.svelte";
 
 	const BUTTONS_LEFT = 0b0000_0001;
@@ -59,8 +61,10 @@
 	let self: FieldInput | undefined;
 	let inputRangeElement: HTMLInputElement | undefined;
 	let text = displayText(value, unit);
-	let isDragging = false;
 	let editing = false;
+	let isDragging = false;
+	let pressingArrow = false;
+	let repeatTimeout: ReturnType<typeof setTimeout> | undefined = undefined;
 	// Stays in sync with a binding to the actual input range slider element.
 	let rangeSliderValue = value !== undefined ? value : 0;
 	// Value used to render the position of the fake slider when applicable, and length of the progress colored region to the slider's left.
@@ -230,8 +234,8 @@
 	// INCREMENT MODE: ARROW BUTTONS
 	// =============================
 
-	function onIncrement(direction: "Decrease" | "Increase") {
-		if (value === undefined) return;
+	function onIncrementPointerDown(e: PointerEvent, direction: "Decrease" | "Increase") {
+		if (value === undefined || e.button !== BUTTON_LEFT) return;
 
 		const actions: Record<NumberInputIncrementBehavior, () => void> = {
 			Add: () => {
@@ -250,7 +254,41 @@
 			},
 			None: () => {},
 		};
-		actions[incrementBehavior]();
+
+		const sendAction = () => {
+			if (!pressingArrow) return;
+
+			actions[incrementBehavior]();
+
+			if (afterInitialDelay) repeatTimeout = setTimeout(sendAction, PRESS_REPEAT_INTERVAL_MS);
+			afterInitialDelay = true;
+		};
+
+		pressingArrow = true;
+		initialValueBeforeDragging = value;
+		let afterInitialDelay = false;
+		sendAction();
+		repeatTimeout = setTimeout(sendAction, PRESS_REPEAT_DELAY_MS);
+		addEventListener("keydown", incrementPressAbort);
+	}
+
+	function onIncrementPointerUp() {
+		pressingArrow = false;
+		clearTimeout(repeatTimeout);
+	}
+
+	function onIncrementMouseDown(e: MouseEvent) {
+		if (e.button === BUTTON_RIGHT) incrementPressAbort();
+	}
+
+	function incrementPressAbort() {
+		const element = self?.element() || undefined;
+		if (element) preventEscapeClosingParentFloatingMenu(element);
+
+		pressingArrow = false;
+		clearTimeout(repeatTimeout);
+		updateValue(initialValueBeforeDragging);
+		removeEventListener("keydown", onIncrementPointerUp);
 	}
 
 	// =======================================
@@ -399,8 +437,8 @@
 		if (rangeSliderClickDragState === "Aborted") {
 			// If we've just aborted the drag by right clicking, but the user hasn't yet released the left mouse button, Firefox treats
 			// some subsequent interactions with the slider (like that right mouse button release, or maybe mouse movement in some cases)
-			// as input changes to the slider position. Thus, until we leave the "Aborted" state by releasing all mouse buttons,
-			// we have to set the slider position back to currently intended value to fight against Firefox's attempts to let the user move it.
+			// as input changes to the slider position. Thus, until we leave the "Aborted" state by releasing all mouse buttons, we have
+			// to set the slider position back to the currently intended value to fight against Firefox's attempts to let the user move it.
 			updateValue(rangeSliderValueAsRendered);
 
 			// Now we exit early because we're ignoring further user input until the user releases all mouse buttons, which gets us back to the "Ready" state.
@@ -511,10 +549,7 @@
 		// Logic for aborting from pressing Escape.
 		if (e instanceof KeyboardEvent) {
 			// Detect if the user pressed Escape and abort the slider drag.
-			if (e.key === "Escape") {
-				// Call the abort helper function.
-				sliderAbort();
-			}
+			if (e.key === "Escape") sliderAbort(true);
 		}
 
 		// Logic for aborting from a right click.
@@ -522,7 +557,7 @@
 		// This handler's "pointermove" event will be fired upon right click even if the cursor didn't move, which is why it's okay to check in this event handler.
 		if (e instanceof PointerEvent && e.buttons & BUTTONS_RIGHT) {
 			// Call the abort helper function
-			sliderAbort();
+			sliderAbort(false);
 		}
 
 		// Recovery from the window losing focus while dragging the slider.
@@ -543,9 +578,11 @@
 	// During this momentary step, the slider hasn't moved yet but we want to allow aborting from this limbo state.
 	function sliderAbortFromMousedown(e: MouseEvent | KeyboardEvent) {
 		// Logic for aborting from a right click or pressing Escape.
-		if ((e instanceof KeyboardEvent && e.key === "Escape") || (e instanceof MouseEvent && e.button === BUTTON_RIGHT)) {
+		const abortWithEscape = e instanceof KeyboardEvent && e.key === "Escape";
+		const abortWithRightClick = e instanceof MouseEvent && e.button === BUTTON_RIGHT;
+		if (abortWithEscape || abortWithRightClick) {
 			// Call the abort helper function
-			sliderAbort();
+			sliderAbort(abortWithEscape);
 
 			// Clean up these event listeners because they were for getting us into this function and now we're done with them.
 			removeEventListener("mousedown", sliderAbortFromMousedown);
@@ -554,7 +591,10 @@
 	}
 
 	// Helper function that performs the state management and cleanup for aborting the slider drag.
-	function sliderAbort() {
+	function sliderAbort(abortWithEscape: boolean) {
+		const element = self?.element() || undefined;
+		if (abortWithEscape && element) preventEscapeClosingParentFloatingMenu(element);
+
 		// End the user's drag by instantaneously disabling and re-enabling the range input element
 		if (inputRangeElement) inputRangeElement.disabled = true;
 		setTimeout(() => {
@@ -580,9 +620,7 @@
 			// dragging the slider, hitting Escape, then releasing the mouse button. This results in being transferred by `onSliderInput()` to the
 			// "Deciding" state when we should remain in the "Ready" state as set here. (For debugging, this can be visualized in CSS by
 			// recoloring the fake slider handle, which is shown in the "Deciding" state.)
-			setTimeout(() => {
-				rangeSliderClickDragState = "Ready";
-			}, 0);
+			setTimeout(() => (rangeSliderClickDragState = "Ready"), 0);
 
 			// Clean up the event listener that was used to call this function.
 			removeEventListener("pointerup", sliderResetAbort);
@@ -617,8 +655,22 @@
 >
 	{#if value !== undefined}
 		{#if mode === "Increment" && incrementBehavior !== "None"}
-			<button class="arrow left" on:click={() => onIncrement("Decrease")} tabindex="-1" />
-			<button class="arrow right" on:click={() => onIncrement("Increase")} tabindex="-1" />
+			<button
+				class="arrow left"
+				on:pointerdown={(e) => onIncrementPointerDown(e, "Decrease")}
+				on:mousedown={onIncrementMouseDown}
+				on:pointerup={onIncrementPointerUp}
+				on:pointerleave={onIncrementPointerUp}
+				tabindex="-1"
+			></button>
+			<button
+				class="arrow right"
+				on:pointerdown={(e) => onIncrementPointerDown(e, "Increase")}
+				on:mousedown={onIncrementMouseDown}
+				on:pointerup={onIncrementPointerUp}
+				on:pointerleave={onIncrementPointerUp}
+				tabindex="-1"
+			></button>
 		{/if}
 		{#if mode === "Range"}
 			<input
@@ -684,6 +736,8 @@
 				border: none;
 				border-radius: 2px;
 				background: rgba(var(--color-1-nearblack-rgb), 0.5);
+				// An outline can appear when pressing the arrow button with left click then hitting Escape, so this stops that from showing
+				outline: none;
 
 				&:hover {
 					background: var(--color-4-dimgray);

editor/src/messages/layout/layout_message_handler.rs
@@ -80,7 +80,7 @@ impl LayoutMessageHandler {
 				};
 				responses.add(callback_message);
 			}
-			Widget::ColorButton(color_button) => {
+			Widget::ColorInput(color_button) => {
 				let callback_message = match action {
 					WidgetValueAction::Commit => (color_button.on_commit.callback)(&()),
 					WidgetValueAction::Update => {
@@ -99,7 +99,7 @@ impl LayoutMessageHandler {
 						};
 
 						(|| {
-							let update_value = value.as_object().expect("ColorButton update was not of type: object");
+							let update_value = value.as_object().expect("ColorInput update was not of type: object");
 
 							// None
 							let is_none = update_value.get("none").and_then(|x| x.as_bool());
@@ -139,7 +139,7 @@ impl LayoutMessageHandler {
 								return (color_button.on_update.callback)(color_button);
 							}
 
-							panic!("ColorButton update was not able to be parsed with color data: {color_button:?}");
+							panic!("ColorInput update was not able to be parsed with color data: {color_button:?}");
 						})()
 					}
 				};

editor/src/messages/layout/utility_types/layout_widget.rs
@@ -327,7 +327,7 @@ impl LayoutGroup {
 		for widget in &mut widgets {
 			let val = match &mut widget.widget {
 				Widget::CheckboxInput(x) => &mut x.tooltip,
-				Widget::ColorButton(x) => &mut x.tooltip,
+				Widget::ColorInput(x) => &mut x.tooltip,
 				Widget::CurveInput(x) => &mut x.tooltip,
 				Widget::DropdownInput(x) => &mut x.tooltip,
 				Widget::FontInput(x) => &mut x.tooltip,
@@ -498,7 +498,7 @@ impl<T> Default for WidgetCallback<T> {
 pub enum Widget {
 	BreadcrumbTrailButtons(BreadcrumbTrailButtons),
 	CheckboxInput(CheckboxInput),
-	ColorButton(ColorButton),
+	ColorInput(ColorInput),
 	CurveInput(CurveInput),
 	DropdownInput(DropdownInput),
 	FontInput(FontInput),
@@ -571,7 +571,7 @@ impl DiffUpdate {
 			let mut tooltip_shortcut = match &mut widget_holder.widget {
 				Widget::BreadcrumbTrailButtons(widget) => Some((&mut widget.tooltip, &mut widget.tooltip_shortcut)),
 				Widget::CheckboxInput(widget) => Some((&mut widget.tooltip, &mut widget.tooltip_shortcut)),
-				Widget::ColorButton(widget) => Some((&mut widget.tooltip, &mut widget.tooltip_shortcut)),
+				Widget::ColorInput(widget) => Some((&mut widget.tooltip, &mut widget.tooltip_shortcut)),
 				Widget::DropdownInput(widget) => Some((&mut widget.tooltip, &mut widget.tooltip_shortcut)),
 				Widget::FontInput(widget) => Some((&mut widget.tooltip, &mut widget.tooltip_shortcut)),
 				Widget::IconButton(widget) => Some((&mut widget.tooltip, &mut widget.tooltip_shortcut)),

editor/src/messages/layout/utility_types/widgets/button_widgets.rs
@@ -148,7 +148,7 @@ pub struct ImageButton {
 
 #[derive(Clone, Derivative, serde::Serialize, serde::Deserialize, WidgetBuilder, specta::Type)]
 #[derivative(Debug, PartialEq, Default)]
-pub struct ColorButton {
+pub struct ColorInput {
 	#[widget_builder(constructor)]
 	pub value: FillChoice,
 
@@ -174,7 +174,7 @@ pub struct ColorButton {
 	// Callbacks
 	#[serde(skip)]
 	#[derivative(Debug = "ignore", PartialEq = "ignore")]
-	pub on_update: WidgetCallback<ColorButton>,
+	pub on_update: WidgetCallback<ColorInput>,
 
 	#[serde(skip)]
 	#[derivative(Debug = "ignore", PartialEq = "ignore")]

editor/src/messages/portfolio/document/navigation/navigation_message.rs
@@ -11,6 +11,8 @@ pub enum NavigationMessage {
 	BeginCanvasTilt { was_dispatched_from_menu: bool },
 	BeginCanvasZoom,
 	CanvasPan { delta: DVec2 },
+	CanvasPanAbortPrepare { x_not_y_axis: bool },
+	CanvasPanAbort { x_not_y_axis: bool },
 	CanvasPanByViewportFraction { delta: DVec2 },
 	CanvasPanMouseWheel { use_y_as_x: bool },
 	CanvasTiltResetAndZoomTo100Percent,

editor/src/messages/portfolio/document/navigation/navigation_message_handler.rs
@@ -29,6 +29,7 @@ pub struct NavigationMessageHandler {
 	navigation_operation: NavigationOperation,
 	mouse_position: ViewportPosition,
 	finish_operation_with_click: bool,
+	abortable_pan_start: Option<f64>,
 }
 
 impl MessageHandler<NavigationMessage, NavigationMessageData<'_>> for NavigationMessageHandler {
@@ -139,6 +140,28 @@ impl MessageHandler<NavigationMessage, NavigationMessageData<'_>> for Navigation
 
 				ptz.pan += transformed_delta;
 				responses.add(BroadcastEvent::CanvasTransformed);
+				responses.add(DocumentMessage::PTZUpdate);
+			}
+			NavigationMessage::CanvasPanAbortPrepare { x_not_y_axis } => {
+				let Some(ptz) = get_ptz_mut(document_ptz, network_interface, graph_view_overlay_open, breadcrumb_network_path) else {
+					log::error!("Could not get PTZ in CanvasPanAbortPrepare");
+					return;
+				};
+				self.abortable_pan_start = Some(if x_not_y_axis { ptz.pan.x } else { ptz.pan.y });
+			}
+			NavigationMessage::CanvasPanAbort { x_not_y_axis } => {
+				let Some(ptz) = get_ptz_mut(document_ptz, network_interface, graph_view_overlay_open, breadcrumb_network_path) else {
+					log::error!("Could not get PTZ in CanvasPanAbort");
+					return;
+				};
+				if let Some(abortable_pan_start) = self.abortable_pan_start {
+					if x_not_y_axis {
+						ptz.pan.x = abortable_pan_start;
+					} else {
+						ptz.pan.y = abortable_pan_start;
+					}
+				}
+				self.abortable_pan_start = None;
 				responses.add(DocumentMessage::PTZUpdate);
 			}
 			NavigationMessage::CanvasPanByViewportFraction { delta } => {

editor/src/messages/portfolio/document/node_graph/document_node_definitions.rs
@@ -3303,7 +3303,7 @@ fn static_input_properties() -> InputProperties {
 		"assign_colors_gradient".to_string(),
 		Box::new(|node_id, index, context| {
 			let (document_node, input_name) = node_properties::query_node_and_input_name(node_id, index, context)?;
-			let gradient_row = node_properties::color_widget(document_node, node_id, index, input_name, ColorButton::default().allow_none(false), true);
+			let gradient_row = node_properties::color_widget(document_node, node_id, index, input_name, ColorInput::default().allow_none(false), true);
 			Ok(vec![gradient_row])
 		}),
 	);
@@ -3329,7 +3329,7 @@ fn static_input_properties() -> InputProperties {
 		"mask_stencil".to_string(),
 		Box::new(|node_id, index, context| {
 			let (document_node, input_name) = node_properties::query_node_and_input_name(node_id, index, context)?;
-			let mask = node_properties::color_widget(document_node, node_id, index, input_name, ColorButton::default(), true);
+			let mask = node_properties::color_widget(document_node, node_id, index, input_name, ColorInput::default(), true);
 			Ok(vec![mask])
 		}),
 	);
@@ -3403,7 +3403,7 @@ fn static_input_properties() -> InputProperties {
 				node_id,
 				index,
 				input_name,
-				ColorButton::default().allow_none(false),
+				ColorInput::default().allow_none(false),
 				true,
 			)])
 		}),

editor/src/messages/portfolio/document/node_graph/node_graph_message_handler.rs
@@ -1822,7 +1822,7 @@ impl NodeGraphMessageHandler {
 		// If only one node is selected then show the preview or stop previewing button
 		if let Some(node_id) = previewing {
 			let button = TextButton::new("End Preview")
-				.icon(Some("Rescale".to_string()))
+				.icon(Some("FrameAll".to_string()))
 				.tooltip("Restore preview to the graph output")
 				.on_update(move |_| NodeGraphMessage::TogglePreview { node_id }.into())
 				.widget_holder();
@@ -1834,7 +1834,7 @@ impl NodeGraphMessageHandler {
 				.any(|export| matches!(export, NodeInput::Node { node_id: export_node_id, .. } if *export_node_id == node_id));
 			if selection_is_not_already_the_output && no_other_selections {
 				let button = TextButton::new("Preview")
-					.icon(Some("Rescale".to_string()))
+					.icon(Some("FrameAll".to_string()))
 					.tooltip("Preview selected node/layer (Shortcut: Alt-click node/layer)")
 					.on_update(move |_| NodeGraphMessage::TogglePreview { node_id }.into())
 					.widget_holder();

commit bc6e76208daef2f8a031ede23f0425ecafcd17de

Fix bug introduced in #2276 causing number inputs to abort on any keyboard input, not just Esc

frontend/src/components/widgets/inputs/NumberInput.svelte
@@ -277,11 +277,11 @@
 		clearTimeout(repeatTimeout);
 	}
 
-	function onIncrementMouseDown(e: MouseEvent) {
-		if (e.button === BUTTON_RIGHT) incrementPressAbort();
-	}
+	function incrementPressAbort(e: KeyboardEvent | MouseEvent) {
+		// Only abort if the user right clicks or presses Escape
+		if (e instanceof KeyboardEvent && e.key !== "Escape") return;
+		if (e instanceof MouseEvent && e.button !== BUTTON_RIGHT) return;
 
-	function incrementPressAbort() {
 		const element = self?.element() || undefined;
 		if (element) preventEscapeClosingParentFloatingMenu(element);
 
@@ -580,14 +580,13 @@
 		// Logic for aborting from a right click or pressing Escape.
 		const abortWithEscape = e instanceof KeyboardEvent && e.key === "Escape";
 		const abortWithRightClick = e instanceof MouseEvent && e.button === BUTTON_RIGHT;
-		if (abortWithEscape || abortWithRightClick) {
-			// Call the abort helper function
-			sliderAbort(abortWithEscape);
 
-			// Clean up these event listeners because they were for getting us into this function and now we're done with them.
-			removeEventListener("mousedown", sliderAbortFromMousedown);
-			removeEventListener("keydown", sliderAbortFromMousedown);
-		}
+		// Call the abort helper function
+		if (abortWithEscape || abortWithRightClick) sliderAbort(abortWithEscape);
+
+		// Clean up these event listeners because they were for getting us into this function and now we're done with them.
+		removeEventListener("mousedown", sliderAbortFromMousedown);
+		removeEventListener("keydown", sliderAbortFromMousedown);
 	}
 
 	// Helper function that performs the state management and cleanup for aborting the slider drag.
@@ -658,7 +657,7 @@
 			<button
 				class="arrow left"
 				on:pointerdown={(e) => onIncrementPointerDown(e, "Decrease")}
-				on:mousedown={onIncrementMouseDown}
+				on:mousedown={incrementPressAbort}
 				on:pointerup={onIncrementPointerUp}
 				on:pointerleave={onIncrementPointerUp}
 				tabindex="-1"
@@ -666,7 +665,7 @@
 			<button
 				class="arrow right"
 				on:pointerdown={(e) => onIncrementPointerDown(e, "Increase")}
-				on:mousedown={onIncrementMouseDown}
+				on:mousedown={incrementPressAbort}
 				on:pointerup={onIncrementPointerUp}
 				on:pointerleave={onIncrementPointerUp}
 				tabindex="-1"
@@ -738,6 +737,9 @@
 				background: rgba(var(--color-1-nearblack-rgb), 0.5);
 				// An outline can appear when pressing the arrow button with left click then hitting Escape, so this stops that from showing
 				outline: none;
+				// TODO: This is a quick, imperfect way to make the arrow buttons appear like they're behind the text (without messing with the element click targets if we used z-index).
+				// TODO: But it doesn't preserve the exact hover color due to the blending. Improve this by using a separate element for displaying the arrow and listening for pointer events.
+				mix-blend-mode: screen;
 
 				&.right {
 					right: 0;