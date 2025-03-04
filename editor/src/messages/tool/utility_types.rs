09c3f91f348a7d1ed83b2e4fb3590b3455199cfe
Dennis Kobert <dennis@kobert.dev>
2025-03-04 19:06:24 +0100

    Store has_dragged in tool state

--- editor/src/messages/tool/utility_types.rs
+++ editor/src/messages/tool/utility_types.rs
@@ -56,16 +56,16 @@ pub trait Fsm {
 	fn transition(self, message: ToolMessage, tool_data: &mut Self::ToolData, transition_data: &mut ToolActionHandlerData, options: &Self::ToolOptions, responses: &mut VecDeque<Message>) -> Self;
 
 	/// Implementing this trait function lets a specific tool provide a list of hints (user input actions presently available) to draw in the footer bar.
-	fn update_hints(&self, responses: &mut VecDeque<Message>, tool_data: &Self::ToolData);
+	fn update_hints(&self, responses: &mut VecDeque<Message>);
 	/// Implementing this trait function lets a specific tool set the current mouse cursor icon.
 	fn update_cursor(&self, responses: &mut VecDeque<Message>);
 
 	/// If this message is a standard tool message, process it and return true. Standard tool messages are those which are common across every tool.
-	fn standard_tool_messages(&self, message: &ToolMessage, responses: &mut VecDeque<Message>, tool_data: &mut Self::ToolData) -> bool {
+	fn standard_tool_messages(&self, message: &ToolMessage, responses: &mut VecDeque<Message>) -> bool {
 		// Check for standard hits or cursor events
 		match message {
 			ToolMessage::UpdateHints => {
-				self.update_hints(responses, tool_data);
+				self.update_hints(responses);
 				true
 			}
 			ToolMessage::UpdateCursor => {
@@ -90,7 +90,7 @@ pub trait Fsm {
 		Self: PartialEq + Sized + Copy,
 	{
 		// If this message is one of the standard tool messages, process it and exit early
-		if self.standard_tool_messages(&message, responses, tool_data) {
+		if self.standard_tool_messages(&message, responses) {
 			return;
 		}
 
@@ -100,7 +100,7 @@ pub trait Fsm {
 		// Update state
 		if *self != new_state {
 			*self = new_state;
-			self.update_hints(responses, tool_data);
+			self.update_hints(responses);
 			if update_cursor_on_transition {
 				self.update_cursor(responses);
 			}
