210e627bfbee8e777e4a4790d0b27006d363c32e
Dennis Kobert <dennis@kobert.dev>
2025-03-04 19:18:22 +0100

    Revert tool_data inclusion in update hints method

--- editor/src/messages/tool/tool_messages/spline_tool.rs
+++ editor/src/messages/tool/tool_messages/spline_tool.rs
@@ -439,7 +439,7 @@ impl Fsm for SplineToolFsmState {
 		}
 	}
 
-	fn update_hints(&self, responses: &mut VecDeque<Message>, _tool_data: &Self::ToolData) {
+	fn update_hints(&self, responses: &mut VecDeque<Message>) {
 		let hint_data = match self {
 			SplineToolFsmState::Ready => HintData(vec![HintGroup(vec![
 				HintInfo::mouse(MouseMotion::Lmb, "Draw Spline"),
