74b6abbb97588b207604702846eb85de9d378c9c
Ellen Gu <gu251@purdue.edu>
2025-03-09 15:18:42 -0400

    Fix fill tool on raster image temporarily breaking the graph (#2398)
    * Fix fill tool on raster image temporarily breaks the graph
    
    * Avoid vector filling raster layer via checking node category
    
    * check raster image using input type instead
    
    * Add additional check for TextureFrameTable
    
    * Enable the ignore raster test
    
    ---------
    
    Co-authored-by: hypercube <0hypercube@gmail.com>

--- editor/src/messages/tool/tool_messages/fill_tool.rs
+++ editor/src/messages/tool/tool_messages/fill_tool.rs
@@ -1,7 +1,6 @@
 use super::tool_prelude::*;
-
+use crate::messages::tool::common_functionality::graph_modification_utils::NodeGraphLayer;
 use graphene_core::vector::style::Fill;
-
 #[derive(Default)]
 pub struct FillTool {
 	fsm_state: FillToolFsmState,
@@ -87,6 +86,10 @@ impl Fsm for FillToolFsmState {
 				let Some(layer_identifier) = document.click(input) else {
 					return self;
 				};
+				// If the layer is a raster layer, don't fill it, wait till the flood fill tool is implemented
+				if NodeGraphLayer::is_raster_layer(layer_identifier, &mut document.network_interface) {
+					return self;
+				}
 				let fill = match color_event {
 					FillToolMessage::FillPrimaryColor => Fill::Solid(global_tool_data.primary_color),
 					FillToolMessage::FillSecondaryColor => Fill::Solid(global_tool_data.secondary_color),
@@ -147,8 +150,6 @@ mod test_fill {
 	}
 
 	#[tokio::test]
-	// TODO: fix https://github.com/GraphiteEditor/Graphite/issues/2270
-	#[should_panic]
 	async fn ignore_raster() {
 		let mut editor = EditorTestUtils::create();
 		editor.new_document().await;
