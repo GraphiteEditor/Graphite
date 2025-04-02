d31c878e4d285aa9a8bd7dc4e6cfd97afc841a62
Dennis Kobert <dennis@kobert.dev>
2025-04-02 08:24:41 +0200

    WIP debugging

--- node-graph/gcore/src/memo.rs
+++ node-graph/gcore/src/memo.rs
@@ -25,9 +25,11 @@ where
 		let mut hasher = DefaultHasher::new();
 		input.hash(&mut hasher);
 		let hash = hasher.finish();
+
 		if let Some(data) = self.cache.lock().as_ref().unwrap().as_ref().and_then(|data| (data.0 == hash).then_some(data.1.clone())) {
 			Box::pin(async move { data })
 		} else {
+			debug!("Cache miss for {hash}");
 			let fut = self.node.eval(input);
 			let cache = self.cache.clone();
 			Box::pin(async move {
