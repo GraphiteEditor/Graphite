GraphiteEditor/Graphite

commit c486c6e3e3fde2963f03115b12db11c5df145775

Fix Pen tool creates single anchor path (#2511)
* Delete layer if path has a single anchor

* Remove unnecessary layer reference from pen state

editor/src/messages/tool/tool_messages/pen_tool.rs
@@ -1601,6 +1601,8 @@ impl Fsm for PenToolFsmState {
 				PenToolFsmState::Ready
 			}
 			(_, PenToolMessage::Abort) => {
+				let should_delete_layer = tool_data.latest_points.len() == 1;
+
 				responses.add(DocumentMessage::AbortTransaction);
 				tool_data.handle_end = None;
 				tool_data.latest_points.clear();
@@ -1608,6 +1610,13 @@ impl Fsm for PenToolFsmState {
 				tool_data.draw_mode = DrawMode::BreakPath;
 				tool_data.snap_manager.cleanup(responses);
 
+				if should_delete_layer && layer.is_some() {
+					responses.add(NodeGraphMessage::DeleteNodes {
+						node_ids: vec![layer.unwrap().to_node()],
+						delete_children: true,
+					});
+					responses.add(NodeGraphMessage::RunDocumentGraph);
+				}
 				responses.add(OverlaysMessage::Draw);
 
 				PenToolFsmState::Ready

commit a6d3c066415ec18ea329a92ef44ab831e97e8c6d

Fix bug introduced in #2511 (#2513)
Fix issue when extending existing paths

Co-authored-by: Starklosch <45690579+Starklosch@users.noreply.github.com>

editor/src/messages/tool/tool_messages/pen_tool.rs
@@ -316,6 +316,7 @@ struct PenToolData {
 
 	snap_cache: SnapCache,
 }
+
 impl PenToolData {
 	fn latest_point(&self) -> Option<&LastPoint> {
 		self.latest_points.get(self.point_index)
@@ -1601,7 +1602,12 @@ impl Fsm for PenToolFsmState {
 				PenToolFsmState::Ready
 			}
 			(_, PenToolMessage::Abort) => {
-				let should_delete_layer = tool_data.latest_points.len() == 1;
+				let should_delete_layer = if layer.is_some() {
+					let vector_data = document.network_interface.compute_modified_vector(layer.unwrap()).unwrap();
+					vector_data.point_domain.ids().len() == 1
+				} else {
+					false
+				};
 
 				responses.add(DocumentMessage::AbortTransaction);
 				tool_data.handle_end = None;
@@ -1610,7 +1616,7 @@ impl Fsm for PenToolFsmState {
 				tool_data.draw_mode = DrawMode::BreakPath;
 				tool_data.snap_manager.cleanup(responses);
 
-				if should_delete_layer && layer.is_some() {
+				if should_delete_layer {
 					responses.add(NodeGraphMessage::DeleteNodes {
 						node_ids: vec![layer.unwrap().to_node()],
 						delete_children: true,