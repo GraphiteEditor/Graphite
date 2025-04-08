fa0a1ac68690ea1f20cccd2d770d8ea6af477365
Kythyria Tieran <kythyria@berigora.net>
2025-04-08 03:25:37 +0100

    Make Bounding Box have empty output on empty input
    Fixes a crash when using the text tool and the Text node is connected to
    a Bounding Box node.

--- node-graph/gcore/src/vector/vector_nodes.rs
+++ node-graph/gcore/src/vector/vector_nodes.rs
@@ -942,8 +942,13 @@ async fn bounding_box(_: impl Ctx, vector_data: VectorDataTable) -> VectorDataTa
 	let vector_data_transform = vector_data.transform();
 	let vector_data = vector_data.one_instance().instance;
 
-	let bounding_box = vector_data.bounding_box_with_transform(vector_data_transform).unwrap();
-	let mut result = VectorData::from_subpath(Subpath::new_rect(bounding_box[0], bounding_box[1]));
+	let bounding_box = vector_data.bounding_box_with_transform(vector_data_transform);
+	let mut result = if let Some(bb) = bounding_box {
+		VectorData::from_subpath(Subpath::new_rect(bb[0], bb[1]))
+	} else {
+		VectorData::empty()
+	};
+
 	result.style = vector_data.style.clone();
 	result.style.set_stroke_transform(DAffine2::IDENTITY);
 
