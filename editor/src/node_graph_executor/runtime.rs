1cfa7893130562e7719800afdcc221b73b490109
hypercube <0hypercube@gmail.com>
2025-04-20 17:12:37 +0100

    More information on monitor nodes

--- editor/src/node_graph_executor/runtime.rs
+++ editor/src/node_graph_executor/runtime.rs
@@ -295,7 +295,7 @@ impl NodeRuntime {
 			} else if let Some(record) = introspected_data.downcast_ref::<IORecord<Context, VectorDataTable>>() {
 				self.vector_modify.insert(parent_network_node_id, record.output.one_instance().instance.clone());
 			} else {
-				log::warn!("failed to downcast monitor node output");
+				log::warn!("failed to downcast monitor node output {parent_network_node_id:?}");
 			}
 		}
 	}
