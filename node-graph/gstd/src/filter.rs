286837fa22a8e44830fe80c2954a3dc05f5eb23f
Calvin Vu <calvintvu@berkeley.edu>
2025-04-20 22:44:50 -0700

    remove image crate, use conversion functions from color.rs

--- node-graph/gstd/src/filter.rs
+++ node-graph/gstd/src/filter.rs
@@ -1,9 +1,8 @@
 use graph_craft::proto::types::PixelLength;
-use graphene_core::raster::Channel;
 use graphene_core::raster::image::{Image, ImageFrameTable};
+use graphene_core::raster::{Bitmap, BitmapMut};
 use graphene_core::transform::{Transform, TransformMut};
 use graphene_core::{Color, Ctx};
-use image::{DynamicImage, ImageBuffer, Rgba};
 
 enum ConvertFunction {
 	ToLinear,
@@ -15,33 +14,19 @@ async fn blur(_: impl Ctx, image_frame: ImageFrameTable<Color>, #[range((0., 100
 	let image_frame_transform = image_frame.transform();
 	let image_frame_alpha_blending = image_frame.one_instance().alpha_blending;
 
-	let image = image_frame.one_instance().instance;
-
-	// Prepare the image data for processing
-	let image_data = bytemuck::cast_vec(image.data.clone());
-	let image_buffer = image::Rgba32FImage::from_raw(image.width, image.height, image_data).expect("Failed to convert internal image format into image-rs data type.");
+	let image = image_frame.one_instance().instance.clone();
 
 	// Run blur algorithm
 	let blurred_image = if radius < 0.1 {
 		// Minimum blur radius
-		image_buffer.into()
+		image.clone()
 	} else if box_blur {
-		gaussian_blur_algorithm(image_buffer, radius, gamma)
+		gaussian_blur_algorithm(image, radius, gamma)
 	} else {
-		box_blur_algorithm(image_buffer, radius, gamma)
+		box_blur_algorithm(image, radius, gamma)
 	};
 
-	// Prepare the image data for returning
-	let buffer = blurred_image.to_rgba32f().into_raw();
-	let color_vec = bytemuck::cast_vec(buffer);
-	let processed_image = Image {
-		width: image.width,
-		height: image.height,
-		data: color_vec,
-		base64_string: None,
-	};
-
-	let mut result = ImageFrameTable::new(processed_image);
+	let mut result = ImageFrameTable::new(blurred_image);
 	*result.transform_mut() = image_frame_transform;
 	*result.one_instance_mut().alpha_blending = *image_frame_alpha_blending;
 
@@ -49,23 +34,12 @@ async fn blur(_: impl Ctx, image_frame: ImageFrameTable<Color>, #[range((0., 100
 }
 
 // Helper to convert image buffer to linear/nonlinear color spaces in-place
-fn convert_color_space(image_buffer: &mut ImageBuffer<Rgba<f32>, Vec<f32>>, convert: ConvertFunction) {
-	for pixel in image_buffer.pixels_mut() {
-		// Leave alpha channels
-		let channels = pixel.0;
-
-		match convert {
-			ConvertFunction::ToLinear => {
-				pixel.0[0] = channels[0].to_linear();
-				pixel.0[1] = channels[1].to_linear();
-				pixel.0[2] = channels[2].to_linear();
-			}
-			ConvertFunction::ToGamma => {
-				pixel.0[0] = Channel::from_linear(channels[0]);
-				pixel.0[1] = Channel::from_linear(channels[1]);
-				pixel.0[2] = Channel::from_linear(channels[2]);
-			}
-		}
+fn convert_color_space(image: &mut Image<Color>, convert: ConvertFunction) {
+	for pixel in image.data.iter_mut() {
+		*pixel = match convert {
+			ConvertFunction::ToLinear => pixel.to_linear_srgb(),
+			ConvertFunction::ToGamma => pixel.to_gamma_srgb(),
+		};
 	}
 }
 
@@ -95,7 +69,7 @@ fn gaussian_kernel(radius: f64) -> Vec<f64> {
 	gaussian_kernel
 }
 
-fn gaussian_blur_algorithm(mut original_buffer: ImageBuffer<Rgba<f32>, Vec<f32>>, radius: f64, gamma: bool) -> DynamicImage {
+fn gaussian_blur_algorithm(mut original_buffer: Image<Color>, radius: f64, gamma: bool) -> Image<Color> {
 	if !gamma {
 		convert_color_space(&mut original_buffer, ConvertFunction::ToLinear)
 	}
@@ -107,8 +81,8 @@ fn gaussian_blur_algorithm(mut original_buffer: ImageBuffer<Rgba<f32>, Vec<f32>>
 	let half_kernel = kernel.len() / 2;
 
 	// Intermediate buffer for horizontal and vertical passes
-	let mut x_axis = ImageBuffer::<Rgba<f32>, Vec<f32>>::new(width, height);
-	let mut y_axis = ImageBuffer::<Rgba<f32>, Vec<f32>>::new(width, height);
+	let mut x_axis = Image::new(width, height, Color::TRANSPARENT);
+	let mut y_axis = Image::new(width, height, Color::TRANSPARENT);
 
 	for pass in [false, true] {
 		let (max, old_buffer, current_buffer) = match pass {
@@ -125,28 +99,24 @@ fn gaussian_blur_algorithm(mut original_buffer: ImageBuffer<Rgba<f32>, Vec<f32>>
 					let p = [x, y][pass] as i32 + (i as i32 - half_kernel as i32);
 
 					if p >= 0 && p < max as i32 {
-						let pixel = old_buffer.get_pixel([p as u32, x][pass], [y, p as u32][pass]);
-
-						r_sum += pixel[0] as f64 * weight;
-						g_sum += pixel[1] as f64 * weight;
-						b_sum += pixel[2] as f64 * weight;
-						a_sum += pixel[3] as f64 * weight;
-						weight_sum += weight;
+						if let Some(px) = old_buffer.get_pixel([p as u32, x][pass], [y, p as u32][pass]) {
+							r_sum += px.r() as f64 * weight;
+							g_sum += px.g() as f64 * weight;
+							b_sum += px.b() as f64 * weight;
+							a_sum += px.a() as f64 * weight;
+							weight_sum += weight;
+						}
 					}
 				}
 
 				// Normalize
-				let rgba = (r_sum, b_sum, g_sum, a_sum);
-				if weight_sum > 0. {
-					let r = (rgba.0 / weight_sum) as f32;
-					let g = (rgba.1 / weight_sum) as f32;
-					let b = (rgba.2 / weight_sum) as f32;
-					let a = (rgba.3 / weight_sum) as f32;
-
-					current_buffer.put_pixel(x, y, Rgba([r, g, b, a]));
+				let (r, g, b, a) = if weight_sum > 0. {
+					((r_sum / weight_sum) as f32, (g_sum / weight_sum) as f32, (b_sum / weight_sum) as f32, (a_sum / weight_sum) as f32)
 				} else {
-					current_buffer.put_pixel(x, y, *old_buffer.get_pixel(x, y));
-				}
+					let px = old_buffer.get_pixel(x, y).unwrap();
+					(px.r(), px.g(), px.b(), px.a())
+				};
+				current_buffer.set_pixel(x, y, Color::from_rgbaf32_unchecked(r, g, b, a));
 			}
 		}
 	}
@@ -155,17 +125,17 @@ fn gaussian_blur_algorithm(mut original_buffer: ImageBuffer<Rgba<f32>, Vec<f32>>
 		convert_color_space(&mut y_axis, ConvertFunction::ToGamma);
 	}
 
-	DynamicImage::ImageRgba32F(y_axis)
+	y_axis
 }
 
-fn box_blur_algorithm(mut original_buffer: ImageBuffer<Rgba<f32>, Vec<f32>>, radius: f64, gamma: bool) -> DynamicImage {
+fn box_blur_algorithm(mut original_buffer: Image<Color>, radius: f64, gamma: bool) -> Image<Color> {
 	if !gamma {
 		convert_color_space(&mut original_buffer, ConvertFunction::ToLinear)
 	}
 
 	let (width, height) = original_buffer.dimensions();
-	let mut x_axis = ImageBuffer::new(width, height);
-	let mut y_axis = ImageBuffer::new(width, height);
+	let mut x_axis = Image::new(width, height, Color::TRANSPARENT);
+	let mut y_axis = Image::new(width, height, Color::TRANSPARENT);
 
 	for pass in [false, true] {
 		let (max, old_buffer, current_buffer) = match pass {
@@ -180,18 +150,18 @@ fn box_blur_algorithm(mut original_buffer: ImageBuffer<Rgba<f32>, Vec<f32>>, rad
 
 				let i = [x, y][pass];
 				for d in (i as i32 - radius as i32).max(0)..=(i as i32 + radius as i32).min(max as i32 - 1) {
-					let pixel = old_buffer.get_pixel([d as u32, x][pass], [y, d as u32][pass]);
-					let weight = 1.;
-
-					r_sum += pixel[0] as f64 * weight;
-					g_sum += pixel[1] as f64 * weight;
-					b_sum += pixel[2] as f64 * weight;
-					a_sum += pixel[3] as f64 * weight;
-					weight_sum += weight;
+					if let Some(px) = old_buffer.get_pixel([d as u32, x][pass], [y, d as u32][pass]) {
+						let weight = 1.;
+						r_sum += px.r() as f64 * weight;
+						g_sum += px.g() as f64 * weight;
+						b_sum += px.b() as f64 * weight;
+						a_sum += px.a() as f64 * weight;
+						weight_sum += weight;
+					}
 				}
 
-				let pixel = Rgba([(r_sum / weight_sum) as f32, (g_sum / weight_sum) as f32, (b_sum / weight_sum) as f32, (a_sum / weight_sum) as f32]);
-				current_buffer.put_pixel(x, y, pixel);
+				let (r, g, b, a) = ((r_sum / weight_sum) as f32, (g_sum / weight_sum) as f32, (b_sum / weight_sum) as f32, (a_sum / weight_sum) as f32);
+				current_buffer.set_pixel(x, y, Color::from_rgbaf32_unchecked(r, g, b, a));
 			}
 		}
 	}
@@ -200,5 +170,5 @@ fn box_blur_algorithm(mut original_buffer: ImageBuffer<Rgba<f32>, Vec<f32>>, rad
 		convert_color_space(&mut y_axis, ConvertFunction::ToGamma);
 	}
 
-	DynamicImage::ImageRgba32F(y_axis)
+	y_axis
 }
