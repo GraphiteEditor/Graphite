1a81e45673ef6cb23b48bc49eef8cb17f1b9b492
seam0s-dev <153828136+seam0s-dev@users.noreply.github.com>
2025-04-30 14:15:46 +0300

    Add the settings popover menu for the Overlays toggle (#2523)
    * Added granular overlays control based on features
    
    * Added basic support for pivot, path, anchors and handles overlay settings
    
    * Added more overlay checks on anchors and handles
    
    * Add new settings over measurements, hover and selection overlays
    
    * Fix errors introduced while rebasing
    
    * Disable anchors and handles functionality with their overlays, extended selection outline check
    
    * Add check to enable/disable outlines on selected layers
    
    * Toggle handles checkbox in sync with anchors checkbox
    
    * Refactor overlays checks
    
    * Remove debug statements
    
    * Update select_tool.rs to resolve conflict
    
    * Minor fix to reflect anchor checkbox state on the handles
    
    * Minor fix to make anchors checkbox work
    
    * Rearrange menu items, and code review
    
    * Fix pivot dragging
    
    * Add handles overlay check when drawing with pen tool
    
    * Fix constrained dragging when transform cage is disabled
    
    * Fix deselecting user selection when anchors are disabled
    
    * Minor fix for disabling anchors
    
    * Remove All from OverlaysType
    
    * Remove debug statements
    
    * Fix editor crash when selecting other layers with path tool and anchors disabled
    
    * Minor fix on overlays check for all overlays
    
    * Add proper code formatting
    
    * Nits
    
    ---------
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

--- editor/src/messages/tool/tool_messages/path_tool.rs
+++ editor/src/messages/tool/tool_messages/path_tool.rs
@@ -960,6 +960,19 @@ impl Fsm for PathToolFsmState {
 				self
 			}
 			(_, PathToolMessage::Overlays(mut overlay_context)) => {
+				let display_anchors = overlay_context.visibility_settings.anchors();
+				let display_handles = overlay_context.visibility_settings.handles();
+				if !display_handles {
+					shape_editor.ignore_selected_handles();
+				} else {
+					shape_editor.mark_selected_handles();
+				}
+				if !display_anchors {
+					shape_editor.ignore_selected_anchors();
+				} else {
+					shape_editor.mark_selected_anchors();
+				}
+
 				// TODO: find the segment ids of which the selected points are a part of
 
 				match tool_options.path_overlay_mode {
