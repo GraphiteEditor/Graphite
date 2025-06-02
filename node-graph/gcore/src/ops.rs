46bc43a6d7664a79423c300c55103e20f04f713c
Dennis Kobert <dennis@kobert.dev>
2025-06-02 10:20:47 +0200

    Add isize/usize and i128/u128 implementations for Convert trait

--- node-graph/gcore/src/ops.rs
+++ node-graph/gcore/src/ops.rs
@@ -593,6 +593,10 @@ macro_rules! impl_convert {
 		impl_convert!(u32, $to);
 		impl_convert!(i64, $to);
 		impl_convert!(u64, $to);
+		impl_convert!(isize, $to);
+		impl_convert!(usize, $to);
+		impl_convert!(i128, $to);
+		impl_convert!(u128, $to);
 		impl_convert!(f32, $to);
 		impl_convert!(f64, $to);
 	};
@@ -605,6 +609,10 @@ impl_convert!(i32);
 impl_convert!(u32);
 impl_convert!(i64);
 impl_convert!(u64);
+impl_convert!(isize);
+impl_convert!(usize);
+impl_convert!(i128);
+impl_convert!(u128);
 impl_convert!(f32);
 impl_convert!(f64);
 
