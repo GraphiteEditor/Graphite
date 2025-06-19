7c9a018b88922ccd11613300e8f0df8569274be3
0SlowPoke0 <govindpratyush@gmail.com>
2025-06-19 12:32:43 +0530

    formatting-fix

--- editor/src/messages/tool/common_functionality/shapes/mod.rs
+++ editor/src/messages/tool/common_functionality/shapes/mod.rs
@@ -1,12 +1,12 @@
-pub mod polygon_shape;
 pub mod ellipse_shape;
 pub mod line_shape;
+pub mod polygon_shape;
 pub mod rectangle_shape;
 pub mod shape_utility;
 pub mod star_shape;
 
 pub use super::shapes::ellipse_shape::Ellipse;
-pub use super::shapes::line_shape::{ Line, LineEnd };
+pub use super::shapes::line_shape::{Line, LineEnd};
 pub use super::shapes::rectangle_shape::Rectangle;
 pub use crate::messages::tool::tool_messages::shape_tool::ShapeToolData;
 use glam::DVec2;
