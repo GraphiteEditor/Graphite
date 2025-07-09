a40de58c7c3ef3cf568476dea5b9e4549a04506a
mTvare <mTvare6@proton.me>
2025-07-09 12:15:41 +0530

    Fix brush bounding boxes by making BrushCacheImpl's hash not shared between different instances (#2845)
    * fix: add cache to each layer
    
    * fix: warning
    
    * fix: tests
    
    * Clean up code
    
    ---------
    
    Co-authored-by: Keavon Chambers <keavon@keavon.com>

--- editor/src/messages/tool/tool_messages/brush_tool.rs
+++ editor/src/messages/tool/tool_messages/brush_tool.rs
@@ -1,6 +1,6 @@
 use super::tool_prelude::*;
 use crate::consts::DEFAULT_BRUSH_SIZE;
-use crate::messages::portfolio::document::graph_operation::transform_utils::{get_current_normalized_pivot, get_current_transform};
+use crate::messages::portfolio::document::graph_operation::transform_utils::get_current_transform;
 use crate::messages::portfolio::document::node_graph::document_node_definitions::resolve_document_node_type;
 use crate::messages::portfolio::document::utility_types::document_metadata::LayerNodeIdentifier;
 use crate::messages::portfolio::document::utility_types::network_interface::FlowType;
@@ -287,9 +287,7 @@ impl BrushToolData {
 			}
 
 			if *reference == Some("Transform".to_string()) {
-				let upstream = document.metadata().upstream_transform(node_id);
-				let pivot = DAffine2::from_translation(upstream.transform_point2(get_current_normalized_pivot(&node.inputs)));
-				self.transform = pivot * get_current_transform(&node.inputs) * pivot.inverse() * self.transform;
+				self.transform = get_current_transform(&node.inputs) * self.transform;
 			}
 		}
 
