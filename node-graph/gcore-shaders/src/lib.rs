9b91b92657a9c2222df23fbe8ff5fcfafe5b542f
firestar99 <firestar99@sydow.cloud>
2025-07-24 11:31:38 +0200

    gcore-shaders: move `context::Ctx`

--- node-graph/gcore-shaders/src/lib.rs
+++ node-graph/gcore-shaders/src/lib.rs
@@ -1,9 +1,11 @@
-pub use glam;
-
 pub mod blending;
 pub mod choice_type;
 pub mod color;
+pub mod context;
 pub mod registry;
+
+pub use context::Ctx;
+pub use glam;
 
 pub trait AsU32 {
 	fn as_u32(&self) -> u32;
