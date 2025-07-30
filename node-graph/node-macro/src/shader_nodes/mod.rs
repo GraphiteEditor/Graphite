a94a02d0591cf7e4eb0a3153c76c971fdba90d22
firestar99 <firestar99@sydow.cloud>
2025-07-30 17:38:09 +0200

    shaders: codegen for per_pixel_adjust shader nodes

--- node-graph/node-macro/src/shader_nodes/mod.rs
+++ node-graph/node-macro/src/shader_nodes/mod.rs
@@ -1,9 +1,12 @@
-use crate::parsing::NodeFnAttributes;
+use crate::parsing::{NodeFnAttributes, ParsedNodeFn};
+use crate::shader_nodes::per_pixel_adjust::PerPixelAdjust;
 use proc_macro2::{Ident, TokenStream};
 use quote::quote;
-use strum::{EnumString, VariantNames};
+use strum::VariantNames;
 use syn::Error;
 use syn::parse::{Parse, ParseStream};
+
+pub mod per_pixel_adjust;
 
 pub const STD_FEATURE_GATE: &str = "std";
 
@@ -16,17 +19,33 @@ pub fn modify_cfg(attributes: &NodeFnAttributes) -> TokenStream {
 	}
 }
 
-#[derive(Debug, EnumString, VariantNames)]
+#[derive(Debug, VariantNames)]
 pub(crate) enum ShaderNodeType {
-	PerPixelAdjust,
+	PerPixelAdjust(PerPixelAdjust),
 }
 
 impl Parse for ShaderNodeType {
 	fn parse(input: ParseStream) -> syn::Result<Self> {
 		let ident: Ident = input.parse()?;
 		Ok(match ident.to_string().as_str() {
-			"PerPixelAdjust" => ShaderNodeType::PerPixelAdjust,
+			"PerPixelAdjust" => ShaderNodeType::PerPixelAdjust(PerPixelAdjust::parse(input)?),
 			_ => return Err(Error::new_spanned(&ident, format!("attr 'shader_node' must be one of {:?}", Self::VARIANTS))),
 		})
+	}
+}
+
+pub trait CodegenShaderEntryPoint {
+	fn codegen_shader_entry_point(&self, parsed: &ParsedNodeFn) -> syn::Result<TokenStream>;
+}
+
+impl CodegenShaderEntryPoint for ShaderNodeType {
+	fn codegen_shader_entry_point(&self, parsed: &ParsedNodeFn) -> syn::Result<TokenStream> {
+		if parsed.is_async {
+			return Err(Error::new_spanned(&parsed.fn_name, "Shader nodes must not be async"));
+		}
+
+		match self {
+			ShaderNodeType::PerPixelAdjust(x) => x.codegen_shader_entry_point(parsed),
+		}
 	}
 }
