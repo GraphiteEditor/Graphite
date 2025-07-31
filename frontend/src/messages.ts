07802204f212bda9ef39970a0d1e928666e61bbf
Keavon Chambers <keavon@keavon.com>
2025-07-31 02:49:16 -0700

    Fix ~1 second delay opening new document in Chrome by correctly setting willReadFrequently (#2962)
    * Fix ~1 second delay opening new document in Chrome by correctly setting willReadFrequently
    
    * Use willReadFrequently in a couple more places

--- frontend/src/messages.ts
+++ frontend/src/messages.ts
@@ -516,7 +516,7 @@ export class Color {
 		const canvas = document.createElement("canvas");
 		canvas.width = 1;
 		canvas.height = 1;
-		const context = canvas.getContext("2d");
+		const context = canvas.getContext("2d", { willReadFrequently: true });
 		if (!context) return undefined;
 
 		context.clearRect(0, 0, 1, 1);
