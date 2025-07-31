3cc9dd79fb9b43ca995b7377210b980ecc0c2cf9
Keavon Chambers <keavon@keavon.com>
2025-07-30 22:53:36 -0700

    Rename the Coordinate data type to Vec2 (#2959)

--- node-graph/gcore/src/extract_xy.rs
+++ node-graph/gcore/src/extract_xy.rs
@@ -2,9 +2,9 @@ use crate::Ctx;
 use dyn_any::DynAny;
 use glam::{DVec2, IVec2, UVec2};
 
-/// Obtains the X or Y component of a coordinate point.
+/// Obtains the X or Y component of a vec2.
 ///
-/// The inverse of this node is "Coordinate Value", which can have either or both its X and Y exposed as graph inputs.
+/// The inverse of this node is "Vec2 Value", which can have either or both its X and Y parameters exposed as graph inputs.
 #[node_macro::node(name("Extract XY"), category("Math: Vector"))]
 fn extract_xy<T: Into<DVec2>>(_: impl Ctx, #[implementations(DVec2, IVec2, UVec2)] vector: T, axis: XY) -> f64 {
 	match axis {
@@ -13,7 +13,7 @@ fn extract_xy<T: Into<DVec2>>(_: impl Ctx, #[implementations(DVec2, IVec2, UVec2
 	}
 }
 
-/// The X or Y component of a coordinate.
+/// The X or Y component of a vec2.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, DynAny, node_macro::ChoiceType, specta::Type, serde::Serialize, serde::Deserialize)]
 #[widget(Dropdown)]
 pub enum XY {
