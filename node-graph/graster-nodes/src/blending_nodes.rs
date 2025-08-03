8fbb81965c16958d8eae466009fc6ad5c2022aa4
firestar99 <firestar99@sydow.cloud>
2025-08-03 23:21:47 +0200

    shaders: switch to f32 params

--- node-graph/graster-nodes/src/blending_nodes.rs
+++ node-graph/graster-nodes/src/blending_nodes.rs
@@ -7,7 +7,7 @@ use graphene_core::table::Table;
 use graphene_core_shaders::Ctx;
 use graphene_core_shaders::blending::BlendMode;
 use graphene_core_shaders::color::{Color, Pixel};
-use graphene_core_shaders::registry::types::Percentage;
+use graphene_core_shaders::registry::types::{Percentage, PercentageF32};
 
 pub trait Blend<P: Pixel> {
 	fn blend(&self, under: &Self, blend_fn: impl Fn(P, P) -> P) -> Self;
@@ -80,7 +80,7 @@ mod blend_std {
 }
 
 #[inline(always)]
-pub fn blend_colors(foreground: Color, background: Color, blend_mode: BlendMode, opacity: f64) -> Color {
+pub fn blend_colors(foreground: Color, background: Color, blend_mode: BlendMode, opacity: f32) -> Color {
 	let target_color = match blend_mode {
 		// Other utility blend modes (hidden from the normal list) - do not have alpha blend
 		BlendMode::Erase => return background.alpha_subtract(foreground),
@@ -152,7 +152,7 @@ fn blend<T: Blend<Color> + Send>(
 	#[gpu_image]
 	under: T,
 	blend_mode: BlendMode,
-	#[default(100.)] opacity: Percentage,
+	#[default(100.)] opacity: PercentageF32,
 ) -> T {
 	over.blend(&under, |a, b| blend_colors(a, b, blend_mode, opacity / 100.))
 }
@@ -168,14 +168,11 @@ fn color_overlay<T: Adjust<Color>>(
 	)]
 	#[gpu_image]
 	mut image: T,
-	#[default(Color::BLACK)] color: Table<Color>,
+	#[default(Color::BLACK)] color: Color,
 	blend_mode: BlendMode,
-	#[default(100.)] opacity: Percentage,
+	#[default(100.)] opacity: PercentageF32,
 ) -> T {
 	let opacity = (opacity as f32 / 100.).clamp(0., 1.);
-
-	let color: Option<Color> = color.into();
-	let color = color.unwrap_or(Color::BLACK);
 
 	image.adjust(|pixel| {
 		let image = pixel.map_rgb(|channel| channel * (1. - opacity));
