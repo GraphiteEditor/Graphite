a4d15a39cd13e701c492515ec92af73502535556
Mohd Mohsin <mohsinmohd9718@gmail.com>
2025-08-03 10:42:00 +0530

    error handling for MessageHandler

--- editor/src/messages/message.rs
+++ editor/src/messages/message.rs
@@ -115,7 +115,11 @@ mod test {
 			} else {
 				("└── ", format!("{}    ", prefix))
 			};
-			if !data.name().is_empty() {
+
+			const FRONTEND_MESSAGE_STR: &str = "FrontendMessage";
+			if data.name().is_empty() && tree.name() != FRONTEND_MESSAGE_STR {
+				panic!("{}'s MessageHandler is missing #[message_handler_data]", tree.name());
+			} else {
 				file.write_all(format!("{}{}{} `{}`\n", prefix, branch, data.name(), data.path()).as_bytes()).unwrap();
 
 				for (i, field) in data.fields().iter().enumerate() {
