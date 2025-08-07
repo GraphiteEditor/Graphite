336f40dd0dc7ce8907f530651aeb1160b35984f3
Dennis Kobert <dennis@kobert.dev>
2025-08-07 11:50:22 +0200

    Include render node in runtime benchmarks

--- node-graph/interpreted-executor/benches/run_cached.rs
+++ node-graph/interpreted-executor/benches/run_cached.rs
@@ -2,15 +2,15 @@ mod benchmark_util;
 
 use benchmark_util::{bench_for_each_demo, setup_network};
 use criterion::{Criterion, criterion_group, criterion_main};
-use graphene_std::Context;
+use graphene_std::application_io::RenderConfig;
 
 fn subsequent_evaluations(c: &mut Criterion) {
 	let mut group = c.benchmark_group("Subsequent Evaluations");
-	let context: Context = None;
+	let context = RenderConfig::default();
 	bench_for_each_demo(&mut group, |name, g| {
 		let (executor, _) = setup_network(name);
 		g.bench_function(name, |b| {
-			b.iter(|| futures::executor::block_on(executor.tree().eval_tagged_value(executor.output(), criterion::black_box(context.clone()))).unwrap())
+			b.iter(|| futures::executor::block_on(executor.tree().eval_tagged_value(executor.output(), criterion::black_box(context))).unwrap())
 		});
 	});
 	group.finish();
