8853d4e8ab50f72dcf02419293c10cd1a3918746
Timon Schelling <me@timon.zip>
2025-08-08 16:16:35 +0000

    nix: use rustc_codegen_spirv.so from nix

--- node-graph/graster-nodes/shaders/build.rs
+++ node-graph/graster-nodes/shaders/build.rs
@@ -6,11 +6,22 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 	let shader_crate = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/.."));
 
-	// install the toolchain and build the `rustc_codegen_spirv` codegen backend with it
-	let backend = cargo_gpu::Install::from_shader_crate(shader_crate.clone()).run()?;
+	let rustc_codegen_spirv_path = std::env::var("RUSTC_CODEGEN_SPIRV_PATH").unwrap_or_default();
+	let mut builder = if rustc_codegen_spirv_path.is_empty() {
+		// install the toolchain and build the `rustc_codegen_spirv` codegen backend with it
+		cargo_gpu::Install::from_shader_crate(shader_crate.clone())
+			.run()?
+			.to_spirv_builder(shader_crate, "spirv-unknown-naga-wgsl")
+	} else {
+		// use the `RUSTC_CODEGEN_SPIRV` environment variable to find the codegen backend
+		let mut builder = cargo_gpu::spirv_builder::SpirvBuilder::new(shader_crate.clone(), "spirv-unknown-naga-wgsl");
+		builder.rustc_codegen_spirv_location = Some(PathBuf::from(rustc_codegen_spirv_path));
+		builder.toolchain_overwrite = Some("nightly".to_string());
+		builder.path_to_target_spec = Some(PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/spirv-unknown-naga-wgsl.json")));
+		builder
+	};
 
 	// build the shader crate
-	let mut builder = backend.to_spirv_builder(shader_crate, "spirv-unknown-naga-wgsl");
 	builder.print_metadata = MetadataPrintout::DependencyOnly;
 	builder.spirv_metadata = SpirvMetadata::Full;
 	builder.shader_crate_features.default_features = false;
