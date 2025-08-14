6c554beeb535f66f2bc78af8eea6446a10b5f9fb
Dennis Kobert <dennis@kobert.dev>
2025-08-14 23:45:27 +0200

    Implement vulkan dmabuf import

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -62,7 +62,7 @@ fn main() {
 	tracing::info!("Cef initialized successfully");
 
 	let rendering_loop_proxy = event_loop.create_proxy();
-	let target_fps = 60;
+	let target_fps = 120;
 	std::thread::spawn(move || {
 		loop {
 			let last_render = Instant::now();
@@ -72,6 +72,8 @@ fn main() {
 			}
 			let frame_time = Duration::from_secs_f32((target_fps as f32).recip());
 			let sleep = last_render + frame_time - Instant::now();
+
+			let _ = rendering_loop_proxy.send_event(CustomEvent::ScheduleBrowserWork(Instant::now()));
 			std::thread::sleep(sleep);
 		}
 	});
