568edd4d8668041f18913f3a10ccf67270b1f263
seabeeberry <seri@Mac.fritz.box>
2025-08-14 23:19:19 +0200

    Added fallback for Safari to trigger a pointerLockChange in case it doesnt fire automatically

--- frontend/src/components/widgets/inputs/NumberInput.svelte
+++ frontend/src/components/widgets/inputs/NumberInput.svelte
@@ -365,6 +365,11 @@
 			cumulativeDragDelta = 0;
 
 			document.exitPointerLock();
+
+			// Fallback for Safari in case pointerlockchange never fires
+			setTimeout(() => {
+				if (!document.pointerLockElement) pointerLockChange();
+			}, 0);
 		};
 		const pointerMove = (e: PointerEvent) => {
 			// Abort the drag if right click is down. This works here because a "pointermove" event is fired when right clicking even if the cursor didn't move.
