aba44eb676a46eb4c49b42d58df99857331c4963
firestar99 <firestar99@sydow.cloud>
2025-09-05 00:23:23 +0200

    shaders-rt: explain fullscreen_vertex coordinates with a drawing

--- node-graph/graster-nodes/src/fullscreen_vertex.rs
+++ node-graph/graster-nodes/src/fullscreen_vertex.rs
@@ -3,6 +3,21 @@ use spirv_std::spirv;
 
 /// webgpu NDC is like OpenGL: (-1.0 .. 1.0, -1.0 .. 1.0, 0.0 .. 1.0)
 /// https://www.w3.org/TR/webgpu/#coordinate-systems
+///
+/// So to make a fullscreen triangle around a box at (-1..1):
+///
+/// ```norun
+/// 3  +
+///    |\
+/// 2  |  \
+///    |    \
+/// 1  +-----+
+///    |     |\
+/// 0  |  0  |  \
+///    |     |    \
+/// -1 +-----+-----+
+///   -1  0  1  2  3
+/// ```
 const FULLSCREEN_VERTICES: [Vec2; 3] = [Vec2::new(-1., -1.), Vec2::new(-1., 3.), Vec2::new(3., -1.)];
 
 #[spirv(vertex)]
