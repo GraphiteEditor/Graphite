e97d5520e8d6719f49ed769b5384e4d5d1e54bbb
Timon <me@timon.zip>
2025-09-13 05:40:31 -0700

    Desktop: Support receiving double click inputs (#3168)
    * input support double clicks and clean up
    
    * Review improvements

--- desktop/src/cef/consts.rs
+++ desktop/src/cef/consts.rs
@@ -1,2 +1,12 @@
+use std::time::Duration;
+
 pub(crate) const RESOURCE_SCHEME: &str = "resources";
 pub(crate) const RESOURCE_DOMAIN: &str = "resources";
+
+pub(crate) const SCROLL_LINE_HEIGHT: usize = 40;
+pub(crate) const SCROLL_LINE_WIDTH: usize = 40;
+pub(crate) const SCROLL_SPEED_X: f32 = 3.0;
+pub(crate) const SCROLL_SPEED_Y: f32 = 3.0;
+
+pub(crate) const MULTICLICK_TIMEOUT: Duration = Duration::from_millis(500);
+pub(crate) const MULTICLICK_ALLOWED_TRAVEL: usize = 4;
