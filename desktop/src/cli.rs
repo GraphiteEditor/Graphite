daea5b617cd9ae316f88916cd84b0c9367bc9d30
Mateo <45690579+c-mateo@users.noreply.github.com>
2025-09-17 16:22:49 -0300

    Add ui-accelerated-painting option

--- desktop/src/cli.rs
+++ desktop/src/cli.rs
@@ -1,13 +1,22 @@
 use std::path::PathBuf;
 use std::sync::OnceLock;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Clone, ValueEnum)]
+pub enum UIAceleratedPainting {
+    Auto,
+    Yes,
+    No,
+}
 
 #[derive(Debug, Parser)]
-// #[command(version, about, long_about = None)]
 #[clap(name = "graphite-cli", version)]
 pub struct Cli {
 	pub files: Option<Vec<PathBuf>>,
+
+	#[arg(long, value_enum, default_value_t = UIAceleratedPainting::Auto,  help = "Enable UI accelerated painting using GPU")]
+	pub ui_accelerated_painting: UIAceleratedPainting,
 }
 
 static CLI: OnceLock<Cli> = OnceLock::new();
