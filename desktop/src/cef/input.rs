4a400960d6df213d609e3f56770781ae839af3f6
Timon Schelling <me@timon.zip>
2025-09-19 20:37:08 +0000

    pointer leave

--- desktop/src/cef/input.rs
+++ desktop/src/cef/input.rs
@@ -11,13 +11,23 @@ use super::consts::{MULTICLICK_ALLOWED_TRAVEL, MULTICLICK_TIMEOUT, SCROLL_LINE_H
 
 pub(crate) fn handle_window_event(browser: &Browser, input_state: &mut InputState, event: &WindowEvent) {
 	match event {
-		WindowEvent::PointerMoved { position, .. } => {
+		WindowEvent::PointerMoved { position, .. } | WindowEvent::PointerEntered { position, .. } => {
 			input_state.cursor_move(position);
 
 			let Some(host) = browser.host() else {
 				return;
 			};
 			host.send_mouse_move_event(Some(&input_state.into()), 0);
+		}
+		WindowEvent::PointerLeft { position, .. } => {
+			if let Some(position) = position {
+				input_state.cursor_move(position);
+			}
+
+			let Some(host) = browser.host() else {
+				return;
+			};
+			host.send_mouse_move_event(Some(&input_state.into()), 1);
 		}
 		WindowEvent::PointerButton { state, button, .. } => {
 			let mouse_button = match button {
