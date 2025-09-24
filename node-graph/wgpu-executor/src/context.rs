a5c83bbd0ba0e000ddd9d83a312b5b8036041d8a
Timon Schelling <me@timon.zip>
2025-09-24 01:06:59 +0000

    WgpuContextBuilder

--- node-graph/wgpu-executor/src/context.rs
+++ node-graph/wgpu-executor/src/context.rs
@@ -1,54 +1,123 @@
 use std::sync::Arc;
-use wgpu::{Device, Instance, Queue};
+use wgpu::{Adapter, Backends, Device, Features, Instance, Queue};
 
 #[derive(Debug, Clone)]
 pub struct Context {
 	pub device: Arc<Device>,
 	pub queue: Arc<Queue>,
 	pub instance: Arc<Instance>,
-	pub adapter: Arc<wgpu::Adapter>,
+	pub adapter: Arc<Adapter>,
 }
 
 impl Context {
 	pub async fn new() -> Option<Self> {
-		let instance_descriptor = wgpu::InstanceDescriptor {
-			backends: wgpu::Backends::all(),
-			..Default::default()
-		};
-		let instance = Instance::new(&instance_descriptor);
-
-		let adapter_options = wgpu::RequestAdapterOptions {
-			power_preference: wgpu::PowerPreference::HighPerformance,
-			compatible_surface: None,
-			force_fallback_adapter: false,
-		};
-
-		let adapter = instance.request_adapter(&adapter_options).await.ok()?;
-
-		Self::new_with_instance_and_adapter(instance, adapter).await
+		ContextBuilder::new().build().await
 	}
+}
 
-	pub async fn new_with_instance_and_adapter(instance: wgpu::Instance, adapter: wgpu::Adapter) -> Option<Self> {
-		let required_limits = adapter.limits();
-		let (device, queue) = adapter
-			.request_device(&wgpu::DeviceDescriptor {
-				label: None,
-				#[cfg(target_family = "wasm")]
-				required_features: wgpu::Features::empty(),
-				#[cfg(not(target_family = "wasm"))]
-				required_features: wgpu::Features::PUSH_CONSTANTS,
-				required_limits,
-				memory_hints: Default::default(),
-				trace: wgpu::Trace::Off,
+#[derive(Default)]
+pub struct ContextBuilder {
+	backends: Backends,
+	features: Features,
+}
+impl ContextBuilder {
+	pub fn new() -> Self {
+		Self {
+			backends: Backends::all(),
+			features: Features::empty(),
+		}
+	}
+	pub fn with_backends(mut self, backends: Backends) -> Self {
+		self.backends = backends;
+		self
+	}
+	pub fn with_features(mut self, features: Features) -> Self {
+		self.features = features;
+		self
+	}
+	pub async fn build(self) -> Option<Context> {
+		self.build_with_adapter_selection_inner(None::<WgpuAdapterSelectorFn>).await
+	}
+	pub async fn build_with_adapter_selection<S: WgpuAdapterSelector>(self, select: S) -> Option<Context> {
+		self.build_with_adapter_selection_inner(Some(select)).await
+	}
+	async fn build_with_adapter_selection_inner<S: WgpuAdapterSelector>(self, select: Option<S>) -> Option<Context> {
+		let instance = self.build_instance();
+
+		let selected_adapter = if let Some(select) = select {
+			self.select_adapter(&instance, select)
+		} else if cfg!(target_os = "windows") {
+			self.select_adapter(&instance, |adapters: &mut Vec<Adapter>| {
+				adapters.iter().position(|a| a.get_info().backend == wgpu::Backend::Dx12).map(|i| adapters.remove(i))
 			})
-			.await
-			.ok()?;
+		} else {
+			None
+		};
 
-		Some(Self {
+		let adapter = if let Some(adapter) = selected_adapter { adapter } else { self.request_adapter(&instance).await? };
+
+		let (device, queue) = self.request_device(&adapter).await?;
+		Some(Context {
 			device: Arc::new(device),
 			queue: Arc::new(queue),
 			adapter: Arc::new(adapter),
 			instance: Arc::new(instance),
 		})
+	}
+	pub async fn available_adapters_fmt(&self) -> impl std::fmt::Display {
+		let instance = self.build_instance();
+		let adapters = instance.enumerate_adapters(self.backends);
+		AvailableAdaptersFormatter(adapters)
+	}
+
+	fn build_instance(&self) -> Instance {
+		Instance::new(&wgpu::InstanceDescriptor {
+			backends: self.backends,
+			..Default::default()
+		})
+	}
+	async fn request_adapter(&self, instance: &Instance) -> Option<Adapter> {
+		instance
+			.request_adapter(&wgpu::RequestAdapterOptions {
+				power_preference: wgpu::PowerPreference::HighPerformance,
+				compatible_surface: None,
+				force_fallback_adapter: false,
+			})
+			.await
+			.ok()
+	}
+	fn select_adapter<S: WgpuAdapterSelector>(&self, instance: &Instance, select: S) -> Option<Adapter> {
+		select(&mut instance.enumerate_adapters(self.backends))
+	}
+	async fn request_device(&self, adapter: &Adapter) -> Option<(Device, Queue)> {
+		adapter
+			.request_device(&wgpu::DeviceDescriptor {
+				label: None,
+				required_features: self.features,
+				required_limits: adapter.limits(),
+				memory_hints: Default::default(),
+				trace: wgpu::Trace::Off,
+			})
+			.await
+			.ok()
+	}
+}
+
+pub trait WgpuAdapterSelector: FnOnce(&mut Vec<Adapter>) -> Option<Adapter> {}
+impl<F> WgpuAdapterSelector for F where F: FnOnce(&mut Vec<Adapter>) -> Option<Adapter> {}
+type WgpuAdapterSelectorFn = fn(&mut Vec<Adapter>) -> Option<Adapter>;
+
+struct AvailableAdaptersFormatter(Vec<Adapter>);
+impl std::fmt::Display for AvailableAdaptersFormatter {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		for (i, adapter) in self.0.iter().enumerate() {
+			let info = adapter.get_info();
+			writeln!(
+				f,
+				"[{}] {:?} {:?} (Name: {}, Driver: {}, Device: {})",
+				i, info.backend, info.device_type, info.name, info.driver, info.device,
+			)?;
+		}
+		Ok(())
 	}
 }
