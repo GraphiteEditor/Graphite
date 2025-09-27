84e44810d2f4be2db0de15fb30c070526887ee06
Dennis Kobert <dennis@kobert.dev>
2025-09-27 12:36:29 +0200

    Run preprocessing on benchmarks and fix deadlock (#3230)

--- node-graph/interpreted-executor/benches/benchmark_util.rs
+++ node-graph/interpreted-executor/benches/benchmark_util.rs
@@ -8,8 +8,12 @@ use interpreted_executor::dynamic_executor::DynamicExecutor;
 use interpreted_executor::util::wrap_network_in_scope;
 
 pub fn setup_network(name: &str) -> (DynamicExecutor, ProtoNetwork) {
-	let network = load_from_name(name);
+	let mut network = load_from_name(name);
 	let editor_api = std::sync::Arc::new(EditorApi::default());
+	println!("generating substitutions");
+	let substitutions = preprocessor::generate_node_substitutions();
+	println!("expanding network");
+	preprocessor::expand_network(&mut network, &substitutions);
 	let network = wrap_network_in_scope(network, editor_api);
 	let proto_network = compile(network);
 	let executor = block_on(DynamicExecutor::new(proto_network.clone())).unwrap();
