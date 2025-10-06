6c033a2cd614eda0f653e24ab326c287cd780f0a
Keavon Chambers <keavon@keavon.com>
2025-10-06 16:28:03 -0700

    Tidy up tooltip documentation for several assorted nodes (#3265)

--- node-graph/gcore/src/debug.rs
+++ node-graph/gcore/src/debug.rs
@@ -3,6 +3,7 @@ use crate::raster_types::{CPU, Raster};
 use crate::table::Table;
 use glam::{DAffine2, DVec2};
 
+/// Meant for debugging purposes, not general use. Logs the input value to the console and passes it through unchanged.
 #[node_macro::node(category("Debug"), name("Log to Console"))]
 fn log_to_console<T: std::fmt::Debug>(_: impl Ctx, #[implementations(bool, f64, u32, u64, DVec2, DAffine2, String)] value: T) -> T {
 	// KEEP THIS `debug!()` - It acts as the output for the debug node itself
