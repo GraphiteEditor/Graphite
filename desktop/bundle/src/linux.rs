36fcafd7998c85d83533cdcf1fc364a9bc3db41d
Timon Schelling <me@timon.zip>
2025-10-22 13:37:12 +0000

    fix linux

--- desktop/bundle/src/linux.rs
+++ desktop/bundle/src/linux.rs
@@ -1,7 +1,4 @@
-use std::collections::HashMap;
 use std::error::Error;
-use std::fs;
-use std::path::{Path, PathBuf};
 
 use crate::common::*;
 
@@ -10,12 +7,11 @@ const PACKAGE: &str = "graphite-desktop-platform-linux";
 pub fn main() -> Result<(), Box<dyn Error>> {
 	let app_bin = build_bin(PACKAGE, None)?;
 
-	//TODO: Implement bundling for linux
+	// TODO: Implement bundling for linux
 
 	// TODO: Consider adding more useful cli
 	if std::env::args().any(|a| a == "open") {
-		let executable_path = app_dir.join(EXEC_PATH).join(APP_NAME);
-		run_command(&executable_path.to_string_lossy(), &[]).expect("failed to open app");
+		run_command(&app_bin.to_string_lossy(), &[]).expect("failed to open app");
 	} else {
 		println!("Binary built and placed at {}", app_bin.to_string_lossy());
 		eprintln!("Bundling for Linux is not yet implemented.");
