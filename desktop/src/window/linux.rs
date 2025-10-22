abf9edfa55bb96b12ae445068600471d2009fe08
Timon Schelling <me@timon.zip>
2025-10-22 15:40:31 +0200

    window abstraction

---
+++ desktop/src/window/linux.rs
@@ -0,0 +1,28 @@
+use winit::event_loop::ActiveEventLoop;
+use winit::platform::wayland::ActiveEventLoopExtWayland;
+use winit::platform::wayland::WindowAttributesWayland;
+use winit::platform::x11::WindowAttributesX11;
+use winit::window::{Window, WindowAttributes};
+
+use crate::consts::{APP_ID, APP_NAME};
+
+use super::NativeWindow;
+
+pub(super) struct NativeWindowImpl {}
+
+impl NativeWindow for NativeWindowImpl {
+	fn configure(attributes: WindowAttributes, _event_loop: &dyn ActiveEventLoop) -> WindowAttributes {
+		if event_loop.is_wayland() {
+			let wayland_attributes = WindowAttributesWayland::default().with_name(APP_ID, "").with_prefer_csd(true);
+			attributes.with_platform_attributes(Box::new(wayland_attributes))
+		} else {
+			let x11_attributes = WindowAttributesX11::default().with_name(APP_ID, APP_NAME);
+			attributes.with_platform_attributes(Box::new(x11_attributes))
+		}
+		attributes.with_platform_attributes(Box::new(mac_window))
+	}
+
+	fn new(_window: &dyn Window) -> Self {
+		NativeWindowImpl {}
+	}
+}
