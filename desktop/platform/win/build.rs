c896953634efe0e9768948ef05dfd34ba92c1057
Timon Schelling <me@timon.zip>
2025-12-06 14:20:24 +0000

    set win product name

--- desktop/platform/win/build.rs
+++ desktop/platform/win/build.rs
@@ -3,6 +3,7 @@ fn main() {
 	{
 		let mut res = winres::WindowsResource::new();
 		res.set_icon("../../../branding/app-icons/graphite.ico");
+		res.set("ProductName", "Graphite");
 		res.compile().expect("Failed to compile Windows resources");
 	}
 }
