f179087a68d71c52166c456c8930a882d7c2edb1
Timon Schelling <me@timon.zip>
2025-12-19 16:34:58 +0100

    mac immediate render mode

--- desktop/src/render/state.rs
+++ desktop/src/render/state.rs
@@ -37,10 +37,13 @@ impl RenderState {
 			format: surface_format,
 			width: size.width,
 			height: size.height,
+			#[cfg(not(target_os = "macos"))]
 			present_mode: surface_caps.present_modes[0],
+			#[cfg(target_os = "macos")]
+			present_mode: wgpu::PresentMode::Immediate,
 			alpha_mode: surface_caps.alpha_modes[0],
 			view_formats: vec![],
-			desired_maximum_frame_latency: 2,
+			desired_maximum_frame_latency: 1,
 		};
 
 		surface.configure(&context.device, &config);
