7d274bb40f11e47fe7bf47ef827ff935003d1bea
Annonnymmousss <jatin02012006@gmail.com>
2025-12-28 01:47:30 +0530

    fix : Hints for G/R/S on the path tool don't show

--- editor/src/messages/tool/tool_messages/path_tool.rs
+++ editor/src/messages/tool/tool_messages/path_tool.rs
@@ -1566,7 +1566,6 @@ impl Fsm for PathToolFsmState {
 			..
 		} = tool_action_data;
 
-		update_dynamic_hints(self, responses, shape_editor, document, tool_data, tool_options, input.mouse.position);
 
 		let ToolMessage::Path(event) = event else { return self };
 
