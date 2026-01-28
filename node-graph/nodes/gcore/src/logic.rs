7220f472ca98e2e2282c5851179f319131acf411
Dennis Kobert <dennis@kobert.dev>
2026-01-28 13:23:19 +0100

    Add JSON get node

--- node-graph/nodes/gcore/src/logic.rs
+++ node-graph/nodes/gcore/src/logic.rs
@@ -90,6 +90,40 @@ fn string_split(
 	string.split(&delimeter).map(str::to_string).collect()
 }
 
+/// Gets a value from either a json object or array given as a string input.
+/// For example, for the input {"name": "ferris"} the key "name" will return "ferris".
+#[node_macro::node(category("Text"))]
+fn json_get(
+	_: impl Ctx,
+	/// The json data.
+	data: String,
+	/// The key to index the object with.
+	key: String,
+) -> String {
+	use serde_json::Value;
+	let Ok(value): Result<Value, _> = serde_json::from_str(&data) else {
+		return "Input is not valid json".into();
+	};
+	match value {
+		Value::Array(ref arr) => {
+			let Ok(index): Result<usize, _> = key.parse() else {
+				return "Json input is an array, but key is not a number".into();
+			};
+			let Some(value) = arr.get(index) else { return "Index out of bounds".into() };
+			value.to_string()
+		}
+		Value::Object(map) => {
+			let Some(value) = map.get(&key) else { return format!("key {key} not found in object") };
+			match value {
+				Value::String(s) => s.clone(),
+				Value::Number(n) => n.to_string(),
+				complex => complex.to_string(),
+			}
+		}
+		_ => "".into(),
+	}
+}
+
 /// Evaluates either the "If True" or "If False" input branch based on whether the input condition is true or false.
 #[node_macro::node(category("Math: Logic"))]
 async fn switch<T, C: Send + 'n + Clone>(
