fa293ae7eb1c0141b910b5c5138fa98fe9231b19
Kshetrimayum Abo <kshabo2004@gmail.com>
2026-02-07 01:05:56 +0530

    Pan and Zoom on Document when in Node Graph

--- editor/src/messages/portfolio/document/document_message.rs
+++ editor/src/messages/portfolio/document/document_message.rs
@@ -214,7 +214,9 @@ pub enum DocumentMessage {
 	UngroupLayer {
 		layer: LayerNodeIdentifier,
 	},
-	PTZUpdate,
+	PTZUpdate {
+		document_ptz_override: bool,
+	},
 	SelectionStepBack,
 	SelectionStepForward,
 	WrapContentInArtboard {
