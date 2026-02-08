e757351d59cf9633808769036fb6e12c1eac4b4a
Kulratan Thapar <kulratan.thapar97@gmail.com>
2026-02-08 11:15:53 +0000

    Make Path Editable

--- editor/src/messages/tool/tool_messages/path_tool.rs
+++ editor/src/messages/tool/tool_messages/path_tool.rs
@@ -12,7 +12,7 @@ use crate::messages::portfolio::document::overlays::utility_functions::{path_ove
 use crate::messages::portfolio::document::overlays::utility_types::{DrawHandles, OverlayContext};
 use crate::messages::portfolio::document::utility_types::clipboards::Clipboard;
 use crate::messages::portfolio::document::utility_types::document_metadata::{DocumentMetadata, LayerNodeIdentifier};
-use crate::messages::portfolio::document::utility_types::network_interface::NodeNetworkInterface;
+use crate::messages::portfolio::document::utility_types::network_interface::{NodeNetworkInterface, OutputConnector};
 use crate::messages::portfolio::document::utility_types::transformation::Axis;
 use crate::messages::preferences::SelectionMode;
 use crate::messages::tool::common_functionality::auto_panning::AutoPanning;
@@ -23,9 +23,12 @@ use crate::messages::tool::common_functionality::shape_editor::{
 };
 use crate::messages::tool::common_functionality::snapping::{SnapCache, SnapCandidatePoint, SnapConstraint, SnapData, SnapManager};
 use crate::messages::tool::common_functionality::utility_functions::{calculate_segment_angle, find_two_param_best_approximate, make_path_editable_is_allowed};
+use graph_craft::concrete;
 use graphene_std::Color;
+use graphene_std::Graphic;
 use graphene_std::renderer::Quad;
 use graphene_std::subpath::pathseg_points;
+use graphene_std::table::Table;
 use graphene_std::transform::ReferencePoint;
 use graphene_std::uuid::NodeId;
 use graphene_std::vector::algorithms::util::pathseg_tangent;
@@ -1576,7 +1579,40 @@ impl Fsm for PathToolFsmState {
 		match (self, event) {
 			(_, PathToolMessage::SelectionChanged) => {
 				// Set the newly targeted layers to visible
-				let target_layers = document.network_interface.selected_nodes().selected_layers(document.metadata()).collect();
+				let mut target_layers = document.network_interface.selected_nodes().selected_layers(document.metadata()).collect::<Vec<_>>();
+
+				let mut i = 0;
+				while i < target_layers.len() {
+					let layer = target_layers[i];
+``
+					let graph_layer = graph_modification_utils::NodeGraphLayer::new(layer, &document.network_interface);
+					let node_id = graph_layer.horizontal_layer_flow().nth(1);
+					let is_vector_layer = if let Some(node) = node_id {
+						let output_connector = OutputConnector::node(node, 0);
+						let output_type = document.network_interface.output_type(&output_connector, &[]);
+						output_type.compiled_nested_type() == Some(&concrete!(Table<Vector>))
+					} else {
+						false
+					};
+
+					if is_vector_layer {
+						i += 1;
+						continue;
+					}
+
+					let mut children = layer.children(document.metadata());
+					if let Some(first_child) = children.next() {
+						target_layers.remove(i);
+						target_layers.insert(i, first_child);
+						let mut count = 1;
+						for child in children {
+							target_layers.insert(i + count, child);
+							count += 1;
+						}
+					} else {
+						i += 1;
+					}
+				}
 
 				shape_editor.set_selected_layers(target_layers);
 
