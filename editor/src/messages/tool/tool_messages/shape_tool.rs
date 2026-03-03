440e876f73b89fd1a7ee6c3d91470a5c6fa3da8f
Ayush Amawate <ayush.amawate.ug20@nsut.ac.in>
2026-03-04 05:25:52 +0530

    remove redundant call

--- editor/src/messages/tool/tool_messages/shape_tool.rs
+++ editor/src/messages/tool/tool_messages/shape_tool.rs
@@ -964,7 +964,6 @@ impl Fsm for ShapeToolFsmState {
 						tool_options.stroke.apply_stroke(tool_options.line_weight, layer, defered_responses);
 					}
 				}
-				tool_options.stroke.apply_stroke(tool_options.line_weight, layer, defered_responses);
 
 				tool_data.data.layer = Some(layer);
 
