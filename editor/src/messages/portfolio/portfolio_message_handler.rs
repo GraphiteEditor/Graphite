a3f88b0f966d8c22d89999e29b607f824206f380
Keavon Chambers <keavon@keavon.com>
2026-03-09 16:28:26 -0700

    Prep for the RC4 release of the desktop app

--- editor/src/messages/portfolio/portfolio_message_handler.rs
+++ editor/src/messages/portfolio/portfolio_message_handler.rs
@@ -1057,7 +1057,7 @@ impl MessageHandler<PortfolioMessage, PortfolioMessageContext<'_>> for Portfolio
 			}
 			PortfolioMessage::RequestStatusBarInfoLayout => {
 				#[cfg(not(target_family = "wasm"))]
-				let widgets = vec![TextLabel::new("Graphite 1.0.0-RC3").disabled(true).widget_instance()]; // TODO: After the RCs, call this "Graphite (beta) x.y.z"
+				let widgets = vec![TextLabel::new("Graphite 1.0.0-RC4").disabled(true).widget_instance()]; // TODO: After the RCs, call this "Graphite (beta) x.y.z"
 				#[cfg(target_family = "wasm")]
 				let widgets = vec![];
 
