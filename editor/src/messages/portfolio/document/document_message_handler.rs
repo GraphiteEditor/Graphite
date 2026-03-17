4c72f9915b3a846517fe882d751ad745a5ef0438
Ziad Ashraf <ziad.ashraf.cs@gmail.com>
2026-03-17 03:11:54 +0200

    FIX: fixed the image import undo bug

--- editor/src/messages/portfolio/document/document_message_handler.rs
+++ editor/src/messages/portfolio/document/document_message_handler.rs
@@ -686,7 +686,7 @@ impl MessageHandler<DocumentMessage, DocumentMessageContext<'_>> for DocumentMes
 					responses.add(NodeGraphMessage::SetDisplayName {
 						node_id: layer.to_node(),
 						alias: name,
-						skip_adding_history_step: false,
+						skip_adding_history_step: true,
 					});
 				}
 				if let Some((parent, insert_index)) = parent_and_insert_index {
@@ -731,7 +731,7 @@ impl MessageHandler<DocumentMessage, DocumentMessageContext<'_>> for DocumentMes
 					responses.add(NodeGraphMessage::SetDisplayName {
 						node_id: layer.to_node(),
 						alias: name,
-						skip_adding_history_step: false,
+						skip_adding_history_step: true,
 					});
 				}
 				if let Some((parent, insert_index)) = parent_and_insert_index {
