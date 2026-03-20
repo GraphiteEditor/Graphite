dbc9d090364c8e389fb6190c992ed299da98ec36
Keavon Chambers <keavon@keavon.com>
2026-03-20 14:18:21 -0700

    Remove editor.ts

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,27 +1,39 @@
 <script lang="ts">
 	import { onMount, onDestroy } from "svelte";
 
-	import type { EditorHandle } from "@graphite/../wasm/pkg/graphite_wasm";
-	import { initWasm, createEditor } from "@graphite/editor";
-	import type { SubscriptionsRouter } from "/src/subscriptions-router";
+	import init, { EditorHandle, receiveNativeMessage } from "@graphite/../wasm/pkg/graphite_wasm";
+	import type { FrontendMessage } from "@graphite/../wasm/pkg/graphite_wasm";
+	import { loadDemoArtwork } from "@graphite/utility-functions/network";
+	import { operatingSystem } from "@graphite/utility-functions/platform";
+	import { createSubscriptionsRouter } from "/src/subscriptions-router";
+	import type { MessageName, SubscriptionsRouter } from "/src/subscriptions-router";
 
 	import Editor from "@graphite/components/Editor.svelte";
 
 	let subscriptions: SubscriptionsRouter | undefined = undefined;
 	let editor: EditorHandle | undefined = undefined;
-	let destroy: (() => void) | undefined = undefined;
 
 	onMount(async () => {
-		await initWasm();
+		// Initialize the Wasm module
+		const wasm = await init();
+		for (const [name, f] of Object.entries(wasm)) {
+			if (name.startsWith("__node_registry")) f();
+		}
+		window.imageCanvases = {};
+		window.receiveNativeMessage = receiveNativeMessage;
 
-		const created = createEditor();
-		subscriptions = created.subscriptions;
-		editor = created.editor;
-		destroy = created.destroy;
+		// Create the editor and subscriptions router
+		const randomSeed = BigInt(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER));
+		subscriptions = createSubscriptionsRouter();
+		editor = EditorHandle.create(operatingSystem(), randomSeed, (messageType: MessageName, messageData: FrontendMessage) => {
+			subscriptions?.handleFrontendMessage(messageType, messageData);
+		});
+
+		await loadDemoArtwork(editor);
 	});
 
 	onDestroy(() => {
-		destroy?.();
+		editor?.free();
 	});
 </script>
 
