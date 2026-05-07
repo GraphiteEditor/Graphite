2b5ead9dec33286e0cd31ce320252fbce8401aee
Timon <me@timon.zip>
2026-05-07 16:07:55 +0000

    Fix

--- desktop/src/socket.rs
+++ desktop/src/socket.rs
@@ -117,7 +117,9 @@ fn run(app_event_scheduler: AppEventScheduler, shutdown_receiver: mpsc::Receiver
 
 fn socket_name() -> Name<'static> {
 	if cfg!(target_os = "windows") {
-		APP_SOCKET_FILE_NAME.to_ns_name::<GenericNamespaced>().expect("valid named pipe name")
+		let dir = crate::dirs::app_data_dir().to_string_lossy().replace('\\', "-");
+		let name = format!("graphite-{dir}-{APP_SOCKET_FILE_NAME}");
+		name.to_ns_name::<GenericNamespaced>().expect("should be avalid named pipe name")
 	} else {
 		crate::dirs::app_data_dir().join(APP_SOCKET_FILE_NAME).to_fs_name::<GenericFilePath>().expect("valid socket path")
 	}
