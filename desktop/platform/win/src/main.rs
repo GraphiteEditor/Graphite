a4885a5f0c38523bf1a0f4a212106c8d80971c8c
Timon <me@timon.zip>
2026-05-08 15:28:19 +0000

    windows

--- desktop/platform/win/src/main.rs
+++ desktop/platform/win/src/main.rs
@@ -1,4 +1,11 @@
 #![windows_subsystem = "windows"]
+
+#[cfg(target_os = "windows")]
+mod file_associations;
+
 fn main() {
+	#[cfg(target_os = "windows")]
+	file_associations::register();
+
 	graphite_desktop::start();
 }
