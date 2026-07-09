1dc83ca71a247652dd1a0cc8d3eec1886773dd87
Sahil Sahney <sahilsahni18@outlook.com>
2026-07-10 00:04:09 +0530

    Fix node catalog preserving type search on autofocus

--- frontend/src/components/floating-menus/NodeCatalog.svelte
+++ frontend/src/components/floating-menus/NodeCatalog.svelte
@@ -113,7 +113,7 @@
 </script>
 
 <LayoutCol class="node-catalog">
-	<TextInput placeholder="Search Nodes…" value={searchTerm} on:value={({ detail }) => (searchTerm = detail)} bind:this={nodeSearchInput} />
+	<TextInput placeholder="Search Nodes…" value={searchTerm} on:value={({ detail }) => (searchTerm = detail)} selectAllOnFocus={false} bind:this={nodeSearchInput} />
 	<div class="list-results" on:wheel|passive|stopPropagation>
 		{#each nodeCategories as nodeCategory}
 			<details open={nodeCategory[1].open}>
