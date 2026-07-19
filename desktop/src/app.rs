1ab1a1861e8ce62cee61c548c36f2ff6ef3b87a2
Timon <me@timon.zip>
2026-07-19 19:30:59 +0000

    Desktop: End pointer lock and pending window drags on any left-equivalent release

--- desktop/src/app.rs
+++ desktop/src/app.rs
@@ -539,9 +539,10 @@ impl ApplicationHandler for App {
 		if let Some(pointer_lock_position) = self.pointer_lock_position
 			&& let WindowEvent::PointerButton {
 				state: ElementState::Released,
-				button: ButtonSource::Mouse(MouseButton::Left),
+				button,
 				..
-			} = event
+			} = &event
+			&& button.clone().mouse_button() == MouseButton::Left
 		{
 			self.pointer_lock_position = None;
 			if let Some(window) = &self.window {
@@ -652,10 +653,10 @@ impl ApplicationHandler for App {
 			}
 
 			WindowEvent::PointerButton {
-				button: ButtonSource::Mouse(MouseButton::Left),
+				button,
 				state: ElementState::Released,
 				..
-			} => {
+			} if button.clone().mouse_button() == MouseButton::Left => {
 				self.window_pending_drag = false;
 			}
 
