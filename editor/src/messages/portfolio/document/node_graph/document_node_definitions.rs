dfec7c9e4ba85aabab15b2cb3169ade0546e1d11
Keavon Chambers <keavon@keavon.com>
2026-07-22 22:03:50 -0700

    New nodes: 'Sign', 'Distance', 'Cross Product', and 'Lerp' (#4361)

--- editor/src/messages/portfolio/document/node_graph/document_node_definitions.rs
+++ editor/src/messages/portfolio/document/node_graph/document_node_definitions.rs
@@ -1221,7 +1221,7 @@ fn document_node_definitions() -> HashMap<DefinitionIdentifier, DocumentNodeDefi
 		},
 		DocumentNodeDefinition {
 			identifier: "Split Vec2",
-			category: "Math: Vector",
+			category: "Math: Vec2",
 			node_template: NodeTemplate {
 				document_node: DocumentNode {
 					implementation: DocumentNodeImplementation::Network(NodeNetwork {
