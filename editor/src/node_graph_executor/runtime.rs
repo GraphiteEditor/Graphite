8b53983e9d766c98151aa674076afd80b174b3ee
Dennis Kobert <dennis@kobert.dev>
2026-08-01 00:59:26 +0200

    Silence the dead-code warnings for groundwork the host wiring will use

--- editor/src/node_graph_executor/runtime.rs
+++ editor/src/node_graph_executor/runtime.rs
@@ -41,6 +41,7 @@ pub struct NodeRuntime {
 	editor_preferences: EditorPreferences,
 	old_graph: Option<NodeNetwork>,
 	update_thumbnails: bool,
+	#[expect(dead_code, reason = "read once the host wires a notifier onto the runtime")]
 	graph_runtime: Arc<DynGraphRuntime>,
 
 	editor_api: Arc<PlatformEditorApi>,
