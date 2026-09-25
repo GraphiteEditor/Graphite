648f1212a0f4d8d35e25f291d78a3054c918abe7
James Lindsay <0hypercube@gmail.com>
2026-09-25 09:42:48 +0100

    Avoid unused dependency warnings

--- desktop/bundle/src/common.rs
+++ desktop/bundle/src/common.rs
@@ -25,6 +25,8 @@ pub(crate) fn profile_path() -> PathBuf {
 }
 
 pub(crate) fn cef_path() -> PathBuf {
+	// Avoid incorrect warnings about an unused dependency (this crate sets the `CEF_PATH` environment variable used below)
+	use cef_dll_sys as _;
 	PathBuf::from(env!("CEF_PATH"))
 }
 
--- tools/third-party-licenses/src/cef.rs
+++ tools/third-party-licenses/src/cef.rs
@@ -65,6 +65,9 @@ fn parse(html: &str) -> Vec<LicenseEntry> {
 }
 
 fn read() -> Result<String, Error> {
+	// Avoid incorrect warnings about an unused dependency (this crate sets the `CEF_PATH` environment variable used below)
+	use cef_dll_sys as _;
+
 	let cef_path = PathBuf::from(env!("CEF_PATH"));
 	let cef_credits = std::fs::read_dir(&cef_path)
 		.map_err(|e| Error::Io(e, format!("Failed to read CEF_PATH directory {}", cef_path.display())))?
